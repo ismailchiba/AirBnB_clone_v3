4533ed7c229f3ac8a648ec335f8b499c317c505e
Kayme976 <7508@holbertonstudents.com>
2024-04-19 18:53:45 +0200

    Refactor error handling and CORS configuration in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,29 +3,35 @@
 Entry point to start the application.
 """
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
+@app.errorhandler(404)
+def error_handler(exception):
+    """Handles errors"""
+
+    return jsonify({"error": "Not found"}), 404
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown"""
+def teardown(exception):
+    """Teardown method to close the storage"""
+
     storage.close()
 
 
-@app.errorhandler(404)
-def error_404(exception):
-    """Returns the 404 error custom messsage"""
-    return {"error": "Not found"}, 404
-
-
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True, debug=True)
+
+    app_host = getenv("HBNB_API_HOST", "0.0.0.0")
+    app_port = int(getenv("HBNB_API_PORT", 5000))
+
+    app.run(host=app_host, port=app_port, threaded=True)
