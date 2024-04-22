415cca785cda7761d935f58365f6fe662623e18d
MarcosBremermann <mabm2004@gmail.com>
2024-04-21 22:40:39 -0300

    Task 5 v1.0

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     return (jsonify({"status": "OK"}))
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def object_count():
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
