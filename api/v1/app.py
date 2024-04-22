8c691a908e2da9f096841110a62b3a5d8e943510
MarcosBremermann <mabm2004@gmail.com>
2024-04-21 23:08:45 -0300

    404 again

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,8 +12,8 @@ def teardown(close):
     storage.close()
 
 @app.errorhandler(404)
-def not_found_error(error):
-    return jsonify({"error": "Not found"}), 404
+def not_found_error(exception):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
