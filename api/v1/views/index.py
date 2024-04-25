fdb33272bf6700a1ca364ecc2b8e15842cb84ad3
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-26 00:53:15 +0100

    Get objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,29 @@ INDEX
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route("/status", strict_slashes=False)
 def getStatus():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats", strict_slashes=False)
+def getObjects():
+    objects = {}
+    for key, value in classes.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
 
 
 if __name__ == "__main__":
