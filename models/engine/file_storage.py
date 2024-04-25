d683e4103ec3540b4e6ba2507b8b0cb7a72b6696
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-25 16:33:03 -0700

    pep 8 validation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -77,7 +77,7 @@ class FileStorage:
                 return record
             else:
                 return None
-    
+
     def count(self, cls=None):
         """retunr the count of total storage or count of class"""
         count = 0
