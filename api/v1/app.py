08b9b01d61ba3585451601c382d51197d5f9b3cc
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-04-25 14:39:14 +0200

    fixing some errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,11 +10,17 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown_appcontext(self):
     storage.close()
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    if os.getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = os.getenv("HBNB_API_HOST")
+    if os.getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(os.getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
