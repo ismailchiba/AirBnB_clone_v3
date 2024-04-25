6a170e26b3d66aecc536fa23961d4f0d541c522e
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-04-25 14:18:47 +0200

    fixing some errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,7 @@
-import json
-
 from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status')
-def status():
-    Status = {
-        'status': "OK",
-    }
-    data = json.dumps(Status)
-    return data
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "OK"})
