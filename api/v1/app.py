f14efd4e1fa5b002e0d47804c2444f7847c93ae1
Ifeanyi Zion <ifeanyizion3@gmail.com>
2024-04-26 04:30:00 -0700

    env variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,14 +2,17 @@
 """
     Api module
 """
-
+import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+"""Flask instance"""
 app.register_blueprint(app_views)
-
+app.url_map.strict_slashes = False
+app_host=os.getenv("HBNB_API_HOST", "0.0.0.0")
+app_port = int(os.getenv("HBNB_API_PORT", "5000"))
 
 @app.teardown_appcontext
 def end_session(exception):
@@ -18,4 +21,10 @@ def end_session(exception):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    app_host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    app_port = int(os.getenv("HBNB_API_PORT", "5000"))
+    app.run(
+            host = app_host,
+            port = app_port,
+            threaded = True
+           )
