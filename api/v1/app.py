db65c1d4832eeaa85be0d6e443a65070f53f33ec
Youssef Elshabrawy <66743031+YElshabrawy@users.noreply.github.com>
2024-04-26 23:13:37 +0300

    [5] not found route

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''this module creates a flask app'''
 
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 from os import getenv
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 def close_storage(exception):
     '''closes storage'''
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    '''returns a 404 error'''
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,9 @@
 '''index for blueprint'''
 
 from api.v1.views import app_views
+from flask import jsonify
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     '''returns status'''
-    return {"status": "OK"}
+    return jsonify({"status": "OK"}), 200
