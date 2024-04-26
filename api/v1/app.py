da0bf33ca861d329b78dd0473f43c65e097bd99d
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-04-26 01:14:59 +0100

    Create 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Create new app """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
@@ -15,7 +15,12 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def notFound(e):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == '__main__':
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', '5000')), 
+            port=int(getenv('HBNB_API_PORT', '5000')),
             threaded=True)
