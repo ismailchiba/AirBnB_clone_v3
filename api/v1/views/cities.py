7a47a0a1d01776745b6a10ea8f952abd429ec8e3
root <root@TasnimSomo>
2024-04-26 19:43:54 +0400

    AirBnB_clone_v3

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,7 @@ from models import storage
 from models.city import City
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'])
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
     """ Gets cities for state_id """
     state = storage.get(State, state_id)
@@ -19,7 +19,7 @@ def get_cities(state_id):
     return jsonify(cities_json), 200
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'])
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
      """ get city by id"""
     city = storage.get(City, city_id)
@@ -29,7 +29,7 @@ def get_city(city_id):
     return jsonify(city_json), 200
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'])
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """ delete city by id"""
     city = storage.get(City, city_id)
@@ -40,7 +40,7 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'])
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """ create new instance """
     state = storage.get(State, state_id)
@@ -58,7 +58,7 @@ def create_city(state_id):
     return jsonify(new_city), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'])
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """  """
     city = storage.get(City, city_id)
