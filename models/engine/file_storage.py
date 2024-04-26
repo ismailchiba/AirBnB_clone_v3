d5d9c25cbb074c0aaa36c4d9dd02ef22b4defa2e
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-26 21:53:57 +0100

    Made changes to conform to pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileExistsError:
             pass
 
     def delete(self, obj=None):
@@ -68,7 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-    
+
     def get(self, cls, id):
         """ This method returns an object based on class and its ID
 
