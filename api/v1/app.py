fe632b7ba3711122abb75ab4a4d9e7ae926a828e
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-26 10:35:38 +0300

    RESTful API

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Status of your API"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def app_tear(error):
+    storage.close()
+
+@app.errorhandler(404)
+def handle_404_error(error):
+    error_response = {"error": "Not found"}
+    return jsonify(error_response), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
