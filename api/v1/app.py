d5319f82abeb95a6158d7ce7e833691ae0a090f7
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-04-27 02:55:30 +0400

    not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """"run app with Flask framework"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -8,6 +8,12 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """404 not found"""
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,14 @@ from models import storage
 def status():
     """Simple function that returns status: ok"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Returns json that contains stats of storage"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
