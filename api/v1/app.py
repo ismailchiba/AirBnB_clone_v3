9b6fa76ce5c38bf97bdf1f728a7bf0b09c4415b7
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-04-26 16:53:02 +0400

    pep fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+""" Starts a Flask web application """
+
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -9,8 +12,11 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
+    """Closes the storage"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=int(port), threaded=True)
