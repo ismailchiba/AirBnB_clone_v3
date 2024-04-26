e45ab79e918f13916c0f2bd1291505ec10de4bb7
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-04-26 17:48:25 +0400

    count of object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,3 +7,23 @@ from api.v1.views import app_views
 def status():
     """Returns a JSON response"""
     return {"status": "OK"}
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Returns object counts"""
+    from models import storage
+    states = storage.count('State')
+    amenities = storage.count('Amenity')
+    cities = storage.count('City')
+    places = storage.count('Place')
+    reviews = storage.count('Review')
+    users = storage.count('User')
+    return {
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users
+    }
