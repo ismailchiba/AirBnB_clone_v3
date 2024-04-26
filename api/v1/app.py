08eed9385196f0670110ee0403641d53a8614e16
DaisyAram <daisyarama03@gmail.com>
2024-04-26 09:43:21 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,18 +9,20 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(self):
     """ handles teardown"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """handles 404 error"""
-    return jsonify('error' :'Not found'), 404
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", "5000")
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
 from flask import Flask, jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status')
+
+@app_views.route('/status', methods=['GET'])
 def app_return():
     """returns a JSON"""
-    return (status='OK')
+    return jsonify({'status': 'OK'})
 
-@app_views.route('/stats')
+
+@app_views.route('/stats', method=['GET'])
 def status():
     """retrives the number of each objects by type"""
     storage = Storage()
