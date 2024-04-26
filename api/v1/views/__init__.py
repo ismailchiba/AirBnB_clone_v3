966311e78a545fa7da82a11084587665266297a0
root <root@TasnimSomo>
2024-04-26 14:55:03 +0400

    AirBnB_clone_v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Status of your API"""
 
-from .index import *
 from flask import Blueprint
-from api.v1.views import amenities
-from api.v1.views import cities
-from api.v1.views import states
-
 
 app_views = Blueprint('app_views', __name__, url='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
