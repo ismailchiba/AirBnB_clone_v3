505de15657eec0109f4872f349862f3ba30da0aa
Omar-Algassim <omarggg2@gamil.com>
2024-04-28 02:59:39 +0300

    fix status api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python3
 """ airbnb api wih flask"""
 
 
 from flask import Flask
 from models import storage
-from api.v1.views import app_viwes
+from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
-app.register_blueprint(app_viwes)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -14,11 +16,13 @@ def tearodwn_close(exception):
     """close data base"""
     storage.close()
 
-app.route("/status")
-def status():
-    return jsonify({"status": "ok"})
-
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    host = os.getenv("HBNB_API_HOST")
+    port = os.getenv("HBNB_API_PORT")
+    if host is None:
+        host = "0.0.0.0"
+    if port is None:
+        port = "5000"
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,13 @@
 
+#!/usr/bin/python3
+"""index """
+
+
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route("/status")
 def status():
+    """retrun status json"""
     return jsonify({"status": "ok"})
