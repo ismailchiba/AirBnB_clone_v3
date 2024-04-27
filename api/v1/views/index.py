de3692399c7cae26e1561d31ba55283528eee404
AMR BY <bouyoussefamr@gmail.com>
2024-04-27 00:01:21 +0000

    mandatory

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """return JSON """
 from api.v1.views import app_views
+import jsonify
 
 
 @app_views.route('/status')
 def status():
-    stat = status = 'OK'}
-    return stat
+    stat = {'status' : 'OK'}
+    return jsonify({'status' : stat})
