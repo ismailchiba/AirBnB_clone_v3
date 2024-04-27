98cf38935afb4a031d74498a446f287e38708d9d
Queendarlin Nnamani <queendarlinnnamani@gmail.com>
2024-04-27 13:07:01 +0100

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ This module contains the endpoint definitions for the AirBnB clone API.
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import  storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
@@ -13,3 +13,19 @@ def api_status():
     Returns the status of the API as a JSON response.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def api_stats():
+    """
+    Retrieves the number of each object type and returns as a JSON response.
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
