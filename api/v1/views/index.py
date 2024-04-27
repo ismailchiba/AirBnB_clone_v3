b32abc03f0f3baed60040083a633f8a17553c930
ashtkarim <assihoutkarim@gmail.com>
2024-04-27 21:00:30 +0000

    fix checker

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,7 +16,7 @@ classes = {
     "amenities": Amenity,
     "cities": City,
     "places": Place,
-    "reviews":Review,
+    "reviews": Review,
     "states": State,
     "users": User
 }
@@ -24,16 +24,14 @@ classes = {
 
 @app_views.route("/status", strict_slashes=False)
 def getStatus():
+    """create a route /status"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats", strict_slashes=False)
 def getObjects():
+    """Route: /api/v1/stats"""
     objects = {}
     for key, value in classes.items():
         objects[key] = storage.count(value)
     return jsonify(objects)
-
-
-if __name__ == "__main__":
-    pass
