aced118d212fc0a0667bc532e09f7b7e67b5d997
emarvelm <marvelousegbe19@gmail.com>
2024-04-27 11:21:34 +0100

    tests must pass

--- models/base_model.py
+++ models/base_model.py
@@ -42,9 +42,10 @@ class BaseModel:
                 self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
+
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
 
     def __str__(self):
