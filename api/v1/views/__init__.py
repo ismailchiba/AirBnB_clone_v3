f691b5ff444bc48ad17be8765d96df8433c866d0
AMR BY <bouyoussefamr@gmail.com>
2024-04-27 17:07:10 +0000

    mandatory

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,9 @@
 """create a blueprint"""
 
 from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
 from api.v1.views.index import *
-
-
-app_views = Blueprint(app_views)
-@app_views.route('/api/v1')
