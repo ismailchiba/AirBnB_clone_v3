16ef7bdea3e1e4b864dacb9ce0801f8c99e9abdb
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-04-27 02:50:03 +0100

    Refactor app.py to set blue print for app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
+""" This module sets blue print for app"""
+
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
