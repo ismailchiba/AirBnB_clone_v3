3bc73376d0501d31f75f352a946d90b159042d7b
ZobaOkafor <judithokafor30@gmail.com>
2024-04-27 13:26:23 -0700

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,12 @@ This module contains the endpoint definitions for the AirBnB clone API.
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
