d80e409c568b75d7e696c6d096f75c2cf1d165b6
DaBetterman <1ebrahimr@gmail.com>
2024-04-27 17:00:45 +0200

    task 6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,8 @@
 """ index file for the project """
 
 
-from . import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -15,4 +15,9 @@ def status():
 @app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """ the stats count """
-    return jsonify(storage.count())
+    todos = {'states': State, 'users': User,
+             'amenities': Amenity, 'cities': City,
+             'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
