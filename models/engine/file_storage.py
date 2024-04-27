6246e7b4c2f2f341d898f2abd7f7a274a214d360
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-04-27 09:09:41 -0700

    Improve storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError as e:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,42 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object based on the class and id"""
+        # check if id is a string and if cls is in classes
+        if isinstance(id, str) and cls in classes.values():
+            # call the all method and pass in the class name
+            # to retrieve the objects of that class
+            retrieve = self.all(cls)
+            # loop through the retrieved objects
+            for key, value in retrieve.items():
+                # check if the id from argument matches the
+                # id of any objects found
+                if key.split('.')[1] == id:
+                    return value
+        # if there is no match or cls is none, or id is not a
+        # string, return none
+        return
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        occurence = None
+        if cls:
+            if cls in classes.values():
+                # user: the name of the class to be retrieved
+                all_objs = self.all(cls)
+                # check if all_objs has values
+                if all_objs:
+                    # if all_objs has values, call len method to
+                    # count its objects
+                    occurence = len(all_objs)
+            else:
+                return
+        # for when cls is none
+        else:
+            # retrieve all objects of all classes
+            all_objs = self.all()
+            if all_objs:
+                occurence = len(all_objs)
+        return occurence
