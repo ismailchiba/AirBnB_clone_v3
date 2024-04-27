db6bb9fe6c9e6be4303aa029bb3b38f12219695b
Tunzale Imanova <114104944+Tunzale1@users.noreply.github.com>
2024-04-27 23:44:17 +0400

    cors handled

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
+
 """"run app with Flask framework"""
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
-@app.errorhandler(404)
-def not_found(e):
-    """404 not found"""
-    return jsonify({"error": "Not found"}), 404
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """shutdown"""
+def teardown(exception):
+    """Closes the storage"""
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 page"""
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=int(port), threaded=True)
