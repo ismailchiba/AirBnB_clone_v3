f23c2042d375951ad0f7453cf4788f62b23079a7
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-27 16:43:36 -0700

    state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,7 +15,9 @@ def get_states():
     return jsonify(x)
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route(
+    '/states/<string:state_id>', methods=['GET'], strict_slashes=False
+)
 def get_state(state_id):
     """get a state"""
     state = storage.get(State, str(state_id))
@@ -56,7 +58,9 @@ def create_state():
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route(
+    '/states/<string:state_id>', methods=['PUT'], strict_slashes=False
+)
 def update_state(state_id):
     """update a state"""
     data = request.get_json()
