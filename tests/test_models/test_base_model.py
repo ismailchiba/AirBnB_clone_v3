62b75392198aedc2976cd38f0ddac519a3f169de
Mariam <kelanimaryam1234@gmail.com>
2024-04-26 19:38:14 -0700

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -158,17 +158,3 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(old_created_at, new_created_at)
         self.assertTrue(mock_storage.new.called)
         self.assertTrue(mock_storage.save.called)
-
-    def test_code_under_deserialization(self):
-        """Test deserialized of BaseModel instances from dictionaries"""
-        data = {
-                "id": "456",
-                "created_at" = "2024-04-26T21:05:53.291487",
-                "updated_at" = "2024-04-26T21:05:53.291487",
-                "name": "MariamObject",
-                "number": "32",
-                }
-        role_inst = BaseModel.from_dict(data)
-        self.assertEqual(role_inst.id, "456",
-        self.assertEqual(role_inst.name, "MariamObject"),
-        self.assertEqual(role_inst.number, "32")
