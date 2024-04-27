fd70de4ddc28fcd4ef43adeb2bf89e27734f3156
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-27 16:42:09 -0700

    state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,8 +6,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, request, abort, make_response
 from models import storage
 from models.state import State
-from datetime import datetime
-import uuid
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -17,24 +15,26 @@ def get_states():
     return jsonify(x)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """get a state"""
-    state = storage.get(State, state_id)
+    state = storage.get(State, str(state_id))
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
 
 @app_views.route(
-    '/states/<state_id>', methods=['DELETE'], strict_slashes=False
+    '/states/<string:state_id>',
+    methods=['DELETE'],
+    strict_slashes=False
 )
 def delete_state(state_id):
     """delete a state"""
-    state = storage.get(State, state_id)
+    state = storage.get(State, str(state_id))
     if state is None:
         abort(404)
-    state.delete()
+    storage.delete(state)
     storage.save()
     return jsonify({}), 200
 
@@ -56,20 +56,20 @@ def create_state():
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """update a state"""
     data = request.get_json()
-    if not data:
+    if data is None:
         return make_response(
             jsonify({"error": "Not a JSON"}), 400
         )
-    state = storage.get(State, state_id)
+    state = storage.get(State, str(state_id))
     if state is None:
         abort(404)
     ignore_keys = ['id', 'created_at', 'updated_at']
     for k, v in data.items():
         if k not in ignore_keys:
             setattr(state, k, v)
-    storage.save()
+    state.save()
     return jsonify(state.to_dict()), 200
