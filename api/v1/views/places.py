d9c13ca4d72a0275729e15ea03b9386b887da4e5
Prudence Wambui <wambuiprudy@gmail.com>
2024-04-27 17:50:10 +0300

    created new views of places and review

---
+++ api/v1/views/places.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+"""
+"""
+from flask import abort, jsonify, request
+from models.city import City
+from models.place import Place
+from models.user import User
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+def get_places_by_city(city_id):
+    """
+    Retrieves the list of all Place objects of a City: GET /api/v1/cities/<city_id>/places
+    If the city_id is not linked to any City object, raise a 404 error
+    """
+    city = storage.get(City, city_id)
+    if not city:
+         abort(404)
+
+    places = [place.to_dict() for place in city.places]
+    return jsonify(places)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def get_places(place_id):
+    """
+    Retrieves a Place object. : GET /api/v1/places/<place_id>
+    If the place_id is not linked to any Place object, raise a 404 error
+    """
+    place = storage.get(Place, place_id)
+    if place:
+        return jsonify(place.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+def delete_place(place_id):
+    """
+    Deletes a Place object: DELETE /api/v1/places/<place_id>
+    If the place_id is not linked to any Place object, raise a 404 error
+    Returns an empty dictionary with the status code 200
+    """
+    place = storage.get(Place, place_id)
+    if place:
+        storage.delete(place)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+def create_place(city_id):
+    """
+    Creates a Place: POST /api/v1/cities/<city_id>/places
+    You must use request.get_json from Flask to transform the HTTP request to a dictionary
+    If the city_id is not linked to any City object, raise a 404 error
+    If the HTTP request body is not valid JSON, raise a 400 error with the message Not a JSON
+    If the dictionary doesn’t contain the key user_id, raise a 400 error with the message Missing user_id
+    If the user_id is not linked to any User object, raise a 404 error
+    If the dictionary doesn’t contain the key name, raise a 400 error with the message Missing name
+    Returns the new Place with the status code 201
+    """
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+
+    data = request.get_json()
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    
+    user = storage.get(User, data['user_id'])
+    if not user:
+        abort(404)
+
+    data['city_id'] = city_id
+    place = Place(**data)
+    place.save()
+    return jsonify(place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def update_place(place_id):
+    """
+    Updates a Place object: PUT /api/v1/places/<place_id>
+    If the place_id is not linked to any Place object, raise a 404 error
+    You must use request.get_json from Flask to transform the HTTP request to a dictionary
+    If the HTTP request body is not valid JSON, raise a 400 error with the message Not a JSON
+    Update the Place object with all key-value pairs of the dictionary
+    Ignore keys: id, user_id, city_id, created_at and updated_at
+    Returns the Place object with the status code 200
+    """
+    place = storage.get(Place, place_id)
+    if place:
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+        data = request.get_json()
+        ignore_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(place, key, value)
+
+        place.save()
+        return jsonify(place.to_dict()), 200
+    else:
+        abort(404)
---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+"""
+"""
+from flask import abort, jsonify, request
+from models.review import Review
+from models.place import Place
+from models.user import User
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
+def get_reviews_of_place(place_id):
+    """
+    Retrieves the list of all Review objects of a Place: GET /api/v1/places/<place_id>/reviews
+    If the place_id is not linked to any Place object, raise a 404 error
+    """
+    place = storage.get(Place, place_id)
+    if not place:
+         abort(404)
+
+    reviews = [review.to_dict() for review in place.reviews]
+    return jsonify(reviews)
+
+
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+def get_review(review_id):
+    """
+    Retrieves a Review object. : GET /api/v1/reviews/<review_id>
+    If the review_id is not linked to any Review object, raise a 404 error
+    """
+    review = storage.get(Review, review_id)
+    if review:
+        return jsonify(review.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'], strict_slashes=False)
+def delete_review(review_id):
+    """
+    Deletes a Review object: DELETE /api/v1/reviews/<review_id>
+    If the review_id is not linked to any Review object, raise a 404 error
+    Returns an empty dictionary with the status code 200
+    """
+    review = storage.get(Review, review_id)
+    if review:
+        storage.delete(review)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
+def create_review(place_id):
+    """
+    Creates a Review: POST /api/v1/places/<place_id>/reviews
+    You must use request.get_json from Flask to transform the HTTP request to a dictionary
+    If the place_id is not linked to any Place object, raise a 404 error
+    If the HTTP body request is not valid JSON, raise a 400 error with the message Not a JSON
+    If the dictionary doesn’t contain the key user_id, raise a 400 error with the message Missing user_id
+    If the user_id is not linked to any User object, raise a 404 error
+    If the dictionary doesn’t contain the key text, raise a 400 error with the message Missing text
+    Returns the new Review with the status code 201
+    """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+
+    data = request.get_json()
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    if 'text' not in data:
+        abort(400, 'Missing text')
+    
+    user = storage.get(User, data['user_id'])
+    if not user:
+        abort(404)
+
+    data['place_id'] = place_id
+    review = Review(**data)
+    review.save()
+    return jsonify(review.to_dict()), 201
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
+def update_review(review_id):
+    """
+    Updates a Review object: PUT /api/v1/reviews/<review_id>
+    If the review_id is not linked to any Review object, raise a 404 error
+    You must use request.get_json from Flask to transform the HTTP request to a dictionary
+    If the HTTP request body is not valid JSON, raise a 400 error with the message Not a JSON
+    Update the Review object with all key-value pairs of the dictionary
+    Ignore keys: id, user_id, place_id, created_at and updated_at
+    Returns the Review object with the status code 200
+    """
+    review = storage.get(Review, review_id)
+    if review:
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+        data = request.get_json()
+        ignore_keys = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
+        for key, value in data.items():
+            if key not in ignore_keys:
+                setattr(review, key, value)
+
+        review.save()
+        return jsonify(review.to_dict()), 200
+    else:
+        abort(404)
