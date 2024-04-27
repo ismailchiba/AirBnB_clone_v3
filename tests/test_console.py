491b92a4e8c6a87bbd0f33d0afefe7363fadfbc0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-27 15:43:40 +0300

    Add tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -59,7 +59,7 @@ class TestConsoleDocs(unittest.TestCase):
 
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
-        pep8s = pycodestyle.StyleGuide(quiet=True)
+        pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
