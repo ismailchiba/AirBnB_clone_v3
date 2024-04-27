b7d2f1b530f0834e325bb149dbc9c0c5674c5415
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-26 22:33:03 -0700

    test ok

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
 from models import storage
 
 
@@ -11,3 +11,16 @@ from models import storage
 def status():
     """status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """Create an endpoint that retrieves the number of each object"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"), 
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
