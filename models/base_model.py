f32a0be6b89f7dd5dd493179d9bf250901e1c15d
Agwu Emmanuel <emmanuelaagwu89@gmail.com>
2024-04-27 06:15:21 +0100

    changes on the branch

--- models/base_model.py
+++ models/base_model.py
@@ -23,8 +23,8 @@ class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, default=datetime.utcnow)
+        created_at = Column(DateTime, default=datetime.today())
+        updated_at = Column(DateTime, default=datetime.today())
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -35,16 +35,16 @@ class BaseModel:
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
             else:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.today()
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.today()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.today()
             self.updated_at = self.created_at
 
     def __str__(self):
@@ -54,8 +54,8 @@ class BaseModel:
 
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
+        self.updated_at = datetime.today()
+        # models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ Retrieves one object according to class name and id"""
+
+        if cls and id:
+            fetch_key = "{}.{}".format(cls, id)
+            all_objects = self.all(cls)
+            return all_objects.get(fetch_key)
+        return None
+
+    def count(self, cls=None):
+        """ Returns the number of all objects in storage """
+        all_obj = self.all(cls)
+        return (len(all_obj))
