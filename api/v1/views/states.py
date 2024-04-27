0fbaa6c6aeecc44dd26672e677bb1d9efa76440b
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-04-27 07:49:25 -0700

    state blueprint

---
+++ api/v1/views/states.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+    State- Blueprint Module
+"""
+
+from flask import jsonify, make_response, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states", methods=['GET'], strict_slashes=False)
+def all_states():
+    """Return all objects of states"""
+    objs = storage.all(State)
+    return jsonify([obj.to_dict() for obj in objs.values()])
+
+
+@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Return state with in States with an Id"""
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    return(jsonify(obj.to_dict()))
+
+
+@app_views.route("/states/<state_id>", methods=['DELETE'],
+                 strict_slashe=False)
+def delete_state(state_id):
+    """get a state by id and delete the state"""
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    obj.delete()
+    storage.save()
+    return make_response(jsonify({}) 200)
+
+
+@app_views.route("/states", methods=['POST'], strict_slashes=False)
+def add_state():
+    """creates a new state"""
+    obj = request.get_json()
+    if not obj:
+        abort(400, "Not a JSON")
+    if 'name' not in obj:
+        abort(400, "Missing name")
+    new_state = State(**obj)
+    storage.new(new_state)
+    storage.save()
+    return make_response(jsonify(new_state), 201)
+
+
+@app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """update a state record"""
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
+
+    for key, value in req.items():
+        if key is not ['id', 'created_at', 'updated_at']:
+            setattr(obj, key, value)
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 200)
