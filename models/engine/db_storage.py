645c2a78d855a0af45fe7860ff635483c2ad34f9
Akpevwe <essiakpevwepraise@gmail.com>
2024-04-27 23:35:35 +0100

    added the close and get function

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,29 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+        
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if(value.id == id):
+                return value
+
+        return None
+    
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given
+        class name. If no name is specified, returns the count of
+        all objects in storage
+        """
+        if cls is None:
+            return len(models.storage.all())
+        else:
+            return len(models.storage.all(cls))
