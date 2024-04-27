b3d2c87ec50c2c2376cbbbfc2baa0180658ed288
Method Mkoma <gershommethod02@gmail.com>
2024-04-27 08:53:49 +0300

    Task 3: Fixing

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from . import app_views
 from flask import jsonify
 
 
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ the ok status """
     return jsonify({'status': "OK"})
