1a40284d0d66feb2b7052b10d21778ca0b3e617e
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-04-28 01:00:02 +0200

    [Update] - Altered with app,py

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,10 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        hostname = '0.0.0.0'
+    if not port:
+        port = 5000
     app.run(host=host, port=port, threaded=True)
