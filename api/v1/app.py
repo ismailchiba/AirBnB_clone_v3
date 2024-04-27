0a619373b99103e38ac61ffccb8e1981281b2a95
Hamid Hamidbayli <hemidbeyli2020@gmail.com>
2024-04-27 15:47:07 +0400

    Added cors conf

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,10 @@ from api.v1.views import app_views
 from flask import Flask
 from models import storage
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
