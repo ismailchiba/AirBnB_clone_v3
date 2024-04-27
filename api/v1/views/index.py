8959a80cfba10cf28e5d9154a5717f9e1d8bbf48
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-27 10:38:46 +0300

    RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """This module defines routes for the status and statistics of the API."""
 
-from api.v1.views import app_views
-from flask import jsonify, Flask
 from models import storage
+from flask import Flask
+from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """Returns the status of the API.
 
