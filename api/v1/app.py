9af6679f493b8e3d36d87fd66218cbce54687717
abdelrhman892 <abdoayman@gmail.com>
2024-04-27 15:40:30 -0700

    fix

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Flask server (variable app)
+"""
+
+
+from flask import Flask
+from models import storage
+from os import getenv
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def downtear(self):
+    '''Status of your API'''
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3xx
+'''api status'''
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
