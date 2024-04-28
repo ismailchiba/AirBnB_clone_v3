79e24c107db4ab9aa66dca4dd796542b9cface60
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-28 17:28:04 +0200

    This module defines a class to manage file storage for hbnb clone.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,6 +40,7 @@ class FileStorage:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
