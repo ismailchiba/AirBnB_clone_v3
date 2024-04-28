5e8e443b17e24471097f60bffb9f31dd82b759bd
Yosrahabouria <yosrahabouria@gmail.com>
2024-04-28 06:08:08 -0700

    correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Flask server (variable app)
 """
-
+Flask server (variable app)
+"""
 
 from flask import Flask, jsonify
 from models import storage
@@ -12,23 +12,18 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
-
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def teardown_appcontext(exception):
+    """Closes the database connection at the end of request"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
+def not_found(error):
+    """Handles 404 errors by returning a JSON response."""
     return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-        app.run(host=host, port=port, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
