e88dd6c05a52689118857ce617e08224af5fdabd
Kdamzy <kolajidedamilare@gmail.com>
2024-04-28 19:54:27 +0100

    updated base model

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,9 @@ Contains class BaseModel
 """
 
 from datetime import datetime
+import json
 import models
-from os import getenv
+import os
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -58,7 +59,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, save_fs=None):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,6 +69,9 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if save_fs is None:
+            if "password" in new_dict:
+                del new_dict["password"]
         return new_dict
 
     def delete(self):
