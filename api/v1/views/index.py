e44d1e62ba268d865df4308b3b8c28e6386fb71e
Tocci997 <duet997@gmail.com>
2024-04-28 13:59:25 -0700

    restful api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """create flask app"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -9,3 +10,18 @@ def api_status():
     """route thst returns a JSON status"""
     response = {'status': "OK"}
     return jsonify(response)
+
+
+@app_views.route('/stats')
+def get_stats():
+    """ retrieves the number of each objects by type"""
+    stats = {
+            'amenities': storage.count('Amenities'),
+            'cities': storage.count('Cities'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'user': storage.count('User'),
+    }
+
+    return jsonify(stats)
