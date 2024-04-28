2bf922e42005c7541a79582ca3fd76bf52493a5f
AMR BY <bouyoussefamr@gmail.com>
2024-04-28 10:50:34 +0000

    mandatory

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,25 @@
 """return JSON """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
 
 
 @app_views.route('/status')
 def status():
     stat = {'status': 'OK'}
     return jsonify(stat)
+
+
+@app_views.route('/stats')
+def stats():
+    objs_num = {"states": storage.count(State), "cities": storage.count(City),
+                "reviews": storage.count(Review), "users": storage.count(User),
+                "places": storage.count(Place),
+                "amenities": storage.count(Amenity)}
+    return jsonify(objs_num)
