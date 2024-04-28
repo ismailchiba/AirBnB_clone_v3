ad4daec9548e395324f2bdb90c6948c0b9edfc9a
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-04-28 11:20:53 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """index"""
 from api.v1.views import app_views
 from flask import jsonify
@@ -14,12 +15,10 @@ classes = {"users": "User", "places": "Place", "states": "State",
            "cities": "City", "amenities": "Amenity",
            "reviews": "Review"}
 
-
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def status():
     ''' routes to status page '''
     return jsonify({'status': 'OK'})
-
 
 @app_views.route('/stats', methods=['GET'])
 def count():
