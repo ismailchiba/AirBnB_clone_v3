22e3ea0be5233895cf7f4fcf1aad223b364b9e94
Method Mkoma <gershommethod02@gmail.com>
2024-04-29 00:34:35 +0300

    Task 04: New Upload

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@
 
 from . import app_views
 from flask import jsonify
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -12,7 +19,15 @@ def status():
     return jsonify({'status': "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """ the stats count """
-    return jsonify(storage.count())
+    counts = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(counts)
