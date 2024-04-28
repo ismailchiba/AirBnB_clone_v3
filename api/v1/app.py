5777d0f55b6ee88b962d6c3421f206283a59232c
emarvelm <marvelousegbe19@gmail.com>
2024-04-28 10:27:15 +0100

    pycodestyle resolved

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-
-from flask import Flask
-from flask import jsonify
-from models import storage
+"""
+This module contains the principal application
+"""
 from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+from os import getenv
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def close_storage():
+def close_db(obj):
     """
     Close the storage when the application context ends.
 
     Args:
-        exception (Exception): An optional exception that occurred during request handling.
+        exception (Exception): An optional exception
+        that occurred during request handling.
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    from os import environ
 
     # Get the host from the environment variable or use default value
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+
     # Get the port from the environment variable or use default value
-    port = int(environ.get('HBNB_API_PORT', 5000))
+    port = getenv('HBNB_API_PORT', default=5000)
 
     # Run the Flask app with the specified host, port, and threaded=True
-    app.run(host=host, port=port, threaded=True)
+    app.run(host, int(port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,6 @@ This module contains endpoint(route) status
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
-"""Create an instance of Blueprint with the URL prefix /api/v1"""
 
 @app_views.route('/status', strict_slashes=False)
 def status():
