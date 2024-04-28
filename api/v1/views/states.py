510b087dc1d2d25307154ab35c47ecb48f2a48b8
Soukaina111 <basmahajar@gmail.com>
2024-04-28 15:50:24 -0700

    APIII40

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' Let's create a State view'''
 
-from flask import Flask , abort , request
+from flask import abort, request, jsonify
 from api.v1.views import app_views
 from models.state import State
 from models import storage
@@ -16,7 +16,7 @@ def retrieve_states():
     return jsonify([state.to_dict() for state in states.values()])
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', strict_slashes=False)
 def get_state_by_id(state_id):
     """
     Retrieves a State object by its ID and returns it as a JSON response.
@@ -43,7 +43,8 @@ def create_state():
     return jsonify(state.to_dict()), 201
 
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_state(state_id):
     """
     Updates a State object by its ID with the data from the JSON request body.
@@ -62,7 +63,8 @@ def update_state(state_id):
     return jsonify(state.to_dict()), 200
 
 
-@app_views.route('/states/<string:state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """
     Deletes a State object by its ID.
