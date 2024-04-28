0a64ec28f4bf7dd24914166548f45bd3d492481c
Mohammed Ahmed <ma2286572@gmail.com>
2024-04-28 05:54:25 +0000

    Disable the debug mode

--- api/v1/app.py
+++ api/v1/app.py
@@ -37,4 +37,4 @@ def handle_request_error(exception):
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
