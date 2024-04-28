be0db861222c1c70915c0c03bcc156f0cca83bed
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-04-28 19:13:19 +0200

    [Update] - Added CORS function in api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,10 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 
