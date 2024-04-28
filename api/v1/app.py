c54564f7035ee9646b363a942f1873efbcc4db12
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-28 22:22:04 +0300

    Make files executable and Add documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,11 @@ starts a Flask web application
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
+host = getenv("HBNB_API_HOST")
+port = getenv("HBNB_API_PORT")
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -17,9 +20,12 @@ def teardown_db(exception):
     storage.close()
 
 
-def launch(host="0.0.0.0", port="5000"):
+def launch(host=None, port=None):
     """App Launcher"""
-    app.run(host, port, threaded=True)
+    # Set default values if none provided
+    host = host or "0.0.0.0"
+    port = port or "5000"
+    app.run(host=host, port=port, threaded=True)
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" Module for status"""
 from api.v1.views import app_views
 
 
