24676227df85b499a772be927aca11aca4f8cd34
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-28 20:19:05 +0300

    Route

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+def launch(host="0.0.0.0", port="5000"):
+        app.run(host, port, threaded=True)
+
+
+if __name__ == "__main__":
+     launch()
---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+from api.v1.views import app_views
+
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """returns status"""
+    return {
+  "status": "OK"
+}
