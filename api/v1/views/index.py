3f15d3e347cf71ada3c117d1d79a364d15fdd0e9
emarvelm <marvelousegbe19@gmail.com>
2024-04-28 09:58:21 +0100

    added docstring

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,13 @@
-from flask import jsonify
+#!/usr/bin/python3
+"""
+This module contains endpoint(route) status
+"""
 from api.v1.views import app_views
+from flask import jsonify
 
 """Create an instance of Blueprint with the URL prefix /api/v1"""
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """
     Returns a JSON response with the status "OK".
