6d766ef83a682532aa4050735e832dfcdfc44419
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-04-28 11:28:04 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def status():
     """
     Returns a JSON status
