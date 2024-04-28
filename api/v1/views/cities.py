2904f41afbbe95dcd9a2f4dfb53f6b5ca45d3ba7
Omar-Algassim <omarggg2@gamil.com>
2024-04-28 18:11:29 +0300

    retrive city related to state

---
+++ api/v1/views/cities.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""index """
+
+from models import storage
+from flask import jsonify, request, abort
+from api.v1.views import app_views
+from models.city import City
+from models.state import State
+
+
+@app_views.route("states/<state_id>/cities", methods=['GET', 'POST'])
+def citys_without_id(state_id=None):
+    """Create a new city or return all the cities"""
+    city = storage.get(State, state_id)
+    if city is None:
+        abort(404)
+    if request.method == 'GET':
+        cities_list = []
+        cities_dict = storage.all(City)
+        for city in cities_dict.values():
+            if city.state_id == state_id:
+                cities_list.append(city.to_dict())
+        return jsonify(cities_list)
+
+    if request.method == 'POST':
+        json = request.get_json()
+        if json is None:
+            abort(400, "Not a JSON")
+        if json.get('name') is None:
+            abort(400, "Missing name")
+        city = City(**json)
+        city.save()
+        return jsonify(city.to_dict()), 201
+
+
+@app_views.route("cities/<city_id>", methods=['GET', 'PUT', 'DELETE'])
+def citys_with_id(city_id=None):
+    """Perform READ UPDATE DELETE operations on a city object"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(city.to_dict())
+
+    if request.method == 'DELETE':
+        city.delete()
+        del city
+        return jsonify({})
+
+    if request.method == 'PUT':
+        json = request.get_json()
+        if json is None:
+            abort(400, "Not a JSON")
+        json.pop('id', None)
+        json.pop('created_at', None)
+        json.pop('updated_at', None)
+
+        for k, v in json.items():
+            setattr(city, k, v)
+        city.save()
+        return jsonify(city.to_dict())
