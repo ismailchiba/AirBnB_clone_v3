10284783c6ac36e79578f631cbf1983a4dd98a10
Nelamin2 <nidalelamin@gmail.com>
2024-04-28 16:53:18 -0700

    tasks

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,37 +4,37 @@ from models.state import State
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_all_states():
+def get_states():
     """
     Retrieves the list of all State objects
     """
-    retr_states = storage.all(State).values()
-    acc_states = []
-    for state in retr_states:
-        acc_states.append(state.to_dict())
-    return jsonify(acc_states)
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """ Retrieves a specific State """
-    spf_state = storage.get(State, state_id)
-    if not spf_state:
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    else:
-        return jsonify(spf_state.to_dict())
+    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
-    """ Deletes a State Object"""
-
+    """
+    Deletes a State Object
+    """
     state = storage.get(State, state_id)
-
     if not state:
         abort(404)
     storage.delete(state)
@@ -44,11 +44,15 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
-    """Creates a new State"""
+    """
+    Creates a State
+    """
     if not request.get_json():
         abort(400, description="Not a JSON")
+
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
+
     data = request.get_json()
     instance = State(**data)
     instance.save()
@@ -57,17 +61,15 @@ def post_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
-    """Updates a State"""
+    """
+    Updates a State
+    """
     state = storage.get(State, state_id)
-
     if not state:
         abort(404)
-
     if not request.get_json():
         abort(400, description="Not a JSON")
-
     ignore = ['id', 'created_at', 'updated_at']
-
     data = request.get_json()
     for key, value in data.items():
         if key not in ignore:
