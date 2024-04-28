1fbb369e4f17f7100b9df7acb151ea5e0f22d441
Mr-Michael-dev <michael.oyedepo@gmail.com>
2024-04-28 13:27:33 +0100

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """Flask app for my API """
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not found'}), 404
 
 
 @app.teardown_appcontext
