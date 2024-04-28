7d6c6c3e4b86a656efa90e68e5ccedc510f3feef
ZobaOkafor <judithokafor30@gmail.com>
2024-04-27 17:08:58 -0700

    Search

--- models/base_model.py
+++ models/base_model.py
@@ -67,7 +67,7 @@ class BaseModel:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]i
+            del new_dict["_sa_instance_state"]
         return new_dict
 
     def delete(self):
