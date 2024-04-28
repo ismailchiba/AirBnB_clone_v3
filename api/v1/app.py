58f6b17894e69f551b1ba6ed8495ee947c661f85
O-G-W-A-L <ogwalja04@gmail.com>
2024-04-28 14:36:59 -0700

    not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,29 @@
 from models import storage
 from flask import Flask, jsonify
 import os
-from os import getenv
+from flask_cors import CORS
 from api.v1.views import app_views
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+
+@app.teardown_appcontext
+def close_app(exc):
+    """when the app is close"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(e):
+    """Return 404"""
+    data = {"error": "Not found"}
+    return jsonify(data), 404
+
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', 5000)
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ.get('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
