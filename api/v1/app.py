b23212f6996059d4cdf03a8463351471e2f327ab
lolosaisa <lwamaitha3924@gmail.com>
2024-04-29 01:14:47 +0300

    connection refused

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,46 +2,18 @@
 """
 app
 """
-
-from flask import Flask, jsonify
-
-from flask_cors import CORS
 from os import getenv
-
-
-from api.v1.views import app_views
+from flask import Flask
 from models import storage
+from api.v1.views import app_views
 
-
+#creating an instance of flask
 app = Flask(__name__)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
-
-@app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-    """
-    storage.close()
-
-
-@app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
-
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    # Getting host and port from environment variables, or using default values
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-index
+created Flask app: app_views
 """
 
 from flask import jsonify
@@ -9,38 +9,14 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=['GET'])
 def status():
     """
     status route
     :return: response with json
     """
-    data = {
-        "status": "OK"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
-
-
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
-def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    #alternative
+    response = {'status':"OK"}
+    return jsonify(response)
+    #return jsonify({"status": "OK"})
+    
