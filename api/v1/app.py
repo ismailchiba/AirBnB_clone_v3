d890d6fb8c798e792a9887f6985ff714351d5621
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-04-28 17:34:25 +0300

    enable cors on all routes

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,10 @@ from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 
