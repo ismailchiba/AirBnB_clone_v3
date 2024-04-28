5790a608938d501e2fb8fbaef6464283509627e1
Michael Mwangi <michaelmichire2016@gmail.com>
2024-04-28 09:46:05 -0700

    Updating DB storage and file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Contains the FileStorage class
-"""
+
+"""Contains the FileStorage class"""
 
 import json
 from models.amenity import Amenity
@@ -25,7 +24,8 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """A fn that returns the dictionary __objects"""
+
         if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
@@ -35,13 +35,15 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """A fn that sets in __objects the obj with key"""
+
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """A fn thatserializes __objects to the JSON file"""
+
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
@@ -49,7 +51,8 @@ class FileStorage:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """A fn that deserializes the JSON file to __objects"""
+
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
@@ -59,7 +62,8 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """A fn that delete obj from __objects if it’s inside"""
+
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
@@ -67,4 +71,30 @@ class FileStorage:
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
+
         self.reload()
+
+    def get(self, cls=None, id=None):
+        """A fn that returns obj based on class name and its ID"""
+
+        if cls is not None and id is not None:
+            for v in self.__objects.values():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    if v.id == id:
+                        return v
+
+        return None
+
+    def count(self, cls=None):
+        """A fn that returns the amount of objects"""
+
+        count = 0
+        if cls is not None and cls in classes:
+            for v in self.__objects.values():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    count += 1
+        else:
+            for i in self.__objects.values():
+                count += 1
+
+        return count
