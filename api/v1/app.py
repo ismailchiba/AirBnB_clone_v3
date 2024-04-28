8cf33c302c34cd456f33d45aa1ffef79200e7f4b
Soukaina111 <basmahajar@gmail.com>
2024-04-28 15:01:09 -0700

    APIII5

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,20 +18,12 @@ def close_store(self):
     '''close the storage'''
     storage.close()
 
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST')
-    port = os.getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
-"""
+
 def get_environment_variable(var_name, default_value):
-    
+    """
     Retrieves the value of an environment variable
     or returns a default value if the variable is not set.
-    
+    """
     return os.getenv(var_name, default_value)
 
 
@@ -39,4 +31,4 @@ if __name__ == "__main__":
     host = get_environment_variable('HBNB_API_HOST', '0.0.0.0')
     port = get_environment_variable('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
-"""
+
