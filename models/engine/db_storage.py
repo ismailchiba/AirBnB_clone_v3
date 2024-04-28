55a4c35b3c0f002a73af1e2a2310093828a12df6
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-04-28 11:52:47 +0100

    task 1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,12 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ A method to retrieve one object """
+        objects = models.storage.all(cls)
+        for key, value in objects.items():
+            temp = cls + '.' + id
+            if temp == key:
+                return value
+        return None
