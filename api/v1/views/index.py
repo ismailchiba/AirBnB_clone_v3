bda3d3619553a87405d9ea6ef483d49d14a4ac4d
Soukaina111 <basmahajar@gmail.com>
2024-04-28 15:09:04 -0700

    APIII3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,20 @@ from models.base_model import BaseModel
 def status():
     ''' shows the status'''
     return jsonify(status='OK')
+
+@app_views.route('/stats', strict_slashes=False)
+def get_statistics():
+    """
+    Endpoint to retrieve statistics about different entities in storage.
+    Returns a JSON response with counts for each entity.
+    """
+    entity_counts = {
+        'states': storage.count(State),
+        'users': storage.count(User),
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review)
+    }
+    return jsonify(entity_counts)
+
