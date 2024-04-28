ae12d9c9d2e403e5c36e4c86316c311c23cffe52
Tocci997 <duet997@gmail.com>
2024-04-28 11:59:59 -0700

    restful api

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,30 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not
+        found
+        """
+        if cls and id:
+            if cls in classes.values() and isinstance(id, str):
+                all_objs = self.all(cls)
+                for key, value in all_objs.items():
+                    if key.split('.')[1] == id:
+                        return value
+            else:
+                return
+        return
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage
+        """
+        if not cls:
+            all_cls_instance = self.all()
+            return len(all_cls_instance)
+        if cls in classes.values():
+            given_cls_instance = self.all(cls)
+            return len(given_cls_instance)
+        if cls not in classes.values():
+            return
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,29 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not
+        found
+        """
+        if cls and id:
+            if cls in classes.values():
+                all_objs = self.all(cls)
+                for value in all_objs.values():
+                    if value.id == id:
+                        return value
+            return
+        return
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage
+        """
+        if not cls:
+            all_cls_instance = self.all()
+            return len(all_cls_instance)
+        if cls in classes.values():
+            given_cls_instance = self.all(cls)
+            return len(given_cls_instance)
+        if cls not in classes.values():
+            return
