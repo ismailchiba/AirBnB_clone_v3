5d0c6b6e3933259be2dc6dc0ac04ea278fc66cd1
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-04-28 13:32:07 +0300

    task: created branch and task 2. Improve storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,13 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class name and its ID, or None if not found."""
+        if cls:
+            return next((obj for obj in self.all(cls).values() if obj.id == id), None)
+        return None
+    
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class name. If no name is passed, returns the count of all objects in storage."""
+        return len(self.all(cls))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,13 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class name and its ID, or None if not found"""
+        if cls:
+            return next((obj for obj in self.all(cls).values() if obj.id == id), None)
+        return None
+
+    def count(self, cls=None):
+        """Returns the count of all objects in storage"""
+        return len(self.all(cls))
