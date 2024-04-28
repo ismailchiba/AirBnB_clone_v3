eddd9b977449fe559539faabd19791d12c7bb3b9
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-04-28 08:56:49 +0100

    added cities api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""" Starts the Blueprint views """
+''' This create a blueprint '''
 from flask import Blueprint
 
-# Create a blueprint named 'app_views' with URL prefix '/api/v1'
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
