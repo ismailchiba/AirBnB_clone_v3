3157ad2b292fd2a74256f577496dc92ad588ff0d
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-04-27 19:14:10 -0700

    state

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,11 +21,15 @@ def teardown_appcontext(self):
 @app.errorhandler(404)
 def error_handler(error):
     """handles 404 error"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(
+        jsonify({'error': 'Not found'}), 404
+    )
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-
-    app.run(host=host, port=port, threaded=True)
+    """run your Flask server"""
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT') if getenv(
+        'HBNB_API_PORT'
+    ) is not None else 5000
+    app.run(host=host, port=int(port), threaded=True)
