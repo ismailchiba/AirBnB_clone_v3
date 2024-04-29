6d0c3426b421066e8926950dc3f2081e41108b66
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-04-29 03:25:18 -0700

    Updated app

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,14 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 
 @app.teardown_appcontext
@@ -22,6 +25,14 @@ def close_db(error):
 def not_found(error):
     """ Error """
     return make_response(jsonify({'error': 'Not Found'}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
