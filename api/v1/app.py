18d205a211ad36f3e50742ebcbc8611efe89d1ea
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-29 04:23:36 -1100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-from flask import flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-from os import getenv
-
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
