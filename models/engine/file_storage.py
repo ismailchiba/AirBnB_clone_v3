c56d90babfb31f4b7d61e062a64278e652336fa0
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-04-29 16:06:12 +0100

    Update file_storage.py
    Added a get(self, cls, id) method that retrieves an object, and a method def count(self, cls=None) that counts the number of objects in the storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ retrieves """
+        if cls in classes.values() and id and type(id) == str:
+            ret_obj = self.all(cls)
+            for key, value in ret_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+    def count(self, cls=None):
+        """ counts objects """
+        objects = self.all(cls)
+        if cls in classes.values():
+            objects = self.all(cls)
+        return len(objects)
