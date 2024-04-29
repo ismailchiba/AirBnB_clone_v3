5dc08d7eb879d466b1af254686a4e678ab51aa3f
Fatma <fatmacivil@hotmail.com>
2024-04-29 15:45:24 +0300

    bb

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -16,7 +16,7 @@ def user_get_all():
     return jsonify(user_list)
 
 
-@app_views.route("/users", methods=["POST"], strict_slashes=False)
+@app_views.route("/users", methods=['POST'], strict_slashes=False)
 def user_create():
     """create user route"""
     user_json = request.get_json(silent=True)
@@ -35,7 +35,7 @@ def user_create():
     return resp
 
 
-@app_views.route("/users/<user_id>",  methods=["GET"], strict_slashes=False)
+@app_views.route("/users/<user_id>",  methods=['GET'], strict_slashes=False)
 def user_by_id(user_id):
     """gets a specific User object by ID"""
 
@@ -47,7 +47,7 @@ def user_by_id(user_id):
     return jsonify(fetched_obj.to_dict())
 
 
-@app_views.route("/users/<user_id>",  methods=["PUT"], strict_slashes=False)
+@app_views.route("/users/<user_id>",  methods=['PUT'], strict_slashes=False)
 def user_put(user_id):
     """updates specific User object by ID"""
     user_json = request.get_json(silent=True)
@@ -55,7 +55,7 @@ def user_put(user_id):
     if user_json is None:
         abort(400, 'Not a JSON')
 
-    fetched_obj = storage.get("User", str(user_id))
+    fetched_obj = storage.get(User, user_id)
 
     if fetched_obj is None:
         abort(404)
@@ -73,7 +73,7 @@ def user_put(user_id):
 def user_delete_by_id(user_id):
     """deletes User by id"""
 
-    fetched_obj = storage.get("User", str(user_id))
+    fetched_obj = storage.get(User, user_id)
 
     if fetched_obj is None:
         abort(404)
