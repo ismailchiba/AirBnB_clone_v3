47372f44fec34b3013467803216cdd17ca597e81
CHEGEBB <chegephil24@gmail.com>
2024-04-29 12:07:26 +0300

    python

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-'''
-Create a Flask app that serves the content of the AirBnB clone v3 RESTful API.
-'''
-from flask import abort, jsonify, make_response, request
+"""This module creates a new view for Amenity objects
+It handles all default RestFul API actions
+And a new route that retrieves the count of all Amenity objects
+"""
+
+
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
+from flask import abort, jsonify, make_response, request
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def amenities():
     """
+    This function creates a new view for Amenity objects
     Retrieves the list of all Amenity objects
+    It handles all default RestFul API actions
     """
     objs = storage.all(Amenity)
     return jsonify([obj.to_dict() for obj in objs.values()])
@@ -21,7 +26,9 @@ def amenities():
                  methods=['GET'], strict_slashes=False)
 def single_amenities(amenity_id):
     """
+    The function retrieves a Amenity object
     Retrieves a Amenity object
+    And returns it in a JSON format
     """
     obj = storage.get(Amenity, amenity_id)
     if not obj:
@@ -29,11 +36,14 @@ def single_amenities(amenity_id):
     return jsonify(obj.to_dict())
 
 
+
 @app_views.route('/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
 def del_amenities(amenity_id):
     """
+    This will delete a Amenity object
     Deletes a Amenity object
+    When a Amenity object is deleted, it returns an empty dictionary
     """
     obj = storage.get(Amenity, amenity_id)
     if not obj:
@@ -47,7 +57,9 @@ def del_amenities(amenity_id):
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def post_amenity():
     """
+    This will create a Amenity object
     Creates a Amenity object
+    Creating a new Amenity object involves actually creating a new Amenity object
     """
     new_amenity = request.get_json()
     if not new_amenity:
@@ -65,7 +77,9 @@ def post_amenity():
                  methods=['PUT'], strict_slashes=False)
 def put_amenity(amenity_id):
     """
+    This will update a Amenity object
     Updates a Amenity object
+    When a Amenity object is updated, it returns the new Amenity object
     """
     obj = storage.get(Amenity, amenity_id)
     if not obj:
