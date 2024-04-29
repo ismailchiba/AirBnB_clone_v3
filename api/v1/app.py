63c5b264a0368728ee4e209288237f98caa4931a
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2024-04-29 05:54:54 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
