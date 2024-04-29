2df9381e7156e08ec9f55c183cc377da42d85ccc
CHEGEBB <chegephil24@gmail.com>
2024-04-29 13:25:58 +0300

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""
+
+"""This module is used to interact with the MySQL database
 Contains the class DBStorage
+And the dictionary classes
+It will be imported in the __init__.py module
 """
 
-import models
-from models.amenity import Amenity
-from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
 from os import getenv
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,12 +24,18 @@ classes = {"Amenity": Amenity, "City": City,
 
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """This class will be used to interact with the MySQL database
+    It will be used to store and retrieve objects from the database
+    interacts with the MySQL database
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        """Instantiate a DBStorage object"""
+        """This creates an instance of the DBStorage class
+        Instantiate a DBStorage object
+        And creates the engine and session attributes
+        """
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
@@ -41,7 +50,10 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """This method returns a dictionary of objects
+        It returns a dictionary of objects of the class name passed
+        query on the current database session
+        """
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
@@ -52,20 +64,28 @@ class DBStorage:
         return (new_dict)
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """This will add the object to the current database session
+        add the object to the current database session
+        It will add the object to the current database session
+        """
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """This will commit all changes of the current database session
+        commit all changes of the current database session
+        It will commit all changes of the current database session
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """This will delete from the current database session
+        delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """This will reload data from the database
+        reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
@@ -77,6 +97,8 @@ class DBStorage:
 
     def get(self, cls, id):
         """
+        This will retrieve an object based on the class name and its ID
+        It then returns the object based on the class name and its ID
         Returns the object based on the class name and its ID, or None if not
         found
         """
@@ -85,7 +107,8 @@ class DBStorage:
         return None
 
     def count(self, cls=None):
-        """
+        """This will return the number of objects in storage
+        And the number of objects in storage matching the class name
         Returns the number of objects in storage matching the given class name.
         If no name is passed, returns the count of all objects in storage.
         """
