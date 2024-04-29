aed3428b630aee574aed743470020b5eab20ab96
AMR BY <bouyoussefamr@gmail.com>
2024-04-29 22:11:20 +0000

    mandatory

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,12 +13,14 @@ from models.user import User
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """status of an API"""
     stat = {'status': 'OK'}
     return jsonify(stat)
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
+    """all states"""
     objs_num = {"states": storage.count(State), "cities": storage.count(City),
                 "reviews": storage.count(Review), "users": storage.count(User),
                 "places": storage.count(Place),
