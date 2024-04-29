f2e4f65351e3d0c513dd701d4bb3345c0c2a420a
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-29 14:48:13 +0200

    A script that starts a Flask web application.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,11 +9,6 @@ from flask import Blueprint
 app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
-<<<<<<< HEAD
-from api.v1.views.cities import *
-from api.v1.views.users import *
-
-=======
 from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
@@ -21,4 +16,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
 from api.v1.views.users import *
->>>>>>> 6b6c4ec97ed5f0e046059b074222b06781a6eaf6
