6c97cf0ffbbe76c73b302b9da33240845fee389e
EstherKiguru <esipraisewambui@gmail.com>
2024-04-29 07:08:17 -0700

    api app

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,12 +10,12 @@ from models import storage
 from api.v1.views import app_views
 
 
-flaskapp = Flask(__name)
+flaskapp = Flask(__name__)
 
 flaskapp.register_blueprint(app_views)
 
 
 if __name__ == '__main__':
     HOST = getenv("HBNB_API_HOST", "0.0.0.0")
-    PORT = int(getenv("HBNB_API_HOST", "0.0.0.0"))
-    app.run(host=HOST, port=PORT, threaded=True)
+    PORT = int(getenv("HBNB_API_PORT", "5000"))
+    flaskapp.run(host=HOST, port=PORT, threaded=True)
