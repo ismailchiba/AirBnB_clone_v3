600bfdf9615cd490d20217073e4c5fa6a435a6a0
CHEGEBB <chegephil24@gmail.com>
2024-04-29 12:46:15 +0300

    state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""
+
+"""This module will create a Flask app that serves the content of the AirBnB clone v3 RESTful API.
 Create a Flask app that serves the content of the AirBnB clone v3 RESTful API.
+It will be imported in the app.py module
 """
+
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
@@ -11,7 +14,9 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def state():
     """
+    This function
     Retrieves the list of all State objects
+    it will be called when the route /states is requested
     """
     objs = storage.all(State)
     return jsonify([obj.to_dict() for obj in objs.values()])
@@ -19,8 +24,9 @@ def state():
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def single_state(state_id):
-    """
+    """This function
     Retrieves a State object
+    It will be called when the route /states/<state_id> is requested
     """
     obj = storage.get(State, state_id)
     if not obj:
@@ -32,7 +38,9 @@ def single_state(state_id):
                  methods=['DELETE'], strict_slashes=False)
 def del_state(state_id):
     """
+    This will dellete a State object
     Deletes a State object
+    It will be called when the route /states/<state_id> is requested
     """
     obj = storage.get(State, state_id)
     if not obj:
@@ -44,8 +52,9 @@ def del_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
-    """
+    """This function
     Creates a State object
+    And will be called when the route /states is requested with the POST method
     """
     new_obj = request.get_json()
     if not new_obj:
@@ -60,8 +69,9 @@ def post_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
-    """
+    """This function
     Updates a State object
+    It will be called when the route /states/<state_id> is requested with the PUT method
     """
     obj = storage.get(State, state_id)
     if not obj:
