4c84e7207cb6f152357518256ac6a12a52e92ce6
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-04-29 05:45:06 -0700

    cross resource api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 """
 import os
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +15,7 @@ app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 app_host = os.getenv("HBNB_API_HOST", "0.0.0.0")
 app_port = int(os.getenv("HBNB_API_PORT", "5000"))
+CORS(app, resources={r"/*": {"origins": app_host}})
 
 
 @app.teardown_appcontext
@@ -25,7 +27,7 @@ def end_session(exception):
 @app.errorhandler(404)
 def not_found(err):
     """custom 404- not found error"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify(error="Not found"), 404)
 
 
 if __name__ == '__main__':
