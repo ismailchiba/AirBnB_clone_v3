cbd2fbe4f4faedaa778f1e76e16ec099096e8cba
GODSPE1 <godspy18@gmail.com>
2024-04-29 22:31:48 +0100

    check for pep8 comliant

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def get_status():
-    """Returns status as OK"""
-    return jsonify({'status':'OK'})
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    '''returns a JSON: "status": "OK"'''
+    return jsonify({"status": "OK"})
