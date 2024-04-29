1606140cdae24c70ad75ea55ccb4e70158057d83
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-29 12:35:09 +0300

    added test cases for get and count methods on both filestorage and dbstorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -68,44 +68,50 @@ test_file_storage.py'])
                             "{:s} method needs a docstring".format(func[0]))
 
 
-# class TestFileStorage(unittest.TestCase):
-#     """Test the FileStorage class"""
+class TestFileStorage(unittest.TestCase):
+    """Test the FileStorage class"""
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_all_returns_dict(self):
+        """Test that all returns the FileStorage.__objects attr"""
+        storage = FileStorage()
+        new_dict = storage.all()
+        self.assertEqual(type(new_dict), dict)
+        self.assertIs(new_dict, storage._FileStorage__objects)
 
-#     def setUp(self):
-#         """Initializing FileStorage class"""
-#         self.storage = FileStorage()
-#         self.user = User()
-#         self.user.id = "user_id"
-#         self.base_model = BaseModel()
-#         self.base_model.id = "base_model_id"
-#         self.storage.new(self.user)
-#         self.storage.new(self.base_model)
-#         self.storage.save()
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_new(self):
+        """test that new adds an object to the FileStorage.__objects attr"""
+        storage = FileStorage()
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = {}
+        test_dict = {}
+        for key, value in classes.items():
+            with self.subTest(key=key, value=value):
+                instance = value()
+                instance_key = instance.__class__.__name__ + "." + instance.id
+                storage.new(instance)
+                test_dict[instance_key] = instance
+                self.assertEqual(test_dict, storage._FileStorage__objects)
+        FileStorage._FileStorage__objects = save
 
-#     def tearDown(self):
-#         """Remove test objects from the storage"""
-#         self.storage.delete(self.user)
-#         self.storage.delete(self.base_model)
-#         self.storage.save()
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
+        storage = FileStorage()
+        new_dict = {}
+        for key, value in classes.items():
+            instance = value()
+            instance_key = instance.__class__.__name__ + "." + instance.id
+            new_dict[instance_key] = instance
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = new_dict
+        storage.save()
+        FileStorage._FileStorage__objects = save
+        for key, value in new_dict.items():
+            new_dict[key] = value.to_dict()
+        string = json.dumps(new_dict)
+        with open("file.json", "r") as f:
+            js = f.read()
+        self.assertEqual(json.loads(string), json.loads(js))
 
-#     def test_get_existing_object(self):
-#         """Test getting an existing object"""
-#         retrieved_user = self.storage.get(User, "user_id")
-#         self.assertIsNotNone(retrieved_user)
-#         self.assertEqual(retrieved_user.id, "user_id")
-#         self.assertIsInstance(retrieved_user, User)
 
-#     def test_get_non_existing_object(self):
-#         """Test getting a non-existing object"""
-#         non_existing_object = self.storage.get(User, "non_existing_id")
-#         self.assertIsNone(non_existing_object)
-
-#     def test_count_all_objects(self):
-#         """Test counting all objects"""
-#         count = self.storage.count()
-#         self.assertEqual(count, 2)
-
-#     def test_count_objects_of_specific_class(self):
-#         """Test counting objects of a specific class"""
-#         count = self.storage.count(User)
-#         self.assertEqual(count, 1)
