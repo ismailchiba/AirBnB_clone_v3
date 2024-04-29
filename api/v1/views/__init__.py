7a1ce880fc9990beabe97ea4423a6d4868179888
SaidLamghari <said.lmg@gmail.com>
2024-04-29 13:17:33 +0100

    api/v1/views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,9 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# package Ajouté h
+# package Ajouté 7. City
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+# 8. Amenity
+from api.v1.views.amenities import *
