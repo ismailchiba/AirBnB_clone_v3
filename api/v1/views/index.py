b2225ecdd12132fd3bc003bfd1de991733ad3ad7
GODSPE1 <godspy18@gmail.com>
2024-04-29 22:26:52 +0100

    update code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,4 +8,4 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def get_status():
     """Returns status as OK"""
-    return jsonify(status='OK')
+    return jsonify({'status':'OK'})
