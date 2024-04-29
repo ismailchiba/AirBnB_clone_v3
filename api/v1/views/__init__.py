8fa28c18606de6264c8dc67f3c209c7af4fd3889
maggie-njeri <mkiri00@gmail.com>
2024-04-29 13:21:22 -0700

    restful

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,4 +8,10 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Wildcard import everything from api.v1.views.index
 # PEP8 will complain about wildcard imports, but it's necessary in this case
 from api.v1.views.index import *
-from api.v1.views.states import *
+from api.v1.views.states import *i
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.users import *
+from api.v1.views.places_amenities import *
