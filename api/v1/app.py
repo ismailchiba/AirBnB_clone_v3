72db5f2f983202da77cb3c7df0a25717f51cf123
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-29 03:21:29 -1100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 from flask import flask
+from flask_cors import CORS
+from os import getenv
 from models import storage
 from api.v1.views import app_views
-import os
 
 app = Flask(__name__)
 
@@ -11,12 +12,12 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def teardown_db(Exception):
+def teardown(Exception):
     storage.close()
 
 
 if __name__ == "__main__":
 
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = getenv("HBNB_API_HOST")
+    port = getenv("HBNB_API_PORT")
     app.run(host=host, port=port, threaded=True)
