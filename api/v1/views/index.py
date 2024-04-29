44b58504ce60622bd3f314bb2b2fdc4e43324ca2
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-29 07:56:49 +0300

    Retrieve the number of each objects by type - Revert

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,46 +11,23 @@ from models.city import City
 from flask import jsonify
 
 
-# Dictionary mapping model classes to their names
-model_classes = {
-    "amenities": storage.count(Amenity),
-    "cities": storage.count(City),
-    "places": storage.count(Place),
-    "reviews": storage.count(Review),
-    "states": storage.count(State),
-    "users": storage.count(User)
-}
-
-
 @app_views.route("/status", methods=["GET"])
 def status():
-    """
-    Returns the status of the application.
-    ---
-    responses:
-      200:
-        description: Status of the application.
-    """
-    return jsonify({"status": "OK"}), 200
+    """returns status"""
+    return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats", methods=["GET"])
 def stats():
     """
-    Retrieves the number of each object by type.
-    ---
-    responses:
-      200:
-        description: Object count by type.
-      500:
-        description: Error occurred, unable to retrieve statistics.
+    Retrieves the number of each objects by type
+    returns stats
     """
-    try:
-        # Use dictionary comprehension to create the stats dictionary
-        stats = {cls_name: storage.count(cls)
-                 for cls_name, cls in model_classes.items()}
-        return jsonify(stats), 200
-    except Exception as e:
-        # Log the exception and return an error response
-        print(f"Error retrieving stats: {e}")
-        return jsonify(error="Unable to retrieve statistics"), 500
+    return jsonify({
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    })
