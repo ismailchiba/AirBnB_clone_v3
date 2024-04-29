5101746a7df815020cd7463fe905a45a7486bb63
Fatma <fatmacivil@hotmail.com>
2024-04-29 14:43:02 +0300

    gg

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,22 +5,18 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users', strict_slashes=False)
+@app_views.route('/users', strict_slashes=False, methods=['GET'])
 def get_users():
     """Retrieves the list of all User objects."""
     users = storage.all(User)
-    user_list = []
-    for user in users.values():
-        user_list.append(user.to_dict())
-    return jsonify(user_list)
+    user_list = [value.to_dict() for value in users.values()]
+    return make_response(jsonify(user_list), 200)
 
 
 @app_views.route('/users/<user_id>',
                  strict_slashes=False, methods=['GET'])
 def get_user(user_id):
     """Retrieves a User object."""
-    if user_id is None:
-        abort(404)
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
@@ -33,12 +29,10 @@ def delete_user(user_id):
     """Deletes a User object."""
     if user_id is None:
         abort(404)
-    user = storage.get(User, user_id)
-    if user is None:
-        abort(404)
-    user.delete()
+
+    user_id.delete()
     storage.save()
-    return jsonify({})
+    return make_response({}, 200)
 
 
 @app_views.route('/users', strict_slashes=False, methods=['POST'])
@@ -60,16 +54,18 @@ def create_user():
                  strict_slashes=False, methods=['PUT'])
 def update_user(user_id):
     """Updates a User object."""
-    if user_id is None:
+    user_by_id = storage.get(User, user_id)
+    if not user_by_id:
         abort(404)
-    user = storage.get(User, user_id)
-    if user is None:
-        abort(404)
-    user_json = request.get_json()
-    if user_json is None:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for key, value in user_json.items():
-        if key not in ['id', 'email', 'created_at', 'updated_at']:
-            setattr(user, key, value)
-    user.save()
-    return jsonify(user.to_dict())
+
+    body_request = request.get_json()
+    if not body_request:
+        return make_response("Not a JSON", 400)
+
+    attributes_to_update = ['first_name', 'last_name', 'email', 'password']
+    for attribute in attributes_to_update:
+        setattr(user_by_id, attribute,
+                body_request.get(attribute, getattr(user_by_id, attribute)))
+    storage.save()
+
+    return make_response(jsonify(user_by_id.to_dict()), 200)
