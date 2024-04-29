d1bf8851335d3d3dd592153d27fce2ee3b354038
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-29 23:43:48 +0300

    Remove errors

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,7 +8,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Wildcard import everything from api.v1.views.index
 # PEP8 will complain about wildcard imports, but it's necessary in this case
 from api.v1.views.index import *
-from api.v1.views.states import *i
+from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.places import *
