024d180be1be651cb676a4bc820dafc49457ce55
Thugg55 <tuggeru7@gmail.com>
2024-04-28 18:43:19 -0700

    task 4 done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route(/status)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def get_stats():
+    """ Retrieve the number of objects by type """
+    stats = {}
+    for obj_type in storage.all().keys():
+        count = storage.count(obj_type)
+        stats[obj_type] = count
+    return jsonify(stats)
