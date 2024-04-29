4c1d2829bfa5c4fefd38d8bc69ec5c33fa62f0a1
Soukaina111 <basmahajar@gmail.com>
2024-04-29 11:25:48 -0700

    init87

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,7 @@ from models import storage
 @app_views.route('/status', strict_slashes=False)
 def status():
     ''' shows the status'''
-    return jsonify({'status' :'OK'})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', strict_slashes=False)
