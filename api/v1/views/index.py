ee717361a9e128136b9f19a337917ee40f3bb24a
CHEGEBB <chegephil24@gmail.com>
2024-04-29 12:29:50 +0300

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""
+
+"""This  module will create a flask app that serves the content of the AirBnB clone v3 RESTful API.
 Create a Flask app that serves the content of the AirBnB clone v3 RESTful API.
+It will be imported in the app.py module
 """
 from flask import jsonify
 from api.v1.views import app_views
@@ -9,8 +11,9 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """
+    """This function
     returns the status of the API
+    It will be called when the route /status is requested
     """
     return jsonify(status="OK")
 
@@ -18,7 +21,9 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stat():
     """
+    This function
     retrieves the number of each object by type
+    It will be called when the route /stats is requested
     """
     return jsonify(
         amenities=storage.count('Amenity'),
