e37252de8b4e6ae0462c91505bd0fe8f654f6a06
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-29 10:45:10 +0300

    Create a new view for State objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Wildcard import everything from api.v1.views.index
 # PEP8 will complain about wildcard imports, but it's necessary in this case
 from api.v1.views.index import *
+from api.v1.views import states
