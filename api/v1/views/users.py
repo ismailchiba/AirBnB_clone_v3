3091db27e13a7406e132664b02e4b1be02acacdd
CHEGEBB <chegephil24@gmail.com>
2024-04-29 20:01:52 +0300

    update

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+
 """
+This is the users module.
 Create a Flask app that serves the content of the AirBnB clone v3 RESTful API.
+It does the following:
+- retrieves the list of all User objects
+- retrieves a User object
+- deletes a User object
 """
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
@@ -11,7 +17,9 @@ from models.user import User
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def users():
     """
+    This function
     Retrieves the list of all User objects
+    It returns a list of all User objects
     """
     objs = storage.all(User)
     return jsonify([obj.to_dict() for obj in objs.values()])
@@ -20,7 +28,9 @@ def users():
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def single_user(user_id):
     """
+    This function
     Retrieves a User object
+    It returns a User object
     """
     obj = storage.get(User, user_id)
     if not obj:
@@ -31,7 +41,9 @@ def single_user(user_id):
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def del_user(user_id):
     """
+    This function
     Deletes a User object
+    It returns an empty dictionary
     """
     obj = storage.get(User, user_id)
     if not obj:
@@ -44,6 +56,7 @@ def del_user(user_id):
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def post_user():
     """
+    This function
     Creates a User object
     """
     new_user = request.get_json()
@@ -63,7 +76,9 @@ def post_user():
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
     """
+    This function
     Updates a User object
+    It returns the User object
     """
     obj = storage.get(User, user_id)
     if not obj:
