29fb1ba44e75e66b3e405f5e8e8f94a9f453f3cb
GODSPE1 <godspy18@gmail.com>
2024-04-29 22:09:46 +0100

    create the api folder and moved v1 into it

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""The flask web application"""
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_flask():
+    """closing the sqlalchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app_host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    app_port = os.environ.get('HBNB_API_PORT', '5000')
+    app.run(host=app_host,
+            port=app_port,
+            threaded=True
+            )
