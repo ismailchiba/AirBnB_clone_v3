20baa15c9d17a69c97fd37471cc03f2412db0249
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-04-29 20:16:53 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,59 +1,40 @@
 #!/usr/bin/python3
 """
-app
+API script for AirBnB_clone_v3
 """
-from os import getenv
-from flask import Flask
 
+import os
+from flask import Flask, jsonify, Response
 from flask_cors import CORS
-
-from api.v1.views import app_views
 from models import storage
-
-
-app = Flask(__name__)
-
-
 from api.v1.views import app_views
-from models import storage
-
-
 app = Flask(__name__)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-    """
+def teardown(self):
+    """ handles teardown """
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: returns 404 json
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
-
-app = Flask(__name__)
-
-app.register_blueprint(app_views)
+def page_not_found(e):
+    """ handles 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
 
 
-if __name__ == "__main__":
-    HOST = getenv("HBNB_API_HOST", "0.0.0.0")
-    PORT = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=HOST, port=PORT, threaded=True) 
+if __name__ == '__main__':
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
+
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        port = '5000'
+
+    app.run(host=host, port=port)
