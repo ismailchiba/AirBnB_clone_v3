d09615a286c88dfebbeca0d53d5dab327a36e5f0
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-29 02:17:54 +0000

    task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this area is for description"""
+from api.v1.views import app_views
+from flask import make_response, jsonify
+
+@app_views.route("/status", methods=['GET'])
+def rt_ok():
+    """ return api response status"""
+    return make_response(jsonify({"status": "OK"}))
