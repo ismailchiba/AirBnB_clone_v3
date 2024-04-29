680f29efecca3d5a71973697923e0411954f787f
Nelamin2 <nidalelamin@gmail.com>
2024-04-28 19:08:45 -0700

    tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,12 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_db(error):
