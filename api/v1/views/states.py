4c2e1cb3a7fc11e73c3ce84c96d17c57bd7806ea
DaisyAram <daisyarama03@gmail.com>
2024-04-29 14:41:12 +0000

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models import state
+from models.state import State
 from datetime import datetime
 import uuid
 
