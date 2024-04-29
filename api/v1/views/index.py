dbe898c9e0301b60f713e2a1f71996177395f8a6
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-04-29 13:57:04 +0000

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,28 @@
 
 from api.v1.views import app_views
 from flask import make_response, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_resp():
     """ return api response status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_data():
+    """ retrieves the number of each objects"""
+    cls = [Amenity, City, Place, Review, State, User]
+    loc = ["amenities", "cities", "places", "reviews", "states", "users"]
+    dict_struc = {}
+    for i in range(len(cls)):
+        dict_struc[loc[i]] = storage.count(cls[i])
+
+    return jsonify(dict_struc)
