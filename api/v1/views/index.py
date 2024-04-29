d0cc22508234dd30ac321112d0eab7390f43c2db
Thugg55 <tuggeru7@gmail.com>
2024-04-28 19:08:19 -0700

    work on index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
+"""
+@author: Edogun Peter Putech
+"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, Blueprint
 from models import storage
 
 
-@app_views.route(/status)
-def status():
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """ get status of the route """
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats')
-def get_stats():
-    """ Retrieve the number of objects by type """
-    stats = {}
-    for obj_type in storage.all().keys():
-        count = storage.count(obj_type)
-        stats[obj_type] = count
-    return jsonify(stats)
