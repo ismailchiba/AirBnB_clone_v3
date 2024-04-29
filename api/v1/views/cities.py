9cbde89addd5630eb4ca465e4df35a4f0dd8680c
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-04-29 18:16:44 +0300

    CRUD operations for city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""
+This file handles all the default  RESTFul API
+actions (CRUD)
+"""
+
 from flask import abort, make_response, request
 from models import storage
 from models.city import City
