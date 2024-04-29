6e4b308da5f74155eeb17353d1086d844a426985
Tycee02 <wakilaminat@gmail.com>
2024-04-29 19:05:51 +0100

    api task

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
+"""create blueprint"""
 from flask import Blueprint
-from api.v1.views.index import *
 
-# Creating an instance of Blueprint with URL prefix /api/v1
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Importing everything in the package of api.v1.views.index
-# pylint: disable=wildcard-import
