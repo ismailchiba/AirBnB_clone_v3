8471f4eadde8806234297f3b75e7ad8d245a4f73
lolosaisa <lwamaitha3914@gmail.com>
2024-04-29 17:30:45 +0000

    frustrated

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,39 +2,46 @@
 """
 app
 """
-from os import getenv
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
 
-<<<<<<< HEAD
-#creating an instance of flask
+from flask import Flask, jsonify
+# from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
+
 app = Flask(__name__)
+
+# CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    teardown function
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
 if __name__ == "__main__":
-    # Getting host and port from environment variables, or using default values
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=HOST, port=PORT, threaded=True)
-=======
-#instance f flask
-app = Flask(__name__)
-
- # Registering the app_views blueprint
- #done when you dont to create every function/methods for app
-app.register_blueprint(app_views)
-
- # Method to handle app teardown
-#@app.teardown_appcontext
-#def teardown_db(exception):
- #        storage.close()
-
-if __name__=="__main__":
-    # Running the Flask server with specified host and port
-        host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-        port = int(os.getenv('HBNB_API_PORT', 5000))
-        app.run(host=host, port=port, threaded=True)
-
->>>>>>> storage_get_count
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
 views
-=======
-Created the flask blueprint
->>>>>>> storage_get_count
 """
 
 from flask import Blueprint
 
-<<<<<<< HEAD
-#all the urls created with appviews must use the path /api/v1
-app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
-#from api.v1.views.states import *
-#from api.v1.views.amenities import *
-#from api.v1.views.cities import *
-#from api.v1.views.places import *
-#from api.v1.views.places_reviews import *
-#from api.v1.views.users import *
-#from api.v1.views.places_amenities import *
-=======
-#all the urls we create must include /api/v1
-app_views = Blueprint('app_views',__name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
->>>>>>> storage_get_count
+from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.users import *
+from api.v1.views.places_amenities import *
