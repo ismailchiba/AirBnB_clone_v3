8ae5d4b534e8c6604f1b45f53ee9262ab8936b9e
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-04-29 12:18:47 -1100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,16 +9,17 @@ from os import getenv
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown(Exception):
     storage.close()
 
+@app.errorhandler(404)
+def detect_issue(error):
+    response = show_response(jsonify({'error': 'Not found'}), 404)
+    return response
 
 if __name__ == "__main__":
-
-    host = getenv("HBNB_API_HOST")
-    port = getenv("HBNB_API_PORT")
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=getenv("HBNB_API_PORT", 5000), threaded=True)
