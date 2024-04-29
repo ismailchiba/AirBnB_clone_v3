e932b72ed58846ddbc375154c3998900f73aa616
DaisyAram <daisyarama03@gmail.com>
2024-04-29 12:59:15 +0000

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,17 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
 @app_views.route('/status', methods=['GET'])
