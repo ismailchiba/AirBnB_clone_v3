19b84df60c4d05df16d618051cd1c8c3d624f854
Soukaina111 <basmahajar@gmail.com>
2024-04-29 09:37:29 -0700

    ini1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from flask import Blueprint
-
+from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+"""
 # package Ajouté 7. City
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 # 8. Amenity
 from api.v1.views.amenities import *
-
+"""
