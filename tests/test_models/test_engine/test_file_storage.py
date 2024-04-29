e0130f2c46d67440de1ce6dee411127ee588a925
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-30 00:04:40 +0300

    Edit test for storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -112,13 +112,6 @@ class TestFileStorageDocs(unittest.TestCase):
             reloaded_obj, "No object was loaded after reload."
         )
 
-    # def test_reload_empty(self):
-    #     """ Load from an empty file """
-    #     with open('file.json', 'w') as f:
-    #         pass
-    #     with self.assertRaises(ValueError):
-    #         storage.reload()
-
     def test_reload_from_nonexistent(self):
         """Nothing happens if file does not exist"""
         self.assertEqual(storage.reload(), None)
