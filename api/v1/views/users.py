8614efeebc5149b6b2b19b2990b33e75f1a05bd4
CHEGEBB <chegephil24@gmail.com>
2024-04-29 13:03:49 +0300

    python

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
+"""This module will create a Flask app that serves the content of the AirBnB clone v3 RESTful API.
+it is the users view module
+What it does is that it retrieves the list of all User objects
 """
-Create a Flask app that serves the content of the AirBnB clone v3 RESTful API.
-"""
+
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
@@ -10,8 +13,10 @@ from models.user import User
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def users():
-    """
+    """This function
     Retrieves the list of all User objects
+    It will be called when the route /users is requested
+    And will return a JSON object with all User objects
     """
     objs = storage.all(User)
     return jsonify([obj.to_dict() for obj in objs.values()])
@@ -19,8 +24,9 @@ def users():
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def single_user(user_id):
-    """
+    """This function
     Retrieves a User object
+    It will be called when the route /users/<user_id> is requested
     """
     obj = storage.get(User, user_id)
     if not obj:
@@ -30,8 +36,9 @@ def single_user(user_id):
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def del_user(user_id):
-    """
+    """This function
     Deletes a User object
+    It will be called when the route /users/<user_id> is requested
     """
     obj = storage.get(User, user_id)
     if not obj:
@@ -43,8 +50,9 @@ def del_user(user_id):
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def post_user():
-    """
+    """This function
     Creates a User object
+    it will be called when the route /users is requested with the POST method
     """
     new_user = request.get_json()
     if not new_user:
@@ -62,12 +70,17 @@ def post_user():
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
-    """
+    """This function
     Updates a User object
+    This will be called when the route /users/<user_id> is requested with the PUT method
+    And will return a JSON object with the updated User object
     """
     obj = storage.get(User, user_id)
     if not obj:
-        """abort with 404 status code if obj is None"""
+        """This function
+        abort with 404 status code if obj is None
+        It will be called when the route /users/<user_id> is requested
+        """
         abort(404)
 
     req = request.get_json()
