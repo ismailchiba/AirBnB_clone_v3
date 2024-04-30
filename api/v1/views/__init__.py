e661116aa2b8f5b53f0a8313c179c88d8118a636
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-30 09:28:22 +0300

    RESTful API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,10 @@
 """Status of your API"""
 
 
-from api.v1.views.index import *
-from api.v1.views.states import *
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
