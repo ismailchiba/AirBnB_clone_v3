6416f604894b97e34d1ba24f81653fa5a729d1c9
sami-hamid <samifadlallah21@gmail.com>
2024-04-30 21:24:21 +0400

    re-fixing task-3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""Flask server (variable app)
+"""create Flask app: and register the blueprint app_view to flask instance app.
 """
-
-from flask import Flask, jsonify
-from models import storage
 from os import getenv
+from flask import Flask
+from models import storage
 from api.v1.views import app_views
+from models import amenity
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
-@app.teardown_appcontext
-def teardown_appcontext(self):
-    '''Closes the storage'''
-    storage.close()
-
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return jsonify'''
-    return jsonify(error='Not found'), 404)
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', default=5000))
-    app.run(host=host, port=port, threaded=True)
+    HOST = getenv('HBNB_API_HOST','0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT,5000'))
+    app.run(host=HOST, port=PORT, threaded=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Initialize the views model for the Restful API"""
+"""create Flask app blueprint .
+"""
 from flask import Blueprint
+
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
