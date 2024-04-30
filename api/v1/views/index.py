7b3996401c5acb4f7abaf257b6f7447ffb3663f6
Ayormitan <kolawoleabaz97@gmail.com>
2024-04-30 10:17:03 -0700

    endpoint that retrieves the number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,19 +15,18 @@ def api_status():
     response = {'status': "OK"}
     return jsonify(response)
 
+    @app_views.route('/stats')
+    def get_stats():
+        """
+        Cresate flask
+        """
+        stats = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User'),
+        }
 
-@app_views.route('/stats')
-def get_stats():
-    """
-    Cresate flask
-    """
-    stats = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User'),
-    }
-
-    return jsonify(stats)
+        return jsonify(stats)
