90e65cf86dee3c0edb3ee21f0162049526181a7e
lupamo <arnoldlupamo@gmail.com>
2024-04-30 19:23:14 +0300

    get status count endpoints_

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,12 @@ routing app views status
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status')
 def status():
@@ -17,11 +22,11 @@ def status():
 def obj_count():
     """retrives the number of objects by type"""
     classes = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(classes)
