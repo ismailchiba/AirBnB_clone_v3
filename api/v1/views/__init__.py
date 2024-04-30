bcea88ba239da7513a3a6746ee6d3f2723419e98
SaidLamghari <said.lmg@gmail.com>
2024-04-30 01:17:50 +0100

    9. User

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" flask views"""
 from flask import Blueprint
-
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
@@ -11,5 +9,4 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 # 8. Amenity
 from api.v1.views.amenities import *
-# 9. User
 from api.v1.views.users import
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,7 +3,7 @@ i#!/usr/bin/python3
 
 from flask import Flask , abort , request
 from api.v1.views import app_views
-from os import name
+# from os import name
 from models.user import User
 from models import storage
 
