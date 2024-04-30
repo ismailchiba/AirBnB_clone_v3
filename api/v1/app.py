cf7173829f7e6a000feecc187947ee2dc083f9e8
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-30 20:50:37 +0300

    Remove debug code in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -35,7 +35,7 @@ def launch():
     # Retrieve host and port from environment variables
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=int(port), threaded=True, debug=True)
+    app.run(host=host, port=int(port), threaded=True)
 
 
 if __name__ == "__main__":
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@ from flask import jsonify, abort, request, redirect, url_for, make_response
 from models import storage
 from models.state import State
 from api.v1.views import app_views
-
+import logging
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
 @app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
@@ -25,7 +25,7 @@ def get_state(state_id=None):
 @app_views.route(
     "/states/<state_id>", methods=["DELETE"], strict_slashes=False
 )
-def delete_state(state_id):
+def delete(state_id):
     """Deletes a State object."""
     state = storage.get(State, state_id)
     if state is None:
@@ -38,26 +38,31 @@ def delete_state(state_id):
 @app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
     """Creates a State."""
-    if not request.json:
+    try:
+        data = request.get_json()
+    except BadRequest:
         abort(400, description="Not a JSON")
-    if "name" not in request.json:
+    if "name" not in data:
         abort(400, description="Missing name")
-    new_state = State(**request.get_json())
+
+    new_state = State(**data)
     storage.new(new_state)
     storage.save()
     return jsonify(new_state.to_dict()), 201
 
 
+
 @app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
-def update_state(state_id):
+def update(state_id):
     """Updates a State object."""
     state = storage.get(State, state_id)
+    data = request.get_json()
     if state is None:
-        abort(404)
-    if not request.json:
-        abort(400, description="Not a JSON")
+        return "Record not found"
+    if not data:
+        return "Record not found"
     ignore_keys = ["id", "created_at", "updated_at"]
-    for key, value in request.get_json().items():
+    for key, value in data.items():
         if key not in ignore_keys:
             setattr(state, key, value)
     storage.save()
