c79963bb1cdf3c2b050abb0dcfd0126b2994c71a
Emma <kilokoemma175@gmail.com>
2024-04-30 12:55:20 -0700

    task4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,20 +4,35 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-"""from models import storage"""
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-"""classes = {
+classes = {
     "amenities": Amenity,
     "cities": City,
     "places": Place,
     "reviews": Review,
     "states": State,
     "users": User
-}"""
+}
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def stat():
     """Returns a JSON"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """Retrieves the number of each objects by type"""
+    counts = {}
+    for cls in classes:
+        counts[cls] = storage.count(classes[cls])
+    return jsonify(counts)
