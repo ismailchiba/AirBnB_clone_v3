c4158abf2c92b4fa4267ce9d08a7f655a59520a0
Ayormitan <kolawoleabaz97@gmail.com>
2024-04-30 09:55:18 -0700

    app, instance of Flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,9 +9,10 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 
-app.register_blueprinit(app_views)
+app.register_blueprint(app_views)
 
-if __name___ == "__main__":
+
+if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', '5000'))
+    PORT = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=HOST, port=PORT, threaded=True)
