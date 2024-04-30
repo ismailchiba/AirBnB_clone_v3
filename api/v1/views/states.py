e58c813aba6c746b6dd9de6cf60da267570f8961
GODSPE1 <godspy18@gmail.com>
2024-04-30 18:27:07 +0100

    delete a state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Handles all default RESTFul API actions:"""
 
-from flask import jsonify
+from flask import jsonify, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -9,9 +9,32 @@ from models.state import State
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def state_list():
-    '''Retrieves the list of all State objects'''
+    """Retrieves the list of all State objects"""
     state_list = storage.all(State).values()
     list_of_states = []
     for state in state_list:
         list_of_states.append(state.to_dict())
     return jsonify(list_of_states)
+
+
+@app_views.route('/status/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a State objects"""
+    state_obj = storage.get(State, state_id)
+    if not state_obj:
+        return abort(404)
+    else:
+        return jsonify(state_obj)
+
+
+@app_views.route('/status/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """Deletes a State object"""
+    state_obj = storage.get(State, state_id)
+    if not state_obj:
+        return abort(404)
+    else:
+        storage.delete(state_obj)
+        storage.save()
+        return jsonify({"status": 200})
