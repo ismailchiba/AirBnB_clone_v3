3b6e248670dbb9fd1dd962a2ea045b7a75fadd42
lupamo <arnoldlupamo@gmail.com>
2024-04-30 12:35:38 +0300

    starting api4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """
 Creating Blueprint
 """
-from flask import Blueprint
 from api.v1.views.index import *
+from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
