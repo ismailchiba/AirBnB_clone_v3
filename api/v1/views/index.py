0f76d871e259efa9e587d5dfa61ce9bb72a809a8
lupamo <arnoldlupamo@gmail.com>
2024-04-30 18:38:22 +0300

    get status count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,24 @@ routing app views status
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """status OK"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('stats')
+def obj_count():
+    """retrives the number of objects by type"""
+    classes = {
+        'Amenity': storage.count('Amenity'),
+        'City': storage.count('City'),
+        'Place': storage.count('Place'),
+        'Review': storage.count('Review'),
+        'State': storage.count('State'),
+        'User': storage.count('User')
+    }
+    return jsonify(classes)
