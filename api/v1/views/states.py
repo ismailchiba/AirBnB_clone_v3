e18282895928bf81d24bc42470c6bbba61ba9dd8
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-30 20:42:53 +0300

    Revert states commit

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,10 +2,8 @@
 """States view for the API."""
 from flask import jsonify, abort, request, redirect, url_for, make_response
 from models import storage
-from werkzeug.exceptions import BadRequest
 from models.state import State
 from api.v1.views import app_views
-import logging
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
@@ -27,7 +25,7 @@ def get_state(state_id=None):
 @app_views.route(
     "/states/<state_id>", methods=["DELETE"], strict_slashes=False
 )
-def delete(state_id):
+def delete_state(state_id):
     """Deletes a State object."""
     state = storage.get(State, state_id)
     if state is None:
@@ -40,30 +38,26 @@ def delete(state_id):
 @app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
     """Creates a State."""
-    try:
-        data = request.get_json()
-    except BadRequest:
+    if not request.json:
         abort(400, description="Not a JSON")
-    if "name" not in data:
+    if "name" not in request.json:
         abort(400, description="Missing name")
-
-    new_state = State(**data)
+    new_state = State(**request.get_json())
     storage.new(new_state)
     storage.save()
     return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
-def update(state_id):
+def update_state(state_id):
     """Updates a State object."""
     state = storage.get(State, state_id)
-    data = request.get_json()
     if state is None:
-        return "Record not found"
-    if not data:
-        return "Record not found"
+        abort(404)
+    if not request.json:
+        abort(400, description="Not a JSON")
     ignore_keys = ["id", "created_at", "updated_at"]
-    for key, value in data.items():
+    for key, value in request.get_json().items():
         if key not in ignore_keys:
             setattr(state, key, value)
     storage.save()
