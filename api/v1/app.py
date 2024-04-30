c1818cc73a6303fbe2d483c2c5c457029a938441
Ayormitan <kolawoleabaz97@gmail.com>
2024-04-30 10:35:28 -0700

    Error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Creatiuon of flask app and fregistring of the blueprint
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -11,8 +11,21 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """
+    """
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
 
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
