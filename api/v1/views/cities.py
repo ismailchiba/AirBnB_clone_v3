5a1eee8d61642600baa1183e49fb927f8b49ac98
Nahima-ayni <nahimamadey1@gmail.com>
2024-04-30 11:10:17 +0300

    RESTful API

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,17 +10,19 @@ from models.state import State
 @app_views.route('/states/<state_id>/cities',
                  methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
-     """ Retrieves the list of all City objects of a State """
+    """ Retrieves the list of all City objects of a State """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
     for city in state.cities:
         city_list = city.to_dict()
+
+
 )
     return jsonify(city_list)
 
 
-@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+@ app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def one_city(city_id):
     city_obj = storage.get(City, city_id)
     if city_obj is None:
@@ -29,7 +31,9 @@ def one_city(city_id):
     return jsonify(city)
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+@ app_views.route('/cities/<city_id>',
+    methods = ['DELETE'],
+     strict_slashes = False)
 def del_city(city_id):
     city_obj = storage.get(City, city_id)
     if city_obj is None:
@@ -38,7 +42,8 @@ def del_city(city_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@ app_views.route('/states/<state_id>/cities',
+                  methods = ['POST'], strict_slashes = False)
 def create_post_city(state_id):
     state = storage.get(State, state_id)
     if state is None:
@@ -54,7 +59,7 @@ def create_post_city(state_id):
     new_city.save()
     return jsonify(new_city.to_dict()), 201
 
-@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+@ app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     city_obj = storage.get(City, city_id)
     if city_obj is None:
