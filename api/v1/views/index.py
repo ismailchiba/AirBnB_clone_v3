ad6278c0c60821fe1b0426691837942956ade039
Ayormitan <kolawoleabaz97@gmail.com>
2024-04-30 09:41:03 -0700

    Routes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Cresate flask app
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status')
 def api_status():
@@ -12,3 +13,19 @@ def api_status():
     """
     response = {'status': "OK"}
     return jsonify(response)
+
+@app_views.route('/stats')
+def get_stats():
+    """
+    Cresate flask 
+    """
+    stats = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User'),
+    }
+
+    return jsonify(stats)
