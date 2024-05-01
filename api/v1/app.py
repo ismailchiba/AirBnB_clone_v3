8da688ded5fe7bbd83ff7d0618bfbc134260a02e
Thugg55 <100637930+Thugg55@users.noreply.github.com>
2024-05-01 18:37:49 +0100

    Update work on app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,22 +12,31 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
-CORS(app, origins='0.0.0.0')
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app_views)
+
 
 @app.teardown_appcontext
-def close_db_sesion(exception=None):
-    """ Call the close method of storage instance """
+def tear(self):
+    ''' closes storage engine '''
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found_error(error):
-    return jsonify({"error": "Not found"}), 404
+def not_found(error):
+    ''' 404 error handler '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
