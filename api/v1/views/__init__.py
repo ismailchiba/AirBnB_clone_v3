bd944a6721bbfb18599b9c6b8ede8a2f0e3f5ba1
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-01 06:54:37 -0700

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Importing index.py here might cause a circular import issue,
-# so import it within the route where it's needed.
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
