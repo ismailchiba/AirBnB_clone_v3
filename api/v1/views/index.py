a80c878b25751116471932dfdf22901d1af55715
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-01 09:55:08 -0700

    update api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Import required module/lib"""
-from api.v1.views import app_views
+from . import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def get_status():
     return jsonify({"status": "OK"})
