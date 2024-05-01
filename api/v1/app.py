0f83165d1e5a6772f3fad27fa75ac233201833dc
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-01 06:51:45 -0700

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
@@ -10,7 +11,8 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_appcontext(exception):
     storage.close()
 
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', default="5000")
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
