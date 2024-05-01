35648f7080da6281270ac6f914ccfa947779588e
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-01 07:30:31 -0700

    pleasure

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""API - Defines the view for places amenities Api calls"""
+import os
+from flask import abort, jsonify, request
+from models import storage
+from models.amenity import Amenity
+from api.v1.views import app_views
+
+
+@app_views.route("/places/<place_id>/amenities", methods=["GET"])
+def show_place_amenities(place_id):
+    """Defines the GET method for getting a list of amenities"""
+    place = storage.get("Place", place_id)
+
+    if place is None:
+        abort(404)
+    return jsonify([a.to_dict() for a in place.amenities])
+
+
+@app_views.route(
+    "/places/<place_id>/amenities/<amenity_id>",
+    methods=["POST", "DELETE"]
+)
+def manage_place_amenities(place_id, amenity_id):
+    """Defines the POST and DELETE methods for a Amenity object"""
+    place = storage.get("Place", place_id)
+    amenity = storage.get("Amenity", amenity_id)
+    exists = False
+    if place is None or amenity is None:
+        abort(404)
+
+    if request.method == "POST":
+        if os.getenv("HBNB_TYPE_STORAGE") == "db":
+            if amenity in place.amenities:
+                exists = True
+            else:
+                place.amenities.append(amenity)
+        else:
+            if amenity.id in place.amenity_ids:
+                exists = True
+            else:
+                place.amenity_ids.append(amenity.id)
+
+        place.save()
+        return jsonify(amenity.to_dict()), (200 if exists else 201)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
+    else:
+        if amenity.id not in place.amenity_ids:
+            abort(404)
+        place.amenity_ids.remove(amenity.id)
+    place.save()
+    return jsonify({}), 200
---
+++ api/v1/views/users.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""API User view."""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models import storage
+from models.user import User
+
+
+@app_views.route("/users", methods=["GET", "POST"])
+def users():
+    """Defines GET and POST methods for the /users route."""
+    if request.method == "GET":
+        return jsonify([u.to_dict() for u in storage.all("User").values()])
+
+    data = request.get_json(silent=True)
+    if data is None:
+        return "Not a JSON", 400
+    if data.get("email") is None:
+        return "Missing email", 400
+    if data.get("password") is None:
+        return "Missing password", 400
+    user = User(**data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route("/users/<user_id>", methods=["GET", "DELETE", "PUT"])
+def user_id(user_id):
+    """Defines GET, PUT and DELETE methods for a specific ID on /users."""
+    user = storage.get("User", user_id)
+    if user is None:
+        abort(404)
+
+    if request.method == "GET":
+        return jsonify(user.to_dict())
+
+    elif request.method == "DELETE":
+        storage.delete(user)
+        storage.save()
+        return jsonify({})
+
+    data = request.get_json(silent=True)
+    if data is None:
+        return "Not a JSON", 400
+    avoid = {"id", "email", "created_at", "updated_at"}
+    [setattr(user, k, v) for k, v in data.items() if k not in avoid]
+    user.save()
+    return jsonify(user.to_dict())
