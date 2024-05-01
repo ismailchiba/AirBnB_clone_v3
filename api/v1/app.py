9690d4332288df95f56f80256b94d5753eeb9cb8
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-01 14:32:44 -0700

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Entry point for HolbertonBnB API calls."""
+"""The API of the end point."""
 from os import getenv
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
-from flasgger import Swagger, swag_from
 from models import storage
 from api.v1.views import app_views
 
@@ -14,31 +13,20 @@ CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
-template = {
-    "swagger": "2.0",
-    "info": {
-        "title": "HolbertonBnB",
-        "description": "RESTful API for HolbertonBnB",
-    }
-}
-swagger = Swagger(app, template=template)
-
 
 @app.errorhandler(404)
-def not_found(error):
-    """Returns a JSON-formatted 404 status code response."""
+def error_not_found(error):
+    """JSON-formatted 404 status code response."""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
 def teardown_storage(exc):
-    """Closes the storage session after every request."""
+    """storage session after every request."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(
-        host=getenv("HBNB_API_HOST", default="0.0.0.0"),
-        port=getenv("HBNB_API_PORT", default="5000"),
-        threaded=True
-    )
+    host = getenv("HBNB_API_HOST", default="0.0.0.0")
+    port = getenv("HBNB_API_PORT", default="5000")
+    app.run(host, port, threaded=True)
