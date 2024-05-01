fe90c72b6d1927ea1754381181f6dced5655a0db
AbuArwa001 <khalfanathman12@yahoo.com>
2024-05-02 01:40:27 +0300

    Check if its valid json - place_search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -18,7 +18,7 @@ def places_search():
     try:
         req_data = request.get_json()
         if req_data is None:
-            abort(404)
+            abort(400, "Not a JSON")
         if not req_data or\
             all(not req_data.get(key)
                 for key in ['states', 'cities', 'amenities']):
