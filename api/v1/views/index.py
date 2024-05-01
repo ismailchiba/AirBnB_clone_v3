bd07818ee5d0dfe97f82092341aa34f446514d8a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-01 10:05:02 -0700

    update api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,19 @@
 """Import required module/lib"""
 from . import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def get_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats(self):
+    """retrieves the number of each objects by type"""
+    stats = {}
+    for cls in storage.classes():
+        stats[cls.__name__] = storage.count(cls)
+
+    return jsonify(stats)
