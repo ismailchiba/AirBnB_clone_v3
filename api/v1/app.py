34e5dfc0dea8ae78e42c91455d8158872a958b14
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-01 04:06:50 -0700

    update api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Import the required module"""
-import models
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -19,4 +18,5 @@ def teardown_storage(exception):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.register_blueprint(app_views, url_prefix='/api/v1')
     app.run(host=host, port=port, threaded=True)
