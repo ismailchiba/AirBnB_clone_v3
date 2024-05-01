e694439946f4ea9d152a0457687ad890633a5bed
“Dino” <daringamaleldin@hotmail.com>
2024-05-01 14:49:26 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,33 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Get a single object from __objects
+
+        Args:
+            cls (str): string representing the class name
+            id  (str): string representing the object ID
+
+        Returns:
+            Object base on the `class` and `id` or else `None`.
+        """
+        if cls is None or id is None:
+            return None
+        key = '{}.{}'.format(cls, id)
+        return self.__objects.get(key, None)
+
+    def count(self, cls=None):
+        """counts all objects of a specific class (cls) in __objects
+        or all objects if no `cls` name is passed
+
+        Arsg:
+            cls (str): String representing the class name. Default (None)
+
+        Returns:
+            `count` of all object in __objects is cls is None, else `count`
+            of the specific onbject in __object.
+        """
+        if not cls:
+            return len(self.__objects)
+        return len([key for key in self.__objects if key.startswith(cls)])
