285672d4256c168d5057dfc15d3beb6e89a335cc
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-01 10:28:17 -0700

    update app/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Import the required module"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+
 
 app = Flask(__name__)
 
@@ -13,6 +14,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_storage(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
