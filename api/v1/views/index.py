39db1c763278d07d0853644757a43c2071b771e6
Thugg55 <100637930+Thugg55@users.noreply.github.com>
2024-05-01 18:24:31 +0100

    Updating on index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,38 @@
 @author: Edogun Peter Putech
 """
 from api.v1.views import app_views
-from flask import jsonify, Blueprint
+from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    """ get status of the route """
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+}
+
+
+@app_views.route("/status")
+def status():
+    """Status of my API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """Retrieve the number of each objects by type"""
+    statistics = {}
+
+    for key, value in classes.items():
+        statistics[key] = storage.count(value)
+
+    return jsonify(statistics)
