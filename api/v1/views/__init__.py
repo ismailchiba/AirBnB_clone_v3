36da6deb694a86d4ebae1e0425830daf2a746322
LyndaLilly <lilianlynda22@gmail.com>
2024-05-01 11:37:05 -0700

    tasks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,1 @@
-#!/usr/bin/python3
 
-'''this is blueprint app'''
-
-from Flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,1 @@
-#!/usr/bin/python3
-'''this is the index'''
 
-from flask import jsonify
-from api.v1.views import app_views
-
-@app_views.route('/status')
-def api_status():
-    """
-    this is the app status
-    """
-
-reponse = ('status': 'OK')
-return jsonify(response)
