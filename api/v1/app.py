23404d3dab59685e05e509d9817372d7e9209e82
LyndaLilly <lilianlynda22@gmail.com>
2024-05-01 11:11:27 -0700

    tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,50 +1,17 @@
 #!/usr/bin/python3
 '''this is the flask
 '''
-import os
-from flask import Flask, jsonify
-from flask_cors import CORS
-
+from os import getenv
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
-
 app = Flask(__name__)
-'''this is the web flask'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
+
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
-
-
-@app.teardown_appcontext
-def teardown_flask(exception):
-    '''this is the flask request'''
-    # print(exception)
-    storage.close()
-
-
-@app.errorhandler(404)
-def error_404(error):
-    '''this is the error request'''
-    return jsonify(error='Not found'), 404
-
-
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    message = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        message = error.description
-    return jsonify(error=message), 400
 
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-'''this is the blueprint api'''
-from flask import Blueprint
+
+'''this is blueprint app'''
+
+from Flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-'''this is the blueprint'''
 
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
 from api.v1.views.index import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.states import *
-from api.v1.views.users import *
