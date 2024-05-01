f73269795185ad7a6bd0ae301898838ba962046b
LyndaLilly <lilianlynda22@gmail.com>
2024-05-01 11:45:37 -0700

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,36 @@
+#!/usr/bin/python3
+'''this is the api'''
+from flask import jsonify
 
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status')
+def get_status():
+    '''this is api status
+    '''
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats')
+def get_stats():
+    '''this gets api stats
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
