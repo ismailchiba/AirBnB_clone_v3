3c911e284c1240ae00e02713af7128080cd890e5
sami-hamid <83097392+sami-hamid@users.noreply.github.com>
2024-05-01 18:32:14 +0000

    task 4 and 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-"""create flask app; index file"""
+"""create Flask app; index file"""
 from flask import Flask, jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """Restful API status"""
-    return jsonify({"status":  "OK"})
+    response = ({"status":  "OK"})
+    return jsonify(response)
+
+@app_views.route('/stats')
+def get_stats():
+    '''
+    
+    '''
+    stats={
+        'amenties': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+    }
+    return jsonify(stats)
+
