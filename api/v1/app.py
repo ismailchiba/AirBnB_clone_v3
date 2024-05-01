9a011abf435d7a3db72b5a5e4bcd880fbb87c3b8
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-05-01 23:52:46 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """app"""
 from flask import Flask, make_response, jsonify
 from models import storage
@@ -23,7 +24,7 @@ def tear(self):
 
 @app.errorhandler(404)
 def not_found(error):
-    ''' handles 404 error and gives json formatted response '''
+    '''script that handles 404 error and gives json formatted response'''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
