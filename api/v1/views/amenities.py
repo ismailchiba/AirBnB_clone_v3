fc0a84a0c3569ba47c74632bb495330190cf99f1
AbuArwa001 <khalfanathman12@yahoo.com>
2024-05-01 14:10:19 +0300

    REfactor get all amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -20,7 +20,7 @@ def get_all_amenities():
     amenities_lst = []
     amenities = storage.all(Amenity)
     for obj in amenities.values():
-        amenities_lst.append(obj.to_json())
+        amenities_lst.append(obj.to_dict())
 
     return jsonify(amenities_lst)
 
