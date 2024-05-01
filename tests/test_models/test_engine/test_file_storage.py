69758d7aafb606c95359ef9ed6bb2b675c2968b3
Profxbaba <daddyslove41@gmail.com>
2024-05-01 06:27:54 -0700

    Updated AirBnB clone - RESTful API

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,19 +113,27 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
-    def test_get(self):
-        """Test for get method."""
-        new_state = State(name="California")
-        models.storage.new(new_state)
-        models.storage.save()
-        retrieved_state = models.storage.get(State, new_state.id)
-        self.assertEqual(new_state, retrieved_state)
 
-    def test_count(self):
-        """Test for count method."""
-        count_before = models.storage.count()
-        new_state = State(name="New York")
-        models.storage.new(new_state)
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that retrieve objects from file.json"""
+        state = State(name='Nairobi')
+        models.storage.new(state)
         models.storage.save()
-        count_after = models.storage.count()
-        self.assertEqual(count_before + 1, count_after)
+
+        state_obj = models.storage.get(State, state.id)
+
+        self.assertEqual(state, state_obj)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that counts objects from file.json"""
+        objs_from_all = len(models.storage.all())
+        objs_from_count = models.storage.count()
+
+        self.assertEqual(objs_from_all, objs_from_count)
+
+        states_from_all = len(models.storage.all(State))
+        states_from_count = models.storage.count(State)
+
+        self.assertEqual(states_from_all, states_from_count)
