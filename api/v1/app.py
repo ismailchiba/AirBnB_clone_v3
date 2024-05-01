96676b5a508b113e37604bff7535031187ebb2da
ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-05-01 19:24:44 +0000

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/app/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
