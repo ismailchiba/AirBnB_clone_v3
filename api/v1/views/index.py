9a70e27e47042302e800332d35597a1f3094fb10
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-05-01 23:40:27 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""
-This module contains endpoint(route) status
-"""
-from api.v1.views import app_views
-from flask import jsonify
-=======
-"""index"""
+"""script for index"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
->>>>>>> 6e6b784f8744e0630b80c062bbc68f66cf7ade6b
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -22,27 +14,6 @@ classes = {"users": "User", "places": "Place", "states": "State",
            "cities": "City", "amenities": "Amenity",
            "reviews": "Review"}
 
-<<<<<<< HEAD
-@app_views.route('/stats', strict_slashes=False)
-def status():
-    """
-    Returns a JSON status
-    """
-    return jsonify({"stats": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
-=======
 
 @app_views.route('/status', methods=['GET'])
 def status():
@@ -57,4 +28,3 @@ def count():
     for cls in classes:
         count_dict[cls] = storage.count(classes[cls])
     return jsonify(count_dict)
->>>>>>> 6e6b784f8744e0630b80c062bbc68f66cf7ade6b
