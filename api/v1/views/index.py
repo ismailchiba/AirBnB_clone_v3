45c3c8981263822cd7a7b490cc655a56b0a6c4d0
sami-hamid <83097392+sami-hamid@users.noreply.github.com>
2024-05-01 15:57:31 +0000

    fixing task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """create flask app; index file"""
-from flask import jsonify
+from flask import Flask, jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """Restful API status"""
-    return jsonify({"status": "OK"})
+    return jsonify({"status":  "OK"})
