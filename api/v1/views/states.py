0a457b1c096e59eecc955146e273ae25f4654803
Tadala <naomikapengule@gmail.com>
2024-05-02 05:25:31 +0200

    Added type:ignore to Pylance errors

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,10 +15,14 @@ states = F(__name__)
 @AV.route('/states', methods=['GET', 'POST'])
 def get_or_create_states():
     """Get all states / Create a new state w no Id"""
+    States = storage.all('State')
+    if States is None:
+        abort(404, 'Not found')
+
     if RQ.method == 'GET':
-        States = storage.all('State')
         response = [sstate.to_dict() for sstate in States.values()]
         status = 200
+
     if RQ.method == 'POST':
         RQ_json = RQ.get_json()
         if RQ_json is None:
@@ -36,25 +40,25 @@ def get_or_create_states():
 def get_del_put_state(state_id):
     """Get, delete or update a State object w a given identifier"""
     response = {}
+    sstate = storage.get('State', state_id)
+    if sstate is None:
+        abort(404, "Not found")
+
     if RQ.method == 'GET':
-        response = storage.get('State', state_id)
-        if response is None:
-            abort(404, "Not found")
+        response = sstate.to_dict()
         status = 200
+
     if RQ.method == 'PUT':
         RQ_json = RQ.get_json()
         if RQ_json is None:
             abort(400, 'Not a JSON')
-        sstate = storage.get('State', state_id)
-        sstate = BM.db_update(RQ_json)
-        response = sstate
+        sstate = State.db_update(RQ_json)
+        sstate.save()   # type: ignore
+        response = sstate.to_dict()    # type: ignore
         status = 200
+
     if RQ.method == 'DELETE':
-        sstate = storage.get('State', state_id)
-        if sstate is None:
-            pass
-        sstate.delete()
+        sstate.delete()   # type: ignore
         del sstate
-        response = {}
         status = 200
     return jsonify(response), status
