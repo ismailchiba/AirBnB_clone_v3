5f0c4737a44c382125261d32a08372bb4841958f
“Dino” <daringamaleldin@hotmail.com>
2024-05-02 14:34:54 -0700

    updated db_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,412 +1,138 @@
 #!/usr/bin/python3
-"""
-Unit Test for BaseModel Class
-"""
-import unittest
-from datetime import datetime
-from models import *
+"""Unittests for testing models/engine/db_storage.py."""
 import os
-from models.base_model import Base
+import pep8
+import inspect
+import unittest
+import MySQLdb
+import models
 from models.engine.db_storage import DBStorage
+from models.state import State
+from models.user import User
 
 
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
-
-
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
 class TestDBStorageDocs(unittest.TestCase):
-    """Class for testing BaseModel docs"""
+    """Test documentation and style."""
 
     @classmethod
     def setUpClass(cls):
-        print('\n\n.................................')
-        print('..... Testing Documentation .....')
-        print('..... For FileStorage Class .....')
-        print('.................................\n\n')
+        """Set up for the doc tests"""
+        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
 
-    def test_doc_file(self):
-        """... documentation for the file"""
-        expected = ' Database engine '
-        actual = db_storage.__doc__
-        self.assertEqual(expected, actual)
+    def test_pep8_conformance_db_storage(self):
+        """Test that models/engine/db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_doc_class(self):
-        """... documentation for the class"""
-        expected = 'handles long term storage of all class instances'
-        actual = DBStorage.__doc__
-        self.assertEqual(expected, actual)
+    def test_pep8_conformance_test_db_storage(self):
+        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_engine/\
+test_db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_doc_all(self):
-        """... documentation for all function"""
-        expected = ' returns a dictionary of all objects '
-        actual = DBStorage.all.__doc__
-        self.assertEqual(expected, actual)
+    def test_db_storage_module_docstring(self):
+        """Test for the db_storage.py module docstring"""
+        self.assertIsNot(DBStorage.__doc__, None,
+                         "db_storage.py needs a docstring")
+        self.assertTrue(len(DBStorage.__doc__) >= 1,
+                        "db_storage.py needs a docstring")
 
-    def test_doc_new(self):
-        """... documentation for new function"""
-        expected = ' adds objects to current database session '
-        actual = DBStorage.new.__doc__
-        self.assertEqual(expected, actual)
+    def test_db_storage_class_docstring(self):
+        """Test for the DBStorage class docstring"""
+        self.assertIsNot(DBStorage.__doc__, None,
+                         "DBStorage class needs a docstring")
+        self.assertTrue(len(DBStorage.__doc__) >= 1,
+                        "DBStorage class needs a docstring")
 
-    def test_doc_save(self):
-        """... documentation for save function"""
-        expected = ' commits all changes of current database session '
-        actual = DBStorage.save.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_doc_reload(self):
-        """... documentation for reload function"""
-        expected = ' creates all tables in database & session from engine '
-        actual = DBStorage.reload.__doc__
-        self.assertEqual(expected, actual)
-
-    def test_doc_delete(self):
-        """... documentation for delete function"""
-        expected = ' deletes obj from current database session if not None '
-        actual = DBStorage.delete.__doc__
-        self.assertEqual(expected, actual)
+    def test_dbs_func_docstrings(self):
+        """Test for the presence of docstrings in DBStorage methods"""
+        for func in self.dbs_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
 
 
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
-class TestStateDBInstances(unittest.TestCase):
-    """testing for class instances"""
+class TestDBStorage(unittest.TestCase):
+    """Test the DBStorage class"""
 
     @classmethod
     def setUpClass(cls):
-        print('\n\n.................................')
-        print('......... Testing DBStorage .;.......')
-        print('........ For State Class ........')
-        print('.................................\n\n')
-
-    def setUp(self):
-        """initializes new BaseModel object for testing"""
-        self.state = State()
-        self.state.name = 'California'
-        self.state.save()
-
-    def test_state_all(self):
-        """... checks if all() function returns newly created instance"""
-        all_objs = storage.all()
-        all_state_objs = storage.all('State')
-
-        exist_in_all = False
-        for k in all_objs.keys():
-            if self.state.id in k:
-                exist_in_all = True
-        exist_in_all_states = False
-        for k in all_state_objs.keys():
-            if self.state.id in k:
-                exist_in_all_states = True
-
-        self.assertTrue(exist_in_all)
-        self.assertTrue(exist_in_all_states)
-
-    def test_state_delete(self):
-        state_id = self.state.id
-        storage.delete(self.state)
-        self.state = None
-        storage.save()
-        exist_in_all = False
-        for k in storage.all().keys():
-            if state_id in k:
-                exist_in_all = True
-        self.assertFalse(exist_in_all)
-
-
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
-class TestUserDBInstances(unittest.TestCase):
-    """testing for class instances"""
+        """Instantiate MySQLdb cursor."""
+        if type(models.storage) == DBStorage:
+            db = MySQLdb.connect(user=os.getenv("HBNB_MYSQL_USER"),
+                                 passwd=os.getenv("HBNB_MYSQL_PWD"),
+                                 db=os.getenv("HBNB_MYSQL_DB"))
+            cls.cursor = db.cursor()
+            cls.storage = DBStorage()
+            cls.storage.reload()
+            cls.state = State(name="California")
+            cls.user = User(email="holberton@holberton.com",
+                            password="password")
+            cls.storage._DBStorage__session.add(cls.state)
+            cls.storage._DBStorage__session.add(cls.user)
+            cls.storage._DBStorage__session.commit()
 
     @classmethod
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('...... Testing FileStorage ......')
-        print('.......... User  Class ..........')
-        print('.................................\n\n')
+    def tearDownClass(cls):
+        """Close MySQLdb cursor."""
+        if type(models.storage) == DBStorage:
+            cls.cursor.close()
+            cls.storage._DBStorage__session.close()
 
-    def setUp(self):
-        """initializes new user for testing"""
-        self.user = User()
-        self.user.email = 'test'
-        self.user.password = 'test'
-        self.user.save()
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_all_no_class(self):
+        """Test all method without a specified class."""
+        objs = storage.all()
+        self.assertEqual(type(objs), dict)
+        self.assertEqual(len(objs), 2)
 
-    def test_user_all(self):
-        """... checks if all() function returns newly created instance"""
-        all_objs = storage.all()
-        all_user_objs = storage.all('User')
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_all_specified_class(self):
+        """Test all method with specified class."""
+        users = self.storage.all(User)
+        self.assertEqual(len(users), 1)
+        self.assertEqual(list(users.values())[0].password, "password")
 
-        exist_in_all = False
-        for k in all_objs.keys():
-            if self.user.id in k:
-                exist_in_all = True
-        exist_in_all_users = False
-        for k in all_user_objs.keys():
-            if self.user.id in k:
-                exist_in_all_users = True
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_new(self):
+        """Test new method."""
+        ny = State(name="New York")
+        self.storage.new(ny)
+        self.assertIn(ny, list(self.storage._DBStorage__session.new))
+        self.storage._DBStorage__session.rollback()
 
-        self.assertTrue(exist_in_all)
-        self.assertTrue(exist_in_all_users)
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
+        ak = State(name="Arkansas")
+        self.storage._DBStorage__session.add(ak)
+        self.storage.save()
+        self.cursor.execute("SELECT * FROM states WHERE name = 'Arkansas'")
+        query = self.cursor.fetchall()
+        self.assertEqual(ak.id, query[0][0])
+        self.cursor.execute("DELETE FROM states WHERE name = 'Arkansas'")
 
-    def test_user_delete(self):
-        user_id = self.user.id
-        storage.delete(self.user)
-        self.user = None
-        storage.save()
-        exist_in_all = False
-        for k in storage.all().keys():
-            if user_id in k:
-                exist_in_all = True
-        self.assertFalse(exist_in_all)
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_get(self):
+        """Test get method."""
+        self.assertEqual(self.storage.get("User", self.user.id), self.user)
 
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_get_nonexistant(self):
+        """Test get method with nonexistant object."""
+        self.assertIsNone(self.storage.get("User", "nonexistant"))
 
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
-class TestCityDBInstances(unittest.TestCase):
-    """testing for class instances"""
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_count_no_class(self):
+        """Test count method without specified class."""
+        self.assertEqual(2, self.storage.count())
 
-    @classmethod
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('...... Testing DBStorage ......')
-        print('.......... City  Class ..........')
-        print('.................................\n\n')
-
-    def setUp(self):
-        """initializes new user for testing"""
-        self.state = State()
-        self.state.name = 'California'
-        self.state.save()
-        self.city = City()
-        self.city.name = 'Fremont'
-        self.city.state_id = self.state.id
-        self.city.save()
-
-    def test_city_all(self):
-        """... checks if all() function returns newly created instance"""
-        all_objs = storage.all()
-        all_city_objs = storage.all('City')
-
-        exist_in_all = False
-        for k in all_objs.keys():
-            if self.city.id in k:
-                exist_in_all = True
-        exist_in_all_city = False
-        for k in all_city_objs.keys():
-            if self.city.id in k:
-                exist_in_all_city = True
-
-        self.assertTrue(exist_in_all)
-        self.assertTrue(exist_in_all_city)
-
-
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
-class TestCityDBInstancesUnderscore(unittest.TestCase):
-    """testing for class instances"""
-
-    @classmethod
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('...... Testing FileStorage ......')
-        print('.......... City Class ..........')
-        print('.................................\n\n')
-
-    def setUp(self):
-        """initializes new user for testing"""
-        self.state = State()
-        self.state.name = 'California'
-        self.state.save()
-        self.city = City()
-        self.city.name = 'San_Francisco'
-        self.city.state_id = self.state.id
-        self.city.save()
-
-    def test_city_underscore_all(self):
-        """... checks if all() function returns newly created instance"""
-        all_objs = storage.all()
-        all_city_objs = storage.all('City')
-
-        exist_in_all = False
-        for k in all_objs.keys():
-            if self.city.id in k:
-                exist_in_all = True
-        exist_in_all_city = False
-        for k in all_city_objs.keys():
-            if self.city.id in k:
-                exist_in_all_city = True
-
-        self.assertTrue(exist_in_all)
-        self.assertTrue(exist_in_all_city)
-
-
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
-class TestPlaceDBInstances(unittest.TestCase):
-    """testing for class instances"""
-
-    @classmethod
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('...... Testing DBStorage ......')
-        print('.......... Place  Class ..........')
-        print('.................................\n\n')
-
-    def setUp(self):
-        """initializes new user for testing"""
-        self.user = User()
-        self.user.email = 'test'
-        self.user.password = 'test'
-        self.user.save()
-        self.state = State()
-        self.state.name = 'California'
-        self.state.save()
-        self.city = City()
-        self.city.name = 'San_Mateo'
-        self.city.state_id = self.state.id
-        self.city.save()
-        self.place = Place()
-        self.place.city_id = self.city.id
-        self.place.user_id = self.user.id
-        self.place.name = 'test_place'
-        self.place.description = 'test_description'
-        self.place.number_rooms = 2
-        self.place.number_bathrooms = 1
-        self.place.max_guest = 4
-        self.place.price_by_night = 100
-        self.place.latitude = 120.12
-        self.place.longitude = 101.4
-        self.place.save()
-
-    def test_place_all(self):
-        """... checks if all() function returns newly created instance"""
-        all_objs = storage.all()
-        all_place_objs = storage.all('Place')
-
-        exist_in_all = False
-        for k in all_objs.keys():
-            if self.place.id in k:
-                exist_in_all = True
-        exist_in_all_place = False
-        for k in all_place_objs.keys():
-            if self.place.id in k:
-                exist_in_all_place = True
-
-        self.assertTrue(exist_in_all)
-        self.assertTrue(exist_in_all_place)
-
-
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
-class TestStorageGet(unittest.TestCase):
-    """
-    Testing `get()` method in DBStorage
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        setup tests for class
-        """
-        print('\n\n.................................')
-        print('...... Testing Get() Method ......')
-        print('.......... Place  Class ..........')
-        print('.................................\n\n')
-
-    def setUp(self):
-        """
-        setup method
-        """
-        self.state = State(name="Florida")
-        self.state.save()
-
-    def test_get_method_obj(self):
-        """
-        testing get() method
-        :return: True if pass, False if not pass
-        """
-        result = storage.get(cls="State", id=self.state.id)
-
-        self.assertIsInstance(result, State)
-
-    def test_get_method_return(self):
-        """
-        testing get() method for id match
-        :return: True if pass, false if not pass
-        """
-        result = storage.get(cls="State", id=str(self.state.id))
-
-        self.assertEqual(self.state.id, result.id)
-
-    def test_get_method_none(self):
-        """
-        testing get() method for None return
-        :return: True if pass, false if not pass
-        """
-        result = storage.get(cls="State", id="doesnotexist")
-
-        self.assertIsNone(result)
-
-
-@unittest.skipIf(storage_type != 'db', 'skip if environ is not db')
-class TestStorageCount(unittest.TestCase):
-    """
-    tests count() method in DBStorage
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """
-        setup tests for class
-        """
-        print('\n\n.................................')
-        print('...... Testing Get() Method ......')
-        print('.......... Place  Class ..........')
-        print('.................................\n\n')
-
-    def setup(self):
-        """
-        setup method
-        """
-        self.state1 = State(name="California")
-        self.state1.save()
-        self.state2 = State(name="Colorado")
-        self.state2.save()
-        self.state3 = State(name="Wyoming")
-        self.state3.save()
-        self.state4 = State(name="Virgina")
-        self.state4.save()
-        self.state5 = State(name="Oregon")
-        self.state5.save()
-        self.state6 = State(name="New_York")
-        self.state6.save()
-        self.state7 = State(name="Ohio")
-        self.state7.save()
-
-    def test_count_all(self):
-        """
-        testing counting all instances
-        :return: True if pass, false if not pass
-        """
-        result = storage.count()
-
-        self.assertEqual(len(storage.all()), result)
-
-    def test_count_state(self):
-        """
-        testing counting state instances
-        :return: True if pass, false if not pass
-        """
-        result = storage.count(cls="State")
-
-        self.assertEqual(len(storage.all("State")), result)
-
-    def test_count_city(self):
-        """
-        testing counting non existent
-        :return: True if pass, false if not pass
-        """
-        result = storage.count(cls="City")
-
-        self.assertEqual(int(0 if len(storage.all("City")) is None else
-                             len(storage.all("City"))), result)
-
-
-if __name__ == '__main__':
-    unittest.main
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_count_specified_class(self):
+        """Test count method with specified class."""
+        self.assertEqual(1, self.storage.count("User"))
