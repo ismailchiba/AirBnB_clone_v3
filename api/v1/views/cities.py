c6889be8f1cd4360db1a2eb2a710a8bcce9d8636
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-02 16:46:15 -0700

    update api/v1/views/cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,6 +4,8 @@ from models import storage
 from api.v1.views import app_views
 from models.state import State
 from models.city import City
+from models.base_model import BaseModel
+
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
 def get_state_cities(state_id):
@@ -15,6 +17,7 @@ def get_state_cities(state_id):
         abort(404)
     return jsonify([city.to_dict() for city in state.cities])
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'])
 def get_city(city_id):
     """
@@ -24,6 +27,7 @@ def get_city(city_id):
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
@@ -36,6 +40,7 @@ def delete_city(city_id):
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def create_city(state_id):
@@ -54,6 +59,7 @@ def create_city(state_id):
     new_city = City(**data)
     new_city.save()
     return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
 def update_city(city_id):
