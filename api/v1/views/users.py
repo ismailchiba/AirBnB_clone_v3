2d1077856105a5f615d57361e613f5d95ad7a71b
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-05-02 16:38:01 -0700

    update api/v1/views/users.y

---
+++ api/v1/views/users.py
@@ -0,0 +1,75 @@
+# api/v1/views/users.py
+from flask import jsonify, request, abort
+from models import storage
+from api.v1.views import app_views
+from models.user import User
+from models.base_model import *
+
+
+@app_views.route('/users', methods=['GET'])
+def get_users():
+    """
+    Retrieve all users
+    """
+    users = storage.all(User)
+    return jsonify([user.to_dict() for user in users.values()])
+
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """
+    Retrieve a user by id
+    """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """
+    Delete a user by id
+    """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'])
+def create_user():
+    """
+    Create a new user
+    """
+    if not request.json:
+        abort(400, "Not a JSON")
+    if 'email' not in request.json:
+        abort(400, "Missing email")
+    if 'password' not in request.json:
+        abort(400, "Missing password")
+    data = request.json
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    """
+    Update a user by id
+    """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    if not request.json:
+        abort(400, "Not a JSON")
+    data = request.json
+    for key, value in data.items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
+
