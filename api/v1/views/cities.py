e96af89e3f25606d619bf5a06d84fb2980710343
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-05-02 11:51:08 +0300

    made changes for CRUD operations for City object

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This file handles all the default  RESTFul API
 actions (CRUD)
@@ -17,14 +16,12 @@ def get_cities_of_state(state_id):
     """
     Retrieves the list of all City objects of a State
     """
-
     state = storage.get("State", state_id)
 
     if state is None:
         abort(404)
 
     cities = [city.to_dict() for city in state.cities]
-
     return jsonify(cities)
 
 
@@ -46,15 +43,11 @@ def delete_city(city_id):
     """
     Deletes a City object with the specified city_id
     """
-
     city = storage.get("City", city_id)
-
     if city is not None:
         city.delete()
         storage.save()
-
         return make_response(jsonify({}), 200)
-
     abort(404)
 
 
@@ -64,14 +57,11 @@ def create_city(state_id):
     """
     Creates a City object
     """
-
     state = storage.get("State", state_id)
-
     if state is not None:
         request_body = request.get_json()
         if not request.is_json:
             return make_response(jsonify({'error': 'Not a JSON'}), 400)
-
         if 'name' not in request_body:
             return make_response(jsonify({'error': 'Missing name'}), 400)
 
@@ -79,7 +69,6 @@ def create_city(state_id):
         new_city = City(**request_body)
         new_city.save()
         return make_response(jsonify(new_city.to_dict()), 201)
-
     abort(404)
 
 
@@ -89,20 +78,14 @@ def update_city(city_id):
     """
     Updates a city object with the given city_id
     """
-
     city = storage.get("City", city_id)
-
     if city is not None:
         request_body = request.get_json()
-
         if not request.is_json:
             return make_response(jsonify({'error': 'Not a JSON'}), 400)
-
         for k, v in request_body.items():
             if k not in ["id", "state_id", "created_at", "updated_at"]:
                 setattr(city, k, v)
-
         city.save()
         return jsonify(city.to_dict())
-
     abort(404)
