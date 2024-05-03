6b7a335edcd6abd7c21bc005cb0a49ca036e1837
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-05-02 17:38:51 -0700

    test

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Defines the view for places amenities Api calls"""
-import os
+"""places amenities"""
 from flask import abort, jsonify, request, make_response
-from flasgger import swag_from
 from models import storage
 from models.amenity import Amenity
 from api.v1.views import app_views
+import os
 
 
 @app_views.route("/places/<place_id>/amenities", methods=["GET"])
 def place_amenities(place_id):
-    """Defines the GET method for getting a list of amenities"""
+    """getting a list of amenities"""
     place = storage.get("Place", place_id)
 
     if place is None:
@@ -20,30 +19,42 @@ def place_amenities(place_id):
 
 @app_views.route(
     "/places/<place_id>/amenities/<amenity_id>",
-    methods=["POST", "DELETE"]
+    methods=["POST"]
 )
-def post_delete_place_amenities(place_id, amenity_id):
-    """Defines the POST and DELETE methods for a Amenity object"""
+def post_place_amenities(place_id, amenity_id):
+    """delete amenity object"""
     place = storage.get("Place", place_id)
     amenity = storage.get("Amenity", amenity_id)
     exists = False
     if place is None or amenity is None:
         abort(404)
 
-    if request.method == "POST":
-        if os.getenv("HBNB_TYPE_STORAGE") == "db":
-            if amenity in place.amenities:
-                exists = True
-            else:
-                place.amenities.append(amenity)
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        if amenity in place.amenities:
+            exists = True
         else:
-            if amenity.id in place.amenity_ids:
-                exists = True
-            else:
-                place.amenity_ids.append(amenity.id)
+            place.amenities.append(amenity)
+    else:
+        if amenity.id in place.amenity_ids:
+            exists = True
+        else:
+            place.amenity_ids.append(amenity.id)
 
-        place.save()
-        return make_response(jsonify(amenity.to_dict()), (200 if exists else 201))
+    place.save()
+    myjson = amenity.to_dict()
+    return make_response(jsonify(myjson), (200 if exists else 201))
+
+@app_views.route(
+    "/places/<place_id>/amenities/<amenity_id>",
+    methods=["DELETE"]
+)
+def my_delete_place_amenities(place_id, amenity_id):
+    """create Amenity object"""
+    place = storage.get("Place", place_id)
+    amenity = storage.get("Amenity", amenity_id)
+    exists = False
+    if place is None or amenity is None:
+        abort(404)
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         if amenity not in place.amenities:
