ba221247d87d2435ecdf2d2a3ac4e37446e63a48
Ifeanyi Zion Chidiebere <64507351+ify-zi@users.noreply.github.com>
2024-05-04 00:54:25 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,6 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.city import City
-from models.user import User
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
@@ -19,15 +13,14 @@ def status():
     return jsonify(status="OK")
 
 
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
-def stats():
-    """return the current count in the db"""
-    stat = {
-            'amenities': storage.count(Amenity),
-            'cities': storage.count(City),
-            'places': storage.count(Place),
-            'reviews': storage.count(Review),
-            'states': storage.count(State),
-            'users': storage.count(User),
-           }
-    return jsonify(stat)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
