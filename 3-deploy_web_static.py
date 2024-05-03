90879a6238dd59187fa4127dd94b5b9e69ce856b
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-03 20:44:54 +0200

    Distrubiting archives on the web servers based on the `2-do_deploy_web-static` file.

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
@@ -11,7 +12,10 @@ env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """
+    Generating a tgz archive
+    """
+
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
@@ -19,12 +23,15 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception:
         return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """
+    Distributing an archive to the web servers
+    """
+
     if exists(archive_path) is False:
         return False
     try:
@@ -40,12 +47,15 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception:
         return False
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """
+    Creating and distributing an archive to the web servers
+    """
+
     archive_path = do_pack()
     if archive_path is None:
         return False
