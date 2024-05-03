a56c27ab09159afff3e0359a3b199913c41d1bc1
creeds-knight <apedoarthur21@gmail.com>
2024-05-03 12:12:40 +0300

    app.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,11 @@
 """ A module that initialises the views model with a blueprint app_views"""
 from flask import Blueprint
 
-app_views = Blueprint('/api/vi', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
+if app_views is not None:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
