4eef2c71fd47b9e085d4acd583d1d1572f7134bb
creeds-knight <apedoarthur21@gmail.com>
2024-05-03 16:22:20 +0300

    task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,8 +22,13 @@ def page_not_found(e):
     return jsonify({"error":"Not found"})
 
 
-
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(debug=True, host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
