b3f922a7e6e25aaa043f43af347d77a98b3ebb24
uwen_godwin <uwengodwin001@gmail.com>
2024-05-04 08:18:51 +0100

    AirBnB Clone

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """
-Cresate flask ap wahalp
+Create Flask app
 """
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-
 @app_views.route('/status')
 def api_status():
     """
-    Api status
+    API status
     """
     response = {'status': "OK"}
     return jsonify(response)
 
-    @app_views.route('/stats')
-    def get_stats():
-        """
-        Cresate flask
-        """
-        stats = {
-            "amenities": storage.count('Amenity'),
-            "cities": storage.count('City'),
-            "places": storage.count('Place'),
-            "reviews": storage.count('Review'),
-            "states": storage.count('State'),
-            "users": storage.count('User'),
-        }
+@app_views.route('/stats')
+def get_stats():
+    """
+    Create flask
+    """
+    stats = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User'),
+    }
 
-        return jsonify(stats)
+    return jsonify(stats)
+
