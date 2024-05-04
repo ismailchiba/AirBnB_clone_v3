1613d2e85d94f3ca3522d8895e5488d1195fce02
Ifeanyi Zion Chidiebere <64507351+ify-zi@users.noreply.github.com>
2024-05-04 01:20:24 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,13 +7,15 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=['GET'],
+                 strict_slashes=False)
 def status():
     """send a 200 reponse to cliet with format 'status': 'ok'"""
     return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'],
+                 strict_slashes=False)
 def stat():
     """returns the number of each objects by type"""
     return jsonify(
