8c02ecb22581b4b59d3d79617d78855d6f27a1c3
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-09 14:00:22 +0100

    Improving storage and adding get and count methods

--- test_get_count.py
+++ test_get_count.py
@@ -2,10 +2,17 @@
 """ Test .get() and .count() methods
 """
 from models import storage
-from models.state import State
+from models.state import State, City
 
+
+stat = State()
+cit = City()
+cit.save()
+stat.save()
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
+
+
 
 first_state_id = list(storage.all(State).values())[0].id
 print("First state: {}".format(storage.get(State, first_state_id)))
