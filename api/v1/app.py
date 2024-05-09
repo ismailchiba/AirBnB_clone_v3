27aa64954cef48ef9e10ef75735d2f3f48d6130d
KhezamiTaha <khezamitaha10@gmail.com>
2024-05-09 18:12:38 +0100

    Improving storage and adding get and count methods

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,8 +19,7 @@ def teardown_appcontext(exception):
     storage.close()
 
 
-host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-port = int(os.environ.get('HBNB_API_PORT', 5000))
-
 if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
