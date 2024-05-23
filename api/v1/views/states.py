9e4f46d7536ea5660e2f71ce5def6e3619328078
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-05-23 16:06:08 +0100

    chore: Add API endpoint for retrieving a specific state by ID

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -19,3 +19,28 @@ def get_all_state():
     response = jsonify(states_list), 200
 
     return response
+
+
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
+def get_state_by_id(state_id):
+    """
+    Retrieve a state by its ID.
+
+    Args:
+        state_id (str): The ID of the state to retrieve.
+
+    Returns:
+        tuple: A tuple containing the JSON representation
+        of the state and the HTTP status code.
+
+    Raises:
+        404: If the state with the specified ID does not exist.
+    """
+    state = storage.get(State, str(state_id)).to_dict()
+
+    if state is None:
+        abort(404)
+
+    response = jsonify(state), 200
+
+    return response
