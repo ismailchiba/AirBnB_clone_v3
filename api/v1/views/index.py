507b653a45c0f8c15272a5366665717aad22854d
Hissitissam <tissam.hisssi@gmail.com>
2024-05-24 08:50:41 +0000

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,18 @@ from flask import jsonify
 def get_status():
     """status route"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """stats route"""
+    classes = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(classes)
+    resp.status_code = 200
