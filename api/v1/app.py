f21a3a12813c9ff3a3870d9d3fbd17fde0e0c9e5
Hissitissam <tissam.hisssi@gmail.com>
2024-05-24 10:39:31 +0000

    task5

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown(exception):
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """ Handel error message """
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
