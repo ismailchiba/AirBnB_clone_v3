03014ccf79d046d0acf4829a9aa4d3ca861d2fb2
edoka <peter.edoka.242464@unn.edu.ng>
2024-05-24 20:52:16 +0100

    tests for get and count

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -68,8 +68,8 @@ test_db_storage.py'])
                             "{:s} method needs a docstring".format(func[0]))
 
 
-class TestFileStorage(unittest.TestCase):
-    """Test the FileStorage class"""
+class TestDBStorage(unittest.TestCase):
+    """Test the DBStorage class"""
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
@@ -86,3 +86,20 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """Test that get properly returns an object instance."""
+        a = State(name='Lagos')
+        a.save()
+        state = models.storage.get(State, a.id)
+        print(state)
+        self.assertEqual(a, state, 'states not equal')
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test that count properly works."""
+        a = State(name='Lagos')
+        a.save()
+        counts = models.storage.count('State')
+        self.assertGreater(counts, 0)
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,21 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that get properly returns an object instance."""
+        storage = FileStorage()
+        a = State(name='Lagos')
+        a.save()
+        state = storage.get(State, a.id)
+        self.assertEqual(a, state, 'states not equal')
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that get properly returns an object instance."""
+        storage = FileStorage()
+        a = State(name='Lagos')
+        a.save()
+        counts = storage.count('State')
+        self.assertGreater(counts, 0)
