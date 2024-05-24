0248b57319f7515be1f480d85722178adfe19270
9-max-6 <mutukumaxwel@gmail.com>
2024-05-24 15:46:28 +0300

    Task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,7 +11,6 @@ from models.state import State
 def getStates():
     """A function to get states"""
     states = storage.all('State')
-    print(states)
     all_states = []
     for state in states.values():
         all_states.append(state.to_dict())
@@ -24,10 +23,9 @@ def getStateById(state_id):
     """
     state = storage.get('State', state_id)
     if not state:
-        abort('404')
+        abort(404)
     else:
         return jsonify(state.to_dict())
-
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE', 'OPTIONS'])
@@ -36,10 +34,12 @@ def deleteStateById(state_id):
     """
     state = storage.get('State', state_id)
     if not state:
-        abort('404')
+        abort(404)
     else:
         storage.delete(state)
+        storage.save()
         return {}, 200
+
 
 @app_views.route('/states/', methods=['POST', 'OPTIONS'])
 def createState():
@@ -49,6 +49,27 @@ def createState():
         new_state = request.get_json()
         state_obj = State(**new_state)
         storage.new(state_obj)
+        storage.save()
     except Exception as e:
         abort(400, 'Not a JSON')
     return jsonify(state_obj.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['PUT', 'OPTIONS'])
+def editState(state_id):
+    """A method to edit state by id
+    """
+    state = storage.get('State', state_id)
+    print(state_id)
+    print(state)
+    if not state:
+        print("Issue")
+    else:
+        try:
+            request_json = request.get_json()
+            for key, value in request_json.items():
+                setattr(state, key, value)
+        except Exception as e:
+            abort(404, 'Not a valid JSON')
+        state.save()
+        return jsonify(state.to_dict()), 200
