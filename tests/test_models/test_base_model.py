25ee27d77570f648daf6a391a54b6564703231be
Y-admen <yassminash305@gmail.com>
2024-05-24 14:51:34 +0300

    update_tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -78,23 +78,21 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(inst.name, "Holberton")
         self.assertEqual(inst.number, 89)
 
-    def test_datetime_attributes(self):
-        """Test that two BaseModel instances have different datetime objects
-        and that upon creation have identical updated_at and created_at
-        value."""
-        tic = datetime.now()
-        inst1 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
-        tic = datetime.now()
-        inst2 = BaseModel()
-        toc = datetime.now()
-        self.assertTrue(tic <= inst2.created_at <= toc)
-        self.assertEqual(inst1.created_at, inst1.updated_at)
-        self.assertEqual(inst2.created_at, inst2.updated_at)
-        self.assertNotEqual(inst1.created_at, inst2.created_at)
-        self.assertNotEqual(inst1.updated_at, inst2.updated_at)
+
+def test_datetime_attributes(self):
+    """Test that two BaseModel instances have different datetime objects"""
+    bm1 = BaseModel()
+    start_time = datetime.now()  # Create a new datetime object
+    bm2 = BaseModel()
+    self.assertNotEqual(bm1.created_at, bm2.created_at)
+    self.assertNotEqual(bm1.updated_at, bm2.updated_at)
+    self.assertLess(bm1.created_at, bm2.created_at)
+    self.assertLess(bm1.updated_at, bm2.updated_at)
+    end_time = datetime.now()
+    self.assertTrue(start_time <= bm1.created_at <= end_time)
+    self.assertTrue(start_time <= bm2.created_at <= end_time)
+    self.assertTrue(start_time <= bm1.updated_at <= end_time)
+    self.assertTrue(start_time <= bm2.updated_at <= end_time)
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
