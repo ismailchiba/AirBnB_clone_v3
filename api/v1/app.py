5aa42f779069c69deea1b4ab66934be94f8969d6
9-max-6 <mutukumaxwel@gmail.com>
2024-05-24 16:39:20 +0300

    Fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,9 +2,11 @@
 """
 App.py, the central application for web app
 """
+
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
