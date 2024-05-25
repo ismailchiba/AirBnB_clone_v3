db1b4eacf8344511e1f4c773a0140bf784617ca4
Madani00 <mamchart@gmail.com>
2024-05-24 18:01:43 -0700

    update; madani

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,13 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        one_object = self.all(cls)
+        return one_object.get(cls.__name__ + '.' + id)
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        cls_object = self.all(cls)
+        return len(cls_object)
