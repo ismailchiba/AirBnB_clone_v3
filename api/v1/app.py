f3753f6bfc9a153aa757b9ae05ffce52081ece8c
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-25 05:49:20 +0300

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,13 +7,15 @@ from os import getenv
 HOST = getenv("HBNB_API_HOST") or '0.0.0.0'
 PORT = getenv("HBNB_API_PORT") or '5000'
 
-app = Flask()
-@app.register_blueprint(app_views)
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
-@app.teardown_appcontext()
-def teardown_appcontext(response_or_exc):
-    """Close storage in when flask app finish its process"""    
+
+@app.teardown_appcontext
+def teardown_appcontext(code):
+    """Close storage in when flask app finish its process"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status_route():
     """status route function: return OK"""
     return jsonify({'status': 'OK'})
