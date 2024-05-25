6628dc761158a3052306c9b613d47458b3506040
KARAM022 <my0fav0library@gmail.com>
2024-05-25 23:20:12 +0100

    task 10

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,65 +8,80 @@ from models.city import City
 from models.place import Place
 from models.user import User
 
-@app_views.route('/api/v1/cities/<city_id>/places', methods=['GET'])
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places(city_id):
     """Retrieves the list of all Place objects of a City"""
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-    places = [place.to_dict() for place in city.places]
-    return jsonify(places)
+    place_list = []
+    city_obj = storage.get("City", str(city_id))
+    for obj in city_obj.places:
+        place_list.append(obj.to_json())
 
-@app_views.route('/api/v1/places/<place_id>', methods=['GET'])
+    return jsonify(place_list)
+
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
     """Retrieves a Place object"""
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404)
-    return jsonify(place.to_dict())
+    fetched_obj = storage.get("Place", str(place_id))
 
-@app_views.route('/api/v1/places/<place_id>', methods=['DELETE'])
+    if fetched_obj is None:
+        abort(404)
+
+    return jsonify(fetched_obj.to_json())
+
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404)
-    storage.delete(place)
-    storage.save()
-    return jsonify({}), 200
+    fetched_obj = storage.get("Place", str(place_id))
 
-@app_views.route('/api/v1/cities/<city_id>/places', methods=['POST'])
+    if fetched_obj is None:
+        abort(404)
+
+    storage.delete(fetched_obj)
+    storage.save()
+
+    return jsonify({})
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """Creates a Place object"""
-    city = storage.get(City, city_id)
-    if not city:
+    place_json = request.get_json(silent=True)
+    if place_json is None:
+        abort(400, 'Not a JSON')
+    if not storage.get("User", place_json["user_id"]):
         abort(404)
-    data = request.get_json()
-    if not data:
-        return jsonify({"error": "Not a JSON"}), 400
-    if 'user_id' not in data:
-        return jsonify({"error": "Missing user_id"}), 400
-    if 'name' not in data:
-        return jsonify({"error": "Missing name"}), 400
-    user = storage.get(User, data['user_id'])
-    if not user:
+    if not storage.get("City", city_id):
         abort(404)
-    new_place = Place(city_id=city_id, **data)
-    new_place.save()
-    return jsonify(new_place.to_dict()), 201
+    if "user_id" not in place_json:
+        abort(400, 'Missing user_id')
+    if "name" not in place_json:
+        abort(400, 'Missing name')
 
-@app_views.route('/api/v1/places/<place_id>', methods=['PUT'])
+    place_json["city_id"] = city_id
+
+    new_place = Place(**place_json)
+    new_place.save()
+    resp = jsonify(new_place.to_json())
+    resp.status_code = 201
+
+    return resp
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object"""
-    place = storage.get(Place, place_id)
-    if not place:
+    place_json = request.get_json(silent=True)
+
+    if place_json is None:
+        abort(400, 'Not a JSON')
+
+    fetched_obj = storage.get("Place", str(place_id))
+
+    if fetched_obj is None:
         abort(404)
-    data = request.get_json()
-    if not data:
-        return jsonify({"error": "Not a JSON"}), 400
-    ignored_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
-    for key, value in data.items():
-        if key not in ignored_keys:
-            setattr(place, key, value)
-    place.save()
-    return jsonify(place.to_dict()), 200
+
+    for key, val in place_json.items():
+        if key not in ["id", "created_at", "updated_at", "user_id", "city_id"]:
+            setattr(fetched_obj, key, val)
+
+    fetched_obj.save()
+
+    return jsonify(fetched_obj.to_json())
