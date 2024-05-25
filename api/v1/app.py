498a3da0c921e13eb9815392bc81d40391cc6cd1
mosescode1 <emosescode@gmail.com>
2024-05-25 11:49:24 +0100

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is an api version 1"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """close all storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error Handlers"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
