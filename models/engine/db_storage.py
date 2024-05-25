0b892a181ec750d21d14bf8b05b5b8915c0d6616
HebaZaki <h_borhan@hotmail.com>
2024-05-25 16:36:06 +0300

    storage database

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the class DBStorage
-"""
-
+"""Contains the class DBStorage"""
 import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
