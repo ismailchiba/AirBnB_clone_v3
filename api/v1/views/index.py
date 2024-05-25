4fa8801bb4fd56e2e42bbb54db5e977751465fcc
mosescode1 <emosescode@gmail.com>
2024-05-25 17:48:44 +0100

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """Index Module"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status")
 def get_status():
     """Return a response status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def get_stats():
+    """Return a response status"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
