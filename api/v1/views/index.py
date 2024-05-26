67af09a8aed2b7718161644bf0147c2a77d1d223
yaredybk <yb12ybk@gmail.com>
2024-05-26 05:59:44 -0700

    states_count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,19 @@ API version 1 views
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def get_status():
     """send status of the server as JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def get_stats():
+    """send the number of each objects by type:
+    """
+    from models.engine.file_storage import classes
+    tmp = {key.lower():storage.count(value) for key, value in classes.items()}
+    return jsonify(tmp)
