95e0f6bda7fce3e34f805f6b36353e5d645150de
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-05-26 14:12:22 -0700

    App

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,13 @@ This module contains the principal application
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
+#!/usr/bin/python3
+"""
+This module contains the principal application
+"""
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 import os
 
 app = Flask(__name__)
@@ -15,6 +22,14 @@ app.register_blueprint(app_views)
 def close_db(obj):
     """ calls methods close() """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(error):
+    """create a handler for 404 errors
+       that returns a JSON-formatted
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
