7b5bf9c307546fc211e37ebb66b04475ac7ef034
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-05-26 15:24:58 +0000

    made a few changes

--- models/user.py
+++ models/user.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -30,7 +31,7 @@ class User(BaseModel, Base):
         super().__init__(*args, **kwargs)
 
     def __setattr__(self, name, value):
-        """ Override setattr to md5 hash `value` if `name` is password """
+        """sets a password with md5 encryption"""
         if name == "password":
-            value = hashlib.md5(value.encode()).hexdigest()
+            value = md5(value.encode()).hexdigest()
         super().__setattr__(name, value)
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -115,27 +115,27 @@ class TestFileStorage(unittest.TestCase):
         self.assertEqual(json.loads(string), json.loads(js))
 
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
-    def test_returns_obj(self):
-        """Test that get returns an existing object """
+    def test_get(self):
+        """ Tests method for obtaining an instance file storage"""
         storage = FileStorage()
-        storage.new(State())
-        first_state_obj = list(storage.all("State").values())[0]
-        state_obj = storage.get("State", first_state_obj.id)
-        self.assertIs(first_state_obj, state_obj)
-
-    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
-    def test_returns_none(self):
-        """Test that get returns None for nonexisting object """
+        dic = {"name": "Vecindad"}
+        instance = State(**dic)
+        storage.new(instance)
+        storage.save()
         storage = FileStorage()
-        state_obj = storage.get("State", "IDONTEXIST")
-        self.assertIsNone(state_obj)
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
 
     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_count(self):
-        """Test that count is properly return """
-        objs = models.storage.all()
-        self.assertEqual(len(objs), models.storage.count())
-        state_objs = models.storage.all("State")
-        self.assertEqual(len(state_objs), models.storage.count("State"))
-        no_objs = models.storage.all("Any")
-        self.assertEqual(len(no_objs), models.storage.count("Any"))
+        """ Tests count method file storage """
+        storage = FileStorage()
+        dic = {"name": "Vecindad"}
+        state = State(**dic)
+        storage.new(state)
+        dic = {"name": "Mexico"}
+        city = City(**dic)
+        storage.new(city)
+        storage.save()
+        c = storage.count()
+        self.assertEqual(len(storage.all()), c)
