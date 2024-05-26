a41ca417a18b721aa15121efa7692c8cbe2cf20f
Lahbiyeb-Ismail <ismaillhbibe@gmail.com>
2024-05-26 17:26:25 +0100

    Refactor places_search endpoint

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -172,7 +172,7 @@ def places_search():
         cities = data.get("cities", None)
         amenities = data.get("amenities", None)
 
-    if not data or not len(data) or (not states and not cities and not amenities):
+    if not states and not cities and not amenities:
         places = storage.all(Place).values()
         list_places = []
         for place in places:
