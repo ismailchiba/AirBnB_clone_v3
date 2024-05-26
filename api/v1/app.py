1862131953b7227d004764052c188710e3658693
Gift <aghaulor.gift@gmail.com>
2024-05-26 11:16:26 -0700

    Add error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,11 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
+from werkzeug.exceptions import HTTPException
+from flask import jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app, resources={"/*": {"origins": '0.0.0.0'}})
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,6 +20,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Close the storage engine"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """ Return a custom 404 error """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
