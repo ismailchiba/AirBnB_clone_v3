69a2b974118bf2f013d544f4a00977c096aed53b
SirJupiter <jupitertoby@gmail.com>
2024-05-26 11:43:46 +0100

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Creates a Flask web application"""
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown(exc):
     """Function closes the storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_not_found(error):
+    """Handles 404 errors, returns JSON-formatted stat code response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,36 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {
+    "amenities": storage.count(Amenity),
+    "cities": storage.count(City),
+    "places": storage.count(Place),
+    "reviews": storage.count(Review),
+    "states": storage.count(State),
+    "users": storage.count(User)
+}
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def route():
-    """Retuns an OK status code in json format"""
-    response = jsonify({"status": "OK"})
-    response.status_code = 200
-    return response
+    """Returns an OK status code in json format"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type"""
+    stats_dict = {}
+    for key in classes.keys():
+        stats_dict[key] = classes[key]
+
+    return jsonify(stats_dict)
