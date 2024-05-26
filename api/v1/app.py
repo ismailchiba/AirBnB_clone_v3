b082bef70a891cf8a0e3dfad682d3b66911fe684
Dan <danlewismuriuki@gmail.com>
2024-05-26 23:16:46 +0300

    Make __init__.py executable
    Added executable permissions to the __init__.py file to ensure it can be executed directly. This change allows for more convenient execution of the file when necessary.

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,4 +25,6 @@ def teardown_db(exxception):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
+    # getenv returns a string and port is an int
+    # Threaded is set to true so it can serve multiple requests at once
     app.run(host=host, port=port, threaded=True)
