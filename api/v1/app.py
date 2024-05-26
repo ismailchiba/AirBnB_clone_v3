458af620c9c09301a7151ef953d71c86963f568d
Benjamin Torfu <btorfu@proton.me>
2024-05-26 22:48:00 +0000

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ A flask module
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
-
+import os
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
@@ -17,4 +17,4 @@ def close_conn(exception):
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST" or "0.0.0.0")
     port = os.environ.get("HBNB_API_PORT", 5000)
-    app.run(host=host, port=port, threaded=true)
+    app.run(host=host, port=port, threaded=True)
