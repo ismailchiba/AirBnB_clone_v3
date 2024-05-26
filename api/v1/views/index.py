17bc68cefa264221c49e1330ae880074e1e3b211
Y-admen <yassminash305@gmail.com>
2024-05-26 06:52:33 +0300

    route_stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@ Index view for API
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
 @app_views.route('/status', methods=['GET'])
@@ -12,3 +19,20 @@ def status():
     Return a JSON response with the status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Retrieves the number of objects by type
+    """
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+
+    return jsonify(stats)
