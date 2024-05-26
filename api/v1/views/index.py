24706c42565809b52da11d31a09fef2ecf15f609
Chiamablessing <chiamablessing@gmail.com>
2024-05-26 03:54:11 +0100

    First API, status

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""index"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """Returns the status of the API"""
+    return jsonify({"status": "OK"})
