0cbb2e6d9b4fbd6a2f0a60422e5606f8b16fc8a7
KARAM022 <my0fav0library@gmail.com>
2024-05-26 01:20:15 +0100

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,18 +10,20 @@ from models import storage
 from api.v1.views import app_views
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ Handel error message """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
