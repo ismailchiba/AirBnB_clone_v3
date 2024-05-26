dbbfa0fe1d73a59f68087f5711cb967bc3b3b209
Ikigu <ikigu.geo@gmail.com>
2024-05-26 20:32:24 +0300

    fix: change strict slashes to false

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False)
 def status():
     """Status"""
     return jsonify({"status": "OK"})
