c05c08d84b92d26775bfda407b3a3a21d0319881
vishaylinp <vishaylinparshotam@gmail.com>
2024-05-26 08:25:35 -0700

    pycode

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,9 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_app (obj):
+def teardown_app(obj):
     """registers a function when app contect is torn down"""
     storage.close()
 
