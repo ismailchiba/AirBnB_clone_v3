7dddd3b8be46c8a8f4425039abf21690575dd3e6
K-kare <kamunawangari@gmail.com>
2024-05-26 10:55:42 -0700

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""index"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+from models import storage
+
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """ status route"""
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
