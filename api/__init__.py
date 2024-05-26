dd80701aa239ed7c5baf8ec47dbe491a577575f4
samuel abiona <samuelo.abiona@gmail.com>
2024-05-26 11:21:18 -0700

    First commit on RESTful API

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""It initializes the package 'api'."""
+    def __init__(self):
+        """An empty method"""
+        pass
---
+++ api/v1/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Initializes the package 'v1'."""
+    def __init__(self):
+        """An empty method with a placeholder"""
+        pass
