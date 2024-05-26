67616841293a8e595842549bedc9d2366de27c08
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-05-26 05:24:38 +0000

    made a few changes relatef to pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,8 +55,9 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception as e:
             pass
+            print(e.args)
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
