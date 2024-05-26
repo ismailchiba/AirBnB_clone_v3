58ef425e400237f1fa52e83a548489b5fe2786cf
Y-admen <yassminash305@gmail.com>
2024-05-26 07:38:37 +0300

    req

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,7 +14,7 @@ def close_storage(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return (jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
