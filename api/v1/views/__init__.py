c8915739979fa47e80c100dc81f31ac7c50eaeca
edoka <peter.edoka.242464@unn.edu.ng>
2024-05-26 22:26:22 +0100

    crud endpoints for state

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
+
 from flask import Blueprint
 
 app_views = Blueprint('views_blueprint',
                       __name__,
-                      url_prefix='/api/v1/')
+                      url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.states import *
---
+++ api/v1/views/states.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""State crud routes."""
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'])
+def all_states():
+    """Retrieve all states."""
+    all = storage.all('State')
+    result = []
+    for key, value in all.items():
+        result.append(value.to_dict())
+    return make_response(jsonify(result))
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def one_state(state_id):
+    """Retrieve one state."""
+    state = storage.get('State', state_id)
+    if state:
+        return make_response(state.to_dict(), 200)
+    abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """Delete a state."""
+    state = storage.get('State', state_id)
+    if state:
+        storage.delete(state)
+        return make_response({}, 200)
+    abort(404)
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Create a state."""
+    try:
+        payload = request.get_json()
+    except Exception as e:
+        return make_response({'error': 'Not a JSON'}, 400)
+
+    if payload and 'name' not in payload:
+        return make_response({'error': 'Missing name'}, 400)
+
+    name = payload.get('name')
+    state = State(name=name)
+    state.save()
+    return make_response(state.to_dict(), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """Update a stat."""
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    try:
+        payload = request.get_json()
+    except Exception as e:
+        return make_response({'error': 'Not a JSON'}, 400)
+
+    if payload and 'name' not in payload:
+        return make_response({'error': 'Missing name'}, 400)
+
+    for key, value in payload.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return make_response(state.to_dict(), 200)
