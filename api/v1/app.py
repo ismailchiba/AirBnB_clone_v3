371b6c64e28ce974d60ddda4db6aebf47d0a5dbb
K-kare <kamunawangari@gmail.com>
2024-05-26 02:57:28 -0700

    app.py

---
+++ api/v1/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" start your API """
+from flask import Flask
+from api.v1.views import app_views
+from models import storage
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+@app.teardown_appcontext
+
+def teardown(exception):
+"""teardown function"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
