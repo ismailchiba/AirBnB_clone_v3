2218929888b91f149e1e20f6c8aa826cc6611030
Kathyslate <mercyoyetunde98@gmail.com>
2024-05-26 16:37:32 -0700

    swasdcd

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,38 @@
 #!/usr/bin/python3
-"""File"""
+""" app.py file """
+
 from flask import Flask, jsonify
-from models import storage
-from api.v1.views import app_views
-import os
 from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})  # Create a CORS instance
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ A method that closes the storage on teardown """
+def teardown(exception):
+    """ teardown function"""
     storage.close()
 
-@app.errorhandler(404)
-def not_found(error):
-    """Handle error if page not found"""
 
-    return jsonify({'error': 'Not found'}), 404
+@app.errorhandler(404)
+def handle_404(exception):
+    """ 404 error handling"""
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
