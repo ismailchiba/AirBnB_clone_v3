88139ef3ec048150e732a26b3cd8904c4fe1b011
Hussein380 <huznigarane@gmail.com>
2024-05-26 09:33:19 +0300

    m

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,7 @@ This module initializes the Blueprint for the API views
 from flask import Blueprint
 
 """Create a Blueprint instance for the API views with specified URL prefix """
-app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 
 """ import all the views module to register their  routes with Blueprint"""
 from api.v1.views.index import *
