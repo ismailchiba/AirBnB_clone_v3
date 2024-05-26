fb35f908871d2b6f65d2ae5ccde26f8bc1833c03
Y-admen <yassminash305@gmail.com>
2024-05-26 06:59:51 +0300

    handel_404

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,8 +12,14 @@ def close_storage(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == "__main__":
-    host = '0.0.0.0' if not getenv('HBNB_API_HOST')else getenv(
-            'HBNB_API_HOST')
-    port = 5000 if not getenv('HBNB_API_PORT') else getenv('HBNB_API_PORT')
+    host = "0.0.0.0" if not getenv("HBNB_API_HOST") \
+        else getenv("HBNB_API_HOST")
+    port = 5000 if not getenv("HBNB_API_PORT") \
+        else getenv("HBNB_API_PORT")
     app.run(host=host, port=port, threaded=True)
