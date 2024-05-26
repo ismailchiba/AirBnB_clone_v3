9587217504dd3e50b374f6e20a5c92903c14460a
Benjamin Torfu <btorfu@proton.me>
2024-05-26 22:56:48 +0000

    myapi

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,11 +8,14 @@ from models import storage
 import os
 app = Flask(__name__)
 
+
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def close_conn(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST" or "0.0.0.0")
