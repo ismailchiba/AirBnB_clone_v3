2e428478bfe500588f49c8ddc346f0482866a9e1
gboigwe <geakande@gmail.com>
2024-05-28 00:57:15 +0100

    Status of API

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,12 @@
 """
 Flask Application
 """
+
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -24,6 +26,6 @@ if __name__ == "__main__":
     """
     Run the Flask application
     """
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', default='5000')
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,9 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
     Returns the status of the API
     """
-    return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"}), 200
