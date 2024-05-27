f87cfed8cb809c40dcb9257800eb2dd6d58eb983
Amine <mouhamedamine.akchouche@gmail.com>
2024-05-27 06:29:00 -0700

    task 1

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" returns json statuses for app_views routes  """
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def stat_return():
+    """ return json status: OK """
+    return jsonify({"status": "OK"})
