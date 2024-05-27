f084a596b4db50a995da5fb8c50f82fe237c30a1
Peacechimamaka <peaceamaka111@gmail.com>
2024-05-27 11:24:26 +0100

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,11 +24,13 @@ def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == "__main__":
-    app.run(
-        host=os.getenv("HBNB_API_HOST") if os.getenv(
-            "HBNB_API_HOST") else "0.0.0.0",
-        port=os.getenv("HBNB_API_PORT") if os.getenv(
-            "HBNB_API_PORT") else "5000",
-        threaded=True
-    )
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
