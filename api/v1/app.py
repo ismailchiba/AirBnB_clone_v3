b5683a04db403ce2e6f447bd2a67176c01ee6636
stanley643 <nmurigi@kabarak.ac.ke>
2024-05-27 03:11:38 -0700

    live coding debuging

--- api/v1/app.py
+++ api/v1/app.py
@@ -40,22 +40,4 @@ if __name__ == '__main__':
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
     port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
-=======
-# api/v1/app.py
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
-import os
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    storage.close()
-
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
 
