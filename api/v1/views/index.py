84aa6e56b44a887cf2e6b4a4f054dbc4e9a72fda
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2024-05-27 15:59:55 -0700

    airbnb_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -11,3 +12,18 @@ def api_status():
     """ Status of API  return: response with json."""
     responce = {'status': "OK"}
     return jsonify(responce)
+
+
+@app_views.route('/stats')
+def get_status():
+    """Retrieves the number of each objects by type."""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+    }
+
+    return jsonify(stats)
