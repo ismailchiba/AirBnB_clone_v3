4c3ba738265ba16d90a3b89a7fda47ecde2cfd99
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2024-05-27 15:32:32 -0700

    airbnb_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
 """ The Index. """
 
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.user import User
-from models import storage
+
 from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+@app_views.route('/status')
+def api_status():
     """ Status of API  return: response with json."""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieve The Number of each object by type. """
-
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objs)
+    responce = {'status': "OK"}
+    return jsonify(responce)
