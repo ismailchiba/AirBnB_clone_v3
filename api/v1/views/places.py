48c1367d6992b3c34bea1b1b69b9843ab3910076
mosescode1 <emosescode@gmail.com>
2024-05-27 11:47:48 +0100

    places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,7 +9,8 @@ from models.place import Place
 from models.user import User
 
 
-@app_views.route("/cities/<city_id>/places", strict_slashes=False, methods=["GET"])
+@app_views.route("/cities/<city_id>/places",
+                 methods=["GET"], strict_slashes=False)
 def get_places(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -18,7 +19,8 @@ def get_places(city_id):
     return jsonify([place.to_dict() for place in city.places])
 
 
-@app_views.route('/places/<place_id>', strict_slashes=False, methods=["GET"])
+@app_views.route('/places/<place_id>',
+                 methods=["GET"], strict_slashes=False)
 def get_place(place_id):
     """Retrieves a Place object"""
     place = storage.get(Place, place_id)
@@ -27,7 +29,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', strict_slashes=False, methods=["DELETE"])
+@app_views.route('/places/<place_id>',
+                 methods=["DELETE"], strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
@@ -38,7 +41,8 @@ def delete_place(place_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/cities/<city_id>/places', strict_slashes=False, methods=["POST"])
+@app_views.route('/cities/<city_id>/places',
+                 methods=["POST"], strict_slashes=False)
 def create_place(city_id):
     """Creates a Place"""
     city = storage.get(City, city_id)
@@ -60,7 +64,8 @@ def create_place(city_id):
     return make_response(jsonify(place.to_dict()), 201)
 
 
-@app_views.route("/places/<place_id>", strict_slashes=False, methods=["PUT"])
+@ app_views.route("/places/<place_id>",
+                  methods=["PUT"], strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object"""
     place = storage.get(Place, place_id)
