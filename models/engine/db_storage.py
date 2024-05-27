cd95c81a925e9b6952d9899a18eb880916597d8a
SirJupiter <jupitertoby@gmail.com>
2024-05-27 11:03:20 +0100

    6. State

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -92,14 +92,12 @@ class DBStorage:
         Returns:
             The number of objects in storage
         """
+        return len(self.all(cls))
         # if cls:
-        #     return len(self.all(cls))
-        # return len(self.all())
-        if cls:
-            return self.__session.query(cls).count()
-        all_models = [classes[key] for key in classes.keys()]
-        return sum(
-            [self.__session.query(model).count() for model in all_models])
+        #     return self.__session.query(cls).count()
+        # all_models = [classes[key] for key in classes.keys()]
+        # return sum(
+        #     [self.__session.query(model).count() for model in all_models])
 
     def close(self):
         """call remove() method on the private session attribute"""
