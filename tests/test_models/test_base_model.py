ffeaba55ae24eb280f7e639dc621e930df1434ff
Henry4593 <jonyango4@gmail.com>
2024-05-27 16:14:24 +0300

    fixed bugs

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -85,6 +85,8 @@ class TestBaseModel(unittest.TestCase):
         tic = datetime.now()
         inst1 = BaseModel()
         toc = datetime.now()
+        self.assertEqual(tic, inst1.created_at)
+        self.assertEqual(toc, inst1.created_at)
         self.assertTrue(tic <= inst1.created_at <= toc)
         time.sleep(1e-4)
         tic = datetime.now()
