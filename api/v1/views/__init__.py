f11a2f9162110278d099135422cc874f27672191
shaun-saisi <saisishaun4@gmail.com>
2024-05-27 17:16:34 +0000

    added the states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Import other views here
+from api.v1.views.states import *
 
-from api.v1.views.index import *
