631bf7f8ad0ec6fb97d6822149c47b45b02bd245
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-27 23:45:07 +0100

    Update city.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -17,7 +17,7 @@ def get_cities_by_states(state_id):
     """get cities information in a specified state"""
     state = storage.get("State", state_id)
     if state is None:
-        return abort(404)
+        abort(404)
     cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
 
@@ -28,7 +28,7 @@ def get_city(city_id):
     """get city information """
     city = storage.get("City", city_id)
     if city is None:
-        return abort(404)
+        abort(404)
     return jsonify(city.to_dict())
 
 
@@ -42,7 +42,7 @@ def delete_city(city_id):
         storage.save()
         return jsonify({}), 200
     else:
-        return abort(404)
+        abort(404)
 
 
 @app_views.route('/states/<string:state_id>/cities/', methods=['POST'],
@@ -50,19 +50,19 @@ def delete_city(city_id):
 def post_city(state_id):
     """creates a city"""
     if request.content_type != 'application/json':
-        return abort(400, 'Not a JSON')
+        abort(400, 'Not a JSON')
     state = storage.get("State", state_id)
     if state is None:
-        return abort(404)
+        abort(404)
     if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        abort(400, 'Not a JSON')
     if 'name' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
+        abort(400, 'Missing name')
     kwargs = request.get_json()
     kwargs['state_id'] = state_id
     city = City(**kwargs)
     city.save()
-    return jsonify(city.to_dict()), 201
+    return make_response(jsonify(city.to_dict()), 201)
 
 
 @app_views.route('/cities/<string:city_id>', methods=['PUT'],
@@ -73,7 +73,7 @@ def put_city(city_id):
     if city is None:
         abort(404)
     if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        abort(400, 'Not a JSON')
     for attr, val in request.get_json().items():
         if attr not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, attr, val)
