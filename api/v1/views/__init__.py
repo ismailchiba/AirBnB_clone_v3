30cb29602fd17d5dd14bd2f95846ca9b17136b04
mosescode1 <emosescode@gmail.com>
2024-05-27 12:09:28 +0100

    added to __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,4 +11,5 @@ class Views:
     from api.v1.views.states import state, one_state, del_state, post_state, update_state
     from api.v1.views.amenities import get_amenities, one_amenity, remove_amenity, create_amenity, update_amenity
     from api.v1.views.places import get_places, get_place, delete_place, create_place
-    from api.v1.views.cities import get_cities
+    from api.v1.views.cities import get_cities, get_city, delete_city, create_city
+    from api.v1.views.users import get_users, get_user, delete_user, create_user, update_user
