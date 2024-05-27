0b238bc6a98d90b43702cf8cb03399453339576f
Kathyslate <mercyoyetunde98@gmail.com>
2024-05-27 03:59:51 -0700

    working?

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" holds class User"""
-from models.base_model import BaseModel, Base
+"""
+User Class for basemodel class
+"""
 import os
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, Float
 from hashlib import md5
@@ -9,23 +11,26 @@ storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
+    """User class handles all application users"""
+    if storage_type == "db":
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade='delete')
-        reviews = relationship("Review", backref="user", cascade='delete')
+
+        places = relationship('Place', backref='user', cascade='delete')
+        reviews = relationship('Review', backref='user', cascade='delete')
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """
+        initialize User Model
+        """
         super().__init__(*args, **kwargs)
 
     @property
