5cef98b7c75ccd96368658e1113ab32432a260cc
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-27 06:38:39 +0300

    count data

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,32 @@
 """ index routes """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+}
 
 @app_views.route("/status")
 def get_status():
     """Status of my API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def count_data():
+    """count data"""
+    total = {}
+    for k, v in classes.items():
+        total[k] = storage.count(v)
+    return jsonify(total)
