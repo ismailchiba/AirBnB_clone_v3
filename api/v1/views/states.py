bb863840a43282cba8abc331bd2d9b0ce8098aec
faykimanthi <faykimanthi@gmail.com>
2024-05-27 07:07:37 -0700

    task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,7 +13,7 @@ def get_all_states():
     """
     Retrieves the list of all State objects. 
     """
-    states + storage.all(State).values()
+    states = storage.all(State).values()
     state_list = [state.to_dict() for state in states]
     return jsonify(state_list)
 
@@ -34,7 +34,7 @@ else:
 @app_views.route('/states/<state_id>', methods-['DELETE'], strict_slashes-False)
  def delete_state(state_id):
     """
-
+    Deletes state
     """
     state = storage.get(State, state_id)
     if state:
@@ -48,7 +48,7 @@ else:
     @app_views.route('/states', methods-['POST'], strict_slashes-False)
  def create_state():
     """
-
+    Creates state
     """
     if request.content_type != 'application/json':
         return abort(404, 'Not a JSON')
@@ -67,7 +67,7 @@ else:
     @app_views.route('/states/<state_id>', methods-['PUT'], strict_slashes-False)
  def update_state(state_id):
     """
-
+    Updates state
     """
     if request.content_type !='application/json':
         return abort(400, 'Not a JSON')
