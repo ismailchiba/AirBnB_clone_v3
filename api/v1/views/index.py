efa8c35504499e5f8d9c6ba02b68c104ff6c28c9
Keegan <keeganjoubert@protonmail.com>
2024-05-27 23:05:42 +0000

    API

--- api/v1/views/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Index view for API"""
-from flask import jsonify
-from api.v1.views import app_views
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ Returns the status of the API """
-    return jsonify({"status": "OK"})
