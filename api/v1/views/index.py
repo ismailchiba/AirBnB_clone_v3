2acc3cc2cfa613a12218fba90dc4d2967ddbebf8
vishaylinp <vishaylinparshotam@gmail.com>
2024-05-27 02:41:57 -0700

    endpoint that retrieves numberof obj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,21 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """retrieves the number of each obj"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "userss": storage.count("User")})
