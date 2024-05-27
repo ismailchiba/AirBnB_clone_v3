5b1b947bcf80af4edf1d9511f86986ec6c2a7764
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-05-27 21:24:59 +0000

    work

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-Views for API
+views
 """
 
 from flask import Blueprint
+
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
@@ -12,5 +15,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
