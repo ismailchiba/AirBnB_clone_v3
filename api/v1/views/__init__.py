17c37ec54e43eeec7b022178cdc4aab81e01f90c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-05-27 04:44:39 +0100

    files to start API, reistering Blueprints

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+""" Create an instance of app_views """
+
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix = '/api/v1')
+
+app_views = Blueprint('app_views', url_prefix = '/api/v1')
 
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" initiate the return of 'status: OK'"""
 
 from api.v1.views import app_views
 from flask import jsonify
