5f356b814f03d1a92496d25ff1331e4646e0a647
Power70 <nwisuanu@gmail.com>
2024-05-27 13:32:40 +0100

    modify the api dirctory

--- api/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-"""It initializes the package 'api'."""
-    def __init__(self):
-        """An empty method"""
-        pass
--- api/v1/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-"""Initializes the package 'v1'."""
-    def __init__(self):
-        """An empty method with a placeholder"""
-        pass
