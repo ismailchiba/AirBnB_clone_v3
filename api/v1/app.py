e4e6064952ffafc2c45395443105d772e419b52a
mahmoudhmoud <jadmclarence@gmail.com>
2024-05-27 16:40:50 -0700

    yry

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,42 +1,38 @@
 #!/usr/bin/python3
-"""module"""
-from flask import Flask, make_response
+''' tab ikl '''
+
+from flask import Flask, jsonify
 from flask_cors import CORS
-import json
-from api.v1.views import app_views
 from os import getenv
+
+from api.v1.views import app_views
+from models import storage
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
-host = getenv("HBNB_API_HOST")
-port = getenv("HBNB_API_PORT")
-cors = CORS(
-    app,
-    resources={r"/*": {"origins": "0.0.0.0"}}
-    )
 
 
 @app.teardown_appcontext
-def teardown(err):
-    """teardown"""
-    from models import storage
-
+def teardown(exception):
+    ''' sawab dala '''
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(err):
-    """ 404 Error """
-    res = {'error': "Not found"}
-    response = make_response(json.dumps(res), 404)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+def handle_404(exception):
+    ''' kad lmochkil '''
+    data = {
+        "error": "Not found"
+    }
 
+    resp = jsonify(data)
+    resp.status_code = 404
 
-if __name__ == "__main__":
-    """executable"""
-    host = "0.0.0.0" if host is None else host
-    port = "5000" if port is None else port
-    app.run(host=host, port=port, threaded=True)
+    return(resp)
+
+if __name__ == '__main__':
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
-"""innddex api"""
+''' dyal litam '''
+
+from flask import jsonify
 from api.v1.views import app_views
 
+from models import storage
 
-@app_views.route("/status")
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    """status"""
-    return {"status": "OK"}
-
-
-@app_views.route("/stats")
-def stats():
-    """api"""
-    from models import storage
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-
-    count = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User),
+    '''  afal kad tam '''
+    data = {
+        "status": "OK"
     }
-    return count
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    ''' wilaya litam '''
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
