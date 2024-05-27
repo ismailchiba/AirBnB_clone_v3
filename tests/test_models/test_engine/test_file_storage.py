a701467d0b523b8ae53bb1c897de46f0359694c0
Sododa <sophieododa@gmail.com>
2024-05-27 13:47:36 +0000

    airbnb

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,48 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """test method for containing instance"""
+        storage = FileStorage()
+
+        storage.reload()
+
+        state_data = ("name": "Mombasa")
+
+        storage.reload = State(**state_data)
+
+        retrieved_state = storage.get(State, state_instance.id)
+
+        self.assertEqual(state_instance, retrieved_state)
+
+        fake_state_id = storage.get(State, 'fake_id')
+        self.assertEqual(fake_state_id, None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """tests for db storage"""
+        storage = FileStorage
+
+        storage.reload()
+
+        state_data = ("name": "Migori")
+
+        state_instance = State(**state_data)
+
+        storage.new(state_instance)
+
+        city_data = ("name": "sophie", "state_id": state_instance.id)
+
+        city_instance = City(**city_data)
+
+        storage.new(city_instance)
+
+        storage.save()
+
+        state_occurrence = storage.count(State)
+        self.assertEqual(state_occurrence, len(storage.all(State)))
+
+        all_occurrence = storage.count()
+        self.assertEqual(all_occurrence, len(storage.all()))
