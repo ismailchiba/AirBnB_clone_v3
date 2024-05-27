d2ed861c432bcfbcf490b1191884e7bebc0d019f
gboigwe <geakande@gmail.com>
2024-05-28 00:25:11 +0100

    Status of API

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,9 +9,11 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
