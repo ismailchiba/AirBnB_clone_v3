a309560fda96eff7a350d0c84f4e6107967371c7
shaun-saisi <saisishaun4@gmail.com>
2024-05-27 13:49:01 +0000

    the endpoint of the api to receive the number of each object by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,33 @@
-#!/usr/bin/python3xx
+#!/usr/bin/python3
 '''api status'''
-import models
-from models import storage
-from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
 
 
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return stuff'''
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def api_status():
+    '''return API status'''
     return jsonify(status='OK')
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def api_stats():
+    '''return JSON responses'''
+    classes = {
+        'states': State,
+        'users': User,
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review
+    }
+    stats = {key: storage.count(cls) for key, cls in classes.items()}
+    return jsonify(stats)
+
