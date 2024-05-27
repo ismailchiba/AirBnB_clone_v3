3c3393f5430f231607b7d286fdb5b9851e0aebb9
Brio-kchr <b.kchr@gmail.com>
2024-05-27 07:33:44 +0000

    Creating Flask server application for AirBnB clone v3

---
+++ api/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Starts AirBnB Flask main application
+"""
+
+from flask import Flask
+from models import storage
+from app.v1.views import app_views
+from os import getenv
+
+API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+API_PORT = getenv('HBNB_API_PORT', 5000)
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_api(exception):
+	""" Closes storage if we encounter an exception """
+	storage.close()
+
+
+if __name__ == "__main__":
+	app.run(host=API_HOST, port=API_PORT, threaded=True)
