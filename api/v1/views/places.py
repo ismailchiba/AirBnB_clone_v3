b97159062aff495e2daac345f1d58edfde6e91b0
Kathyslate <mercyoyetunde98@gmail.com>
2024-05-27 02:17:05 -0700

    swaqlok

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" route for places id"""
+""" route for handling Place objects """
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
 from models.place import Place
@@ -8,7 +8,9 @@ from models.place import Place
 @app_views.route("/cities/<city_id>/places", methods=["GET"],
                  strict_slashes=False)
 def places_by_city(city_id):
-    """retrieves all Place objects by city"""
+    """
+    retrieves all Place objects by city
+    """
     place_list = []
     city_obj = storage.get("City", str(city_id))
     for obj in city_obj.places:
