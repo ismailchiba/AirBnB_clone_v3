bbd45170d61c8996432fc6c68e1c53a26b0c19d5
GreyWhat <greywhat2002@gmail.com>
2024-05-27 02:21:26 -0700

    added custom 404 page not found to app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """create variable"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def teardown_app(obj):
     """registers a function when app contect is torn down"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found_page(error):
+    """return custom json fomatted 404 page not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
