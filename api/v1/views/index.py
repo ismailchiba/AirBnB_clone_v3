df01caead619916f76981c18fab04f36fd1eb6a9
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-27 01:41:24 +0100

    add /api/v1/stats return count of each object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,26 @@ from flask import Flask, jsonify
 from models import storage
 
 
+
 @app_views.route('/status', strict_slashes=False)
 def hbnb_status():
     """hbnb Status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnb_stats():
+    """ Retrieves the number of each objects by type """
+    hbnb_class = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+
+    stats = {}
+    for k, val in hbnb_class.items():
+        stats[k] = storage.count(val)
+
+    return jsonify(stats)
