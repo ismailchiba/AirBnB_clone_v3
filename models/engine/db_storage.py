05e6d7bcd0f5bd10fe822aac4bdc2b571558bd13
Timzblue12 <timzblue.gmail.com>
2024-05-27 17:07:48 -0700

    fixed pycodestyle errors

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -23,7 +23,7 @@ classes = {"Amenity": Amenity, "City": City,
 
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """interacts with the MySQL database"""
     __engine = None
     __session = None
 
@@ -78,36 +78,6 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-<<<<<<< HEAD
-        """
-        Returns the object based on the class name and its ID, or
-        None if not found
-        """
-        if cls not in classes.values():
-            return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
-
-        return None
-
-    def count(self, cls=None):
-        """
-        count the number of objects in storage
-        """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
-
-        return count
-=======
         """Return one object or `None` if not found"""
         objects = self.__session.query(cls)
         for obj in objects:
@@ -122,4 +92,3 @@ class DBStorage:
             if cls is None or cls is classes[clss] or cls is clss:
                 total += len(self.__session.query(classes[clss]).all())
         return total
->>>>>>> storage_get_count
