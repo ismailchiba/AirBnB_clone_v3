eac88fdc518104db254482e1aaca07b431c8d6b3
gboigwe <geakande@gmail.com>
2024-05-28 21:42:09 +0100

    Creating an endpoint to retrieve the objects type:

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,6 @@ retrieves the number of each objects by type
 """
 
 
-from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -14,6 +13,7 @@ from models.state import State
 from models.user import User
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
