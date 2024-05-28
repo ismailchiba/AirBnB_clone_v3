c5fae5f73f956553494d09b6e44eb6233db5cf90
Kokiben <bennassarkautar@gmail.com>
2024-05-28 11:46:14 -0700

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,20 +2,20 @@
 """RESTFul API"""
 from flask import jsonify, abort, request
 from models import storage
-from api.v1.views import app_views
 from models.user import User
+from api.v1.views import app_views
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def list_all_users():
-    """Retrieve the list of all User objects"""
+    """list of all User"""
     users = storage.all(User).values()
     return jsonify([user.to_dict() for user in users]), 200
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user():
-    """Create a new User object"""
+    """new User"""
     if not request.is_json:
         abort(400, description="Not a JSON")
     data = request.get_json()
@@ -23,15 +23,15 @@ def create_user():
         abort(400, description="Missing email")
     if 'password' not in data:
         abort(400, description="Missing password")
-    new_user = User(**data)
-    storage.new(new_user)
+    nw_user = User(**data)
+    storage.new(nw_user)
     storage.save()
-    return jsonify(new_user.to_dict()), 201
+    return jsonify(nw_user.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user_by_id(user_id):
-    """Retrieve a User object by user_id"""
+    """get User"""
     user = storage.get(User, user_id)
     if not user:
         abort(404)
@@ -40,7 +40,7 @@ def get_user_by_id(user_id):
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user_by_id(user_id):
-    """Delete a User object by user_id"""
+    """ delete User """
     user = storage.get(User, user_id)
     if not user:
         abort(404)
@@ -51,16 +51,16 @@ def delete_user_by_id(user_id):
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user_by_id(user_id):
-    """Update a User object by user_id"""
+    """update User"""
     user = storage.get(User, user_id)
     if not user:
         abort(404)
     if not request.is_json:
         abort(400, description="Not a JSON")
     data = request.get_json()
-    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+    ig_key = ['id', 'email', 'created_at', 'updated_at']
     for key, value in data.items():
-        if key not in ignore_keys:
+        if key not in ig_key:
             setattr(user, key, value)
     storage.save()
     return jsonify(user.to_dict()), 200
