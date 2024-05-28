1f75c3f1fba7ab16d274304bcf5cd9f1dc9baf4e
Chiamablessing <chiamablessing@gmail.com>
2024-05-28 02:24:44 +0100

    Fix error

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,8 +4,6 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
-from datetime import datetime
-import uuid
 
 
 @app_views.route('/states/', strict_slashes=False)
