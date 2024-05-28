ce5abb3c4c1597ad213bc7f184f67e50647edbaf
K-kare <kamunawangari@gmail.com>
2024-05-28 08:33:35 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Module create a flask app and register the blueprint app_views to flask instance app. 
-"""
+""" Flask Application"""
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
