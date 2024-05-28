d5eed5fbc9ae647ea66596171e8c7d3535b41c41
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-28 01:52:51 +0100

    Update  v2.2

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -10,8 +10,6 @@ from flask import abort, jsonify, make_response, request
 
 @app_views.route(
     'places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
-@swag_from(
-    'documentation/place_amenity/get_places_amenities.yml', methods=['GET'])
 def get_place_amenities(place_id):
     """
     Retrieves the list of all Amenity objects of a Place
@@ -34,9 +32,6 @@ def get_place_amenities(place_id):
 @app_views.route(
     '/places/<place_id>/amenities/<amenity_id>',
     methods=['DELETE'], strict_slashes=False)
-@swag_from(
-    'documentation/place_amenity/delete_place_amenities.yml',
-    methods=['DELETE'])
 def delete_place_amenity(place_id, amenity_id):
     """
     Deletes a Amenity object of a Place
@@ -67,9 +62,6 @@ def delete_place_amenity(place_id, amenity_id):
 @app_views.route(
     '/places/<place_id>/amenities/<amenity_id>',
     methods=['POST'], strict_slashes=False)
-@swag_from(
-    'documentation/place_amenity/post_place_amenities.yml',
-    methods=['POST'])
 def post_place_amenity(place_id, amenity_id):
     """
     Link a Amenity object to a Place
