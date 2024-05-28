66d2ddf991be5b67553c0602b25cd8a7047be491
vale-hash <newlechim@gmail.com>
2024-05-28 19:29:59 +0100

    api started aded blue print and status route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,9 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Return json status"""
-    return jsonify({"status": "OK"})
+    return jsonify(
+        {
+            "status": "OK"
+        })
 
 
