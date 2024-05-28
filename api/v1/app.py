cb094acfccd3411e15d3367b1ba2f04a93d4acd3
K-kare <kamunawangari@gmail.com>
2024-05-28 02:32:36 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-""" Create a flask app and register the blueprint app_views """
+"""
+Create a flask app and register the blueprint app_views 
+"""
+
 from flask import Flask
-from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
