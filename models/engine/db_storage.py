ffbb41f7db051e9fdbabea966923fdabac2a9cfc
AhMeDMaGDY28 <ray2legand@gmail.com>
2024-05-28 16:53:16 +0300

    making the db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -21,7 +21,7 @@ classes = {"Amenity": Amenity, "City": City,
 
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """interacts with the MySQL database"""
     __engine = None
     __session = None
 
@@ -71,6 +71,19 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
+    def get(self, cls, id):
+        """retrieve an object"""
+        if cls and id:
+            key = cls.__name__ + '.' + id
+            return self.__session.get(cls, id)
+        return None
+    
+    def count(self, cls=None):
+        """count the number of class or all"""
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
+    
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
