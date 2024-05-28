42995b5e75577c3504499336a40a12608b8a7de1
Benjamin Torfu <btorfu@proton.me>
2024-05-28 20:56:48 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ An index module
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -13,4 +14,32 @@ def stat():
     '''
     returns the status of the page in json format
     '''
-    return jsonify({"status": "OK"})
+    return jsonify({
+        "status": "OK"
+        })
+
+@app_views.route("/stats", strict_slashes=False)
+def objects():
+    '''
+    counts the number of objects
+    '''
+    objs = {}
+    classes = {
+            "amenities": "amenities",
+            "cities": "cities",
+            "places": "places",
+            "reviews": "reviews",
+            "states": "states",
+            "users": "users"
+            }
+
+    for class_name, class_property in classes.items():
+        count = storage.count(class_name)
+        objs[class_property] = count
+
+    return jsonify(objs)
+@app_views.route("/nop", strict_slashes=False)
+def nop():
+    return jsonify({
+        "error": "Not found"
+        })
