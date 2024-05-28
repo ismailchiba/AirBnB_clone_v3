2143fc81482d55902c63ea980ab6a16ab26fd70c
gboigwe <geakande@gmail.com>
2024-05-28 16:30:49 +0100

    The status of API

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,24 +8,24 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+
 @app.teardown_appcontext
 def teardown_db(exception):
-    """
-    Close the storage connection
-    """
+    """Close the storage connection"""
     storage.close()
 
 
-if __name__ == "__main__":
-    """
-    Run the Flask application
-    """
+if __name__ == '__main__':
+    """Run the Flask application"""
     host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
     port = os.getenv('HBNB_API_PORT', default='5000')
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """
 Index View for API
 """
+
 from api.v1.views import app_views
 from flask import jsonify
 
