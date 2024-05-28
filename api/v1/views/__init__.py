5f799ef5d664eaab38520d5b1aceebdfb9d966db
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-05-28 05:11:17 +0300

    task 3

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import everything from api.v1.views.index
+from api.v1.views.index import *
