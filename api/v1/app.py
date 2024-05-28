e945a77c9a0c9e97ba9df4c1f6386af040dc8c1b
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-05-28 03:16:23 +0100

    APi

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,6 +25,21 @@ def teardown_flask(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
+
+
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
