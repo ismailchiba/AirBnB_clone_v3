6c8d9a3bc3ed52cbd2ef1ed3b2ac5beba481ac7f
Kokiben <bennassarkautar@gmail.com>
2024-05-28 13:23:46 -0700

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """API"""
 from flask import Blueprint
+
+# Create a Blueprint instance with url prefix /api/v1
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import of everything in the package api.v1.views.index
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.index import *
@@ -8,7 +13,3 @@ from api.v1.views.amenities import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.users import *
-
-
-# Create a Blueprint instance with url prefix /api/v1
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
