1728f284e0e830ea5bc0894d0597d453bba96679
Henry4593 <jonyango4@gmail.com>
2024-05-29 07:02:03 +0300

    fixed bugs

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,9 @@ related to places, amenities, users, and more. It provides a structured
 way to organize and manage API endpoints within your Flask application.
 """
 from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
@@ -14,6 +17,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
