9b4c4f63537918bd3df0e306ce6ac4d59437260e
zguaidh <zguaidhabiba@gmail.com>
2024-05-29 00:10:48 -0700

    task 3: pycodestyle fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 # creating a route /status on the object app_views that returns a JSON
 @app_views.route("/status", methods=["GET"])
 def status():
