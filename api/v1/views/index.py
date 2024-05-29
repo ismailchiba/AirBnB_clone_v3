e1ea8e72134d9f96280798d5c01164216eb192ff
Kingsley Okpo <kngsley2018@gmail.com>
2024-05-29 13:51:30 +0100

    1-Makefile

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,15 @@ this is the indedx module
 """
 
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status')
 def status():
@@ -13,3 +21,16 @@ def status():
         'status': 'OK'
     }
     return response
+
+@app_views.route('/stats')
+def stats():
+    """returns status : OK"""
+    obj ={
+        "amenities":storage.count(Amenity), 
+        "cities": storage.count(City), 
+        "places": storage.count(Place), 
+        "reviews": storage.count(Review), 
+        "states": storage.count(State), 
+        "users": storage.count(User)
+    }
+    return jsonify(obj)
