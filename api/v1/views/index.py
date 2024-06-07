1c2ea164418699706e054ea471623a47d26231ae
JohnIanOngayi <ongayijohnian@gmail.com>
2024-06-07 12:56:27 +0300

    Fixed /api/v1/status route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,10 @@ Defines route for status used to check api availability
 """
 
 from api.v1.views.__init__ import app_views
-import json
+from flask import jsonify
 
 
 @app_views.route("/status", strict_slashes=False)
 def check_status():
     """Returns status of api"""
-    return json.dumps({"status": "OK"})
+    return jsonify({"status": "OK"})
