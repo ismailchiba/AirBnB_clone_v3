afaf3ea44bb71eabada5fe7bc1a8a964f24daf0c
MubarakXBT <Kazeemmubarak@gmail.com>
2024-06-07 21:19:54 +0100

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status')
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
