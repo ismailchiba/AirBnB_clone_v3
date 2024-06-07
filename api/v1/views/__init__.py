a3145fa9de4263cd665e91201b3de789416d03e5
MubarakXBT <Kazeemmubarak@gmail.com>
2024-06-07 21:13:46 +0100

    Pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,6 @@
 """
 __init__ for app_views
 """
-from flask import Blueprint 
-
+from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,18 +3,12 @@
 module index
 - app_views router
 """
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
-@app_views.route('/status')
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """ Status of API """
     return jsonify({"status": "OK"})
-
-@app_views.stat('/stats')
-def stats():
-    dic = {}
-    obj = ["amenities", "cities", "places", "reviews", "states", "users"]
-    for key in obj:
-        dic[key] = storage.count(key)
-    return dic
