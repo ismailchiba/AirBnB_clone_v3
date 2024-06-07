e9e3ba096f1b73862d06a6dffba4182ce3d61404
RadouaneAbn <radouaneabn@gmail.com>
2024-06-07 20:14:03 +0100

    4

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,18 +4,12 @@ from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import environ
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-HOST = "0.0.0.0"
-PORT = 5000
-
-if getenv("HBNB_API_HOST"):
-    HOST = getenv("HBNB_API_HOST")
-if getenv("HBNB_API_PORT"):
-    PORT = getenv("HBNB_API_PORT")
 
 
 @app.teardown_appcontext
@@ -31,4 +25,7 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
+    host = environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = environ.get("HBNB_API_PORT", "5000")
     app.run(host=HOST, port=PORT, threaded=True)
+    
