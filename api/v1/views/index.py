bbf686fcebefe61d91ffb6ea453e07f4999422fd
MubarakXBT <Kazeemmubarak@gmail.com>
2024-06-07 17:14:33 +0100

    documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,16 @@ module index
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.stat('/stats')
+def stats():
+    dic = {}
+    obj = ["amenities", "cities", "places", "reviews", "states", "users"]
+    for key in obj:
+        dic[key] = storage.count(key)
+    return dic
