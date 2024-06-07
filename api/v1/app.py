ba9584ca2722e57dc22592f17187043753faa9f8
MubarakXBT <Kazeemmubarak@gmail.com>
2024-06-07 15:32:08 +0100

    Task 2

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
+App Module
 """
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def app_teardown(obj):
-    """"""
+    """ closes the datatbase"""
     storage.close()
 
 if __name__=="__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
+module index
+- app_views router
 """
 from api.v1.views import app_views
 from flask import jsonify
 
-@my_blueprint.route('/status')
-def status:
+@app_views.route('/status')
+def status():
     return jsonify({"status": "OK"})
