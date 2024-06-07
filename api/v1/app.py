2e64fc5cf1a191527c3f367508207166d9189c73
RadouaneAbn <radouaneabn@gmail.com>
2024-06-07 20:08:02 +0100

    4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-""" Start a Flask web application """
-from os import getenv
+""" This script creates an api flask for hbnb project"""
+from api.v1.views import app_views
+from flask_cors import CORS
+from os import environ
 from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+"""<------ view routes ------>"""
 app.register_blueprint(app_views)
-HOST = "0.0.0.0"
-PORT = 5000
-
-if getenv("HBNB_API_HOST"):
-    HOST = getenv("HBNB_API_HOST")
-if getenv("HBNB_API_PORT"):
-    PORT = getenv("HBNB_API_PORT")
+CORS(app, origins='0.0.0.0')
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ Closes the storage session """
+def teardown_db(exception=None):
+    """ This function called each time the database is updated
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """ Handles the 404 error """
-    return jsonify({"error": "Not found!"}), 404
+def not_found(error):
+    """ This function handels status_code '404'
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host=HOST, port=PORT, threaded=True)
+    host = environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = environ.get("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
