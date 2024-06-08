61001384af93438c6b6bf2e0b91a6483c6a2564d
john-otienoh <johnteclaire@gmail.com>
2024-06-08 03:32:11 -0700

    custom error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Create Flask app"""
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -10,7 +10,19 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """have to design a “404 page"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    '''Custom error page'''
+    return jsonify({"error", "Not found"}, 404)
+
+
 if __name__ == "__main__":
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(host=HOST, port=PORT, threaded=True, debug=True)
