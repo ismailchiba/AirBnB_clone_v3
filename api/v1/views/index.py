232202eddb4dcb11f8b657f8906d41d2fae4a8a6
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-06-09 16:15:11 +0300

    pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """gets status in json format"""
