56c3a8deaebe17e84069481731adc53e37b32590
ZainAlabidin <zynly9303@gmail.com>
2024-06-09 23:38:06 +0300

    ok

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,16 +7,18 @@ import os
 
 app = Flask(__name__)
 
-#Register the plueprint
+# Register the plueprint
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(self):
-    """ handle @app.teardown_appcontext that calls storage.close() """
+    """handle @app.teardown_appcontext that calls storage.close()"""
+
 
 storage.close()
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,8 @@
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'])
+
+@app_views.route("/status", methods=["GET"])
 def status():
-    """ route for status page """
+    """route for status page"""
     return jsonify({"status": "OK"})
