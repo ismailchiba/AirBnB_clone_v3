8dd8624308f8c18bec56fba4ac26677078f9d18f
alaa <alaamglal@gmail.com>
2024-06-09 13:50:42 -0700

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,6 +8,7 @@ from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
+from models.review import Review
 from models.state import State
 from models.user import User
 
@@ -27,6 +28,7 @@ def count_obj():
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
         "places": storage.count(Place),
+        "reviews": storage.count(Review),
         "states": storage.count(State),
         "users": storage.count(User)
         })
