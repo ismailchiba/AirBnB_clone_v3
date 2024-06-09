dfad7cd87589c5ec5b687a846d12e750f2bebdb8
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-06-09 17:32:33 +0100

    errorhandler added to app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,9 +2,10 @@
 """
     The API for the AirBnB Clone
 """
-from flask import Flask
-from models import storage
 from api.v1.views import app_views
+from flask import Flask, Response
+import json
+from models import storage
 from os import getenv
 
 
@@ -19,6 +20,16 @@ def teardown(exception):
         The teardown function
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+        Handles Resource Not Found
+    """
+    error_dict = {"error": "Not found"}
+    json_format = json.dumps(error_dict, indent=2)
+    return Response(json_format, mimetype="application/json", status=404)
 
 
 if __name__ == "__main__":
