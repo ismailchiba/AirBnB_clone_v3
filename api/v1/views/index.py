f7fbd5992f6dc28b15142edf83b97f92e50e6526
ZainAlabidin <zynly9303@gmail.com>
2024-06-09 22:50:51 +0300

    ok

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" create routes """
+
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """ route for status page """
+    return jsonify({"status": "OK"})
