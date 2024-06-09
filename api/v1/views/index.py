dbc56c71264990daefdb02262cc1e4d2f3b07487
ZainAlabidin <zynly9303@gmail.com>
2024-06-10 00:40:43 +0300

    ok

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 """ create routes """
 
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"])
 def status():
     """route for status page"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    """Create an endpoint that retrieves the number of each objects by type"""
+    counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+    return jsonify(counts)
