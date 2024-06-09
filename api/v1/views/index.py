9ea461239a4941437a2e37aa6e189ef2fa692e9a
ZainAlabidin <zynly9303@gmail.com>
2024-06-10 01:45:53 +0300

    ok

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -19,16 +19,14 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", methods=["GET"])
-def stats():
-    """Create an endpoint that retrieves the number of each objects by type"""
-    counts = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User),
-    }
-    json_str = json.dumps(counts, indent=2) + "\n"
-    return json_str
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
