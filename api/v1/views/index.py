6f1a40418688e85d9b8e559ff804d16f3a77d7d5
AyaG96 <aya.gamal.zaher@gmail.com>
2024-06-10 06:08:32 -0700

    added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import jsonify
-@app_views.route('/status', methods=['GET'])
+from flask import Flask, Blueprint, jsonify
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns a JSON: "status": "OK"."""
     return jsonify({"status": "OK"})
