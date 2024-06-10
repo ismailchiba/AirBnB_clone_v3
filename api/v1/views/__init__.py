55e24820b38d0e712578241c2bfe83f452aee20c
kharboutmanel98 <kharbout2017@gmail.com>
2024-06-10 10:18:44 +0000

    commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,9 +6,11 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.user import User
 
 """import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.users import *
