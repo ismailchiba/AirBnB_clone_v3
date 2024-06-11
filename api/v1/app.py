b878d5adad2f9ec73fdfac368ffeb76b0a6f1c2d
chudistar01 <christianigwebuike31@gmail.com>
2024-06-10 20:00:52 -0700

    airbnb

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Create flask app
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -18,7 +18,16 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """
+    error handler
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(debug = True, host=host, port=port, threaded=True)
