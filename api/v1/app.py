011e34700e920cc2e71533e1fac67ff3be3bc168
ZainAlabidin <zynly9303@gmail.com>
2024-06-11 05:59:23 +0300

    ok

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # Register the plueprint
 app.register_blueprint(app_views)
 
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
