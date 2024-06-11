8a37a0cb2cd586626690d7e51638bf6a726b2e32
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-06-11 12:47:10 +0300

    uodated  the file api/v1/views/index.py to perform CRUD endpoints for the State model, registered the app_views blueprint in api/v1/app.py; i also successfuly imported the app_views blueprint in api/v1/views/__init__.py, you can use any API tool like postman to to verify the endpoints after starting the application(python3 -m api.v1.app)

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,15 @@ from models import storage
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def teardodwn_db(exception):
+    """teardown"""
+    storage.close()
 
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,58 @@
 #!/usr/bin/python3
 """defines api status"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify, request, abort
+from models import storage
+from models.state import State
 
 
-@app_views.route('/status')
-def api_status():
-    """return route"""
-    response = {"status": "OK"}
-    return jsonify(response)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """"retrieve the list of all state objects"""
+    all_states = storage.all(State) # dictionary of all states
+    states_list = [state.to_dict() for state in all_states.values()]
+    return jsonify(states_list)
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """retrieve a state object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """create a new state object"""
+    if not request.json or 'name' not in request.json:
+        abort(400, 'Not a JSON')
+    if not request.json['name'] or not isinstance(request.json['name'], str):
+        abort(400, 'Missing name')
+    state = State(**request.get_json())
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """update a state object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    for key, value in request.json.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def dlete_state(state_id):
+    """delete a state object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200 | '', 204
