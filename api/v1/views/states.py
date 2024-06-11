7cbf242709686e1c79b22f30aa5c4c6248871f68
favour orji <favour@titanname.com>
2024-06-11 10:54:06 +0000

    add

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""states api"""
-from flask import Flask
+"""states"""
+from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 from datetime import datetime
 import uuid
-from api.v1.views import api_views
+
 
 @app_views.route('/states/', methods=['GET'])
 def list_states():
