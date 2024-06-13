ee4699459233c554367ff86698fbd9dd224bc37d
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-13 04:29:36 -0700

    correct status code returned

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -44,7 +44,7 @@ def delete_state(state_id=None):
 
     storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+    return {}, 200
 
 
 @app_views.route("/states", methods=['POST'])
