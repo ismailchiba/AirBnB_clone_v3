f9c388b8c4f6132c672b05ce5fc9a2d003160872
davidddeveloper <programmingdonebydavid@gmail.com>
2024-06-17 05:37:26 -0700

    cors fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,13 @@
 """
 import os
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 
