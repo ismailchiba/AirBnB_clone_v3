76e7b27c8d5a3eec6a4fe33481d3f395aa7e67b9
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 02:09:09 -0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-from flask import jsonify
+"""App module using Flask"""
+
+
+from flask import Flask, Blueprint, jsonify, request
 from api.v1.views import app_views
 from models import storage
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def api_status():
-    return (jsonify({"status": "OK"}))
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
-def object_count():
-    classes = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
-    key_names = ["amenities", "cities", "places", "reviews", "states", "users"]
-    data = {}
-    index = 0
-    for type in classes:
-        count = storage.count(type)
-        data[key_names[index]] = count
-        index += 1
-    return (jsonify(data))
+@app_views.route('/status', methods=['GET'])
+def statusRoute():
+    """Status Route"""
+    if request.method == 'GET':
+        return jsonify({
+        "status": "OK"
+    })
+
+
+@app_views.route('/stats')
+def count():
+    """ Count objects """
+    dictob = {}
+    clss = {
+        "Amenity": "amenities",
+        "Place": "places",
+        "State": "states",
+        "City": "cities",
+        "Review": "reviews",
+        "User": "users"
+    }
+
+    for cls in clss:
+        count = storage.count(cls)
+        dictob[clss.get(cls)] = count
+    return jsonify(dictob)
