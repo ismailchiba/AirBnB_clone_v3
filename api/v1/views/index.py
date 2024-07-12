b9d7e57efab955dbd1089a7d6ca1139b8c85926f
Dtbissy <isaactheedwards@gmail.com>
2024-07-12 14:00:04 -0500

    PEPsi8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def status():
     status = {
-        "status" : "OK"
+        "status": "OK"
     }
+
+    
     return jsonify(status)
