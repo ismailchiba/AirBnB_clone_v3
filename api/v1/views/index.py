1ce15950f95ed6bbf85b0635fa7a3d6c08eb4a8d
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-13 15:08:45 +0000

    testing changes to index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -18,13 +24,11 @@ def status():
 def stats():
     """This should return a JSONIfird stat list"""
     stats = {
-        "amenities": storage.count("amenities"),
-        "cities": storage.count("cities"),
-        "countries": storage.count("countries"),
-        "images": storage.count("images"),
-        "places": storage.count("places"),
-        "reviews": storage.count("reviews"),
-        "states": storage.count("states"),
-        "users": storage.count("users")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(stats)
