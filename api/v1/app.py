18f760fc9f6d1619091a0b5aaff6fc89ddc5b439
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-13 15:19:17 +0000

    changed response type in app.py 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is our flask instance"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -17,7 +17,8 @@ def teardown(exception):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return jsonify({"error": "Not found"})
+    """returns a 404 response and a json answer"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
