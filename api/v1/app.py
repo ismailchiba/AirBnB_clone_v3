8e6f9e17598ac3634ce1cf7cae0784f75ec614dd
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-13 15:16:13 +0000

    adding 404 handling to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is our flask instance"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +14,10 @@ def teardown(exception):
     """teardown function"""
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"error": "Not found"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
