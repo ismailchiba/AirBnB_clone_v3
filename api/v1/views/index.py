9a28bade74d9fbdd85f66fb7b69e27f1c374b6ac
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-13 14:56:16 +0000

    pycodestyle index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,17 +13,18 @@ def status():
 
     return jsonify(status)
 
+
 @app_views.route('/stats')
 def stats():
     """This should return a JSONIfird stat list"""
     stats = {
-  "amenities": storage.count("amenities"), 
-  "cities": storage.count("cities"),
-  "countries": storage.count("countries"),
-  "images": storage.count("images"),
-  "places": storage.count("places"),
-  "reviews": storage.count("reviews"),
-  "states": storage.count("states"),
-  "users": storage.count("users")
-}
+        "amenities": storage.count("amenities"),
+        "cities": storage.count("cities"),
+        "countries": storage.count("countries"),
+        "images": storage.count("images"),
+        "places": storage.count("places"),
+        "reviews": storage.count("reviews"),
+        "states": storage.count("states"),
+        "users": storage.count("users")
+    }
     return jsonify(stats)
