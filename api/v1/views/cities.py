67abc70b9699ff93a3252db784488b30ed4fb2fd
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-14 17:23:21 -0500

    changed variable names for returned city and state objects

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -13,10 +13,10 @@ def get_cities_by_state(state_id):
     """
     Retrieves a list of all cities in specified state
     """
-    state_obj = storage.get("State", state_id)
-    if not state_obj:
+    state = storage.get("State", state_id)
+    if not state:
         abort(404)
-    cities = [city.to_dict() for city in state_obj.cities]
+    cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
@@ -24,20 +24,20 @@ def get_city_by_id(city_id):
     """
     Retrieves a specific city by ID
     """
-    city_obj = storage.get("City", city_id)
-    if not city_obj:
+    city = storage.get("City", city_id)
+    if not city:
         abort(404)
-    return jsonify(city_obj.to_dict())
+    return jsonify(city.to_dict())
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """
     Deletes a specific city by ID
     """
-    city_obj = storage.get("City", city_id)
-    if not city_obj:
+    city = storage.get("City", city_id)
+    if not city:
         abort(404)
-    storage.delete(city_obj)
+    storage.delete(city)
     return jsonify({}), 200
 
 @app_views.route('/cities', methods=['POST'], strict_slashes=False)
@@ -54,7 +54,7 @@ def post_city(state_id):
         abort(400, 'Missing name')
     city = City(**request.get_json())
     city.state_id = state.id
-    city.save()
+    storage.save()
     return jsonify(city.to_dict()), 201
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
