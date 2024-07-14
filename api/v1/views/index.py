bd071ba6c78c7bdfe4785d9e5ee9d4ee706ce0a2
popstarmav <cj.johnson2017@gmail.com>
2024-07-14 16:18:35 -0500

    added models.classes import class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,14 +4,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.base_model import classes
-
+from models.classes import classes
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
@@ -19,5 +17,4 @@ def get_stats():
     obj_counts = {}
     for cls_name, cls in classes.items():
         obj_counts[cls_name] = storage.count(cls)
-
     return jsonify(obj_counts)
