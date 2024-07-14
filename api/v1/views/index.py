cf94e655237d4934d7f52a4df74e18148b61d1a8
LJThao <yengfong.thao@atlasschool.com>
2024-07-14 05:41:51 +0000

    started API

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Index for Route Module"""
+
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})
