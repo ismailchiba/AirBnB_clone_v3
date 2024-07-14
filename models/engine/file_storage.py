547d45e340082cf09fc5c1242becda4fc5fa134a
LJThao <yengfong.thao@atlasschool.com>
2024-07-14 04:53:18 +0000

    updated file storage with def get and count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve an object"""
+        cls_name = cls.__name__ if not isinstance(cls, str) else cls
+        key = f"{cls_name}.{id}"
+        return self.__objects.get(key)
+    
+    def count(self, cls=None):
+        """count the number of objects"""
+        return len(self.all(cls))
+    
+
