49ee1ba7261f6b14124dcd80021eef7667cfdda6
aallm004 <abigailrosea10@gmail.com>
2024-07-14 13:43:27 -0500

    updated reload in file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import os
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -67,16 +68,28 @@ class FileStorage:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
-        try:
-            with open(self.__file_path, 'r') as f:
-                jo = json.load(f)
-            for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except FileNotFoundError:
-            print("File not found. Handle this case.")
-        except json.JSONDecodeError:
-            print("Invalid JSON format. Handle this case.")
+        """This method deserializes the JSON file and populates the
+        "__objects" dictionary with objects stored in the JSON file."""
+
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as file:
+                objects_dict = json.load(file)
+            for key, value in objects_dict.items():
+                cls_name = value["__class__"]
+                if cls_name == "BaseModel":
+                    self.__objects[key] = BaseModel(**value)
+                elif cls_name == "User":
+                    self.__objects[key] = User(**value)
+                elif cls_name == "State":
+                    self.__objects[key] = State(**value)
+                elif cls_name == "City":
+                    self.__objects[key] = City(**value)
+                elif cls_name == "Amenity":
+                    self.__objects[key] = Amenity(**value)
+                elif cls_name == "Place":
+                    self.__objects[key] = Place(**value)
+                elif cls_name == "Review":
+                    self.__objects[key] = Review(**value)
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
