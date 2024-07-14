88c249ee790de09b7b0acf5fcc22969f47851f5a
popstarmav <cj.johnson2017@gmail.com>
2024-07-14 11:52:31 -0500

    test end points to ensure it correctly returns the expected JSON structure with counts for each object type.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """Index for Route Module"""
 
-
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
+from models.base_model import classes
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Returns the number of each object type in the storage"""
+    obj_counts = {}
+    for cls_name, cls in classes.items():
+        obj_counts[cls_name] = storage.count(cls)
+
+    return jsonify(obj_counts)
