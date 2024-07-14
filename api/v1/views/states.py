5fe7f796c0da6ef917a25cd4945ccbcc6df304fe
aallm004 <abigailrosea10@gmail.com>
2024-07-14 17:09:42 -0500

    pycode error: line too long, corrected in states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -24,7 +24,8 @@ def get_state_by_id(state_id):
     return jsonify(state_json)
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
