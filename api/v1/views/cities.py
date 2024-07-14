ba710eb578c4a55031934029d2ded9f8b83e36e2
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-14 17:38:32 -0500

    in get_cities_by _state broke up list creation into all logical steps. Should return jsonified list

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -17,8 +17,11 @@ def get_cities_by_state(state_id):
     state = storage.get("State", state_id)
     if not state:
         abort(404)
-    cities = [city.to_dict() for city in state.cities]
-    return jsonify(cities)
+    cities = state.cities
+    cities_list = []
+    for city in cities:
+        cities_list.append(city.to_dict())
+    return jsonify(cities_list)
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city_by_id(city_id):
