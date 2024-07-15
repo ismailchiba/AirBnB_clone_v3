873bf3b9df44ff12a816bb3694ea7e6abce21972
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-14 20:02:31 -0500

    pycode styling for comments lines 60, 82

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -57,7 +57,7 @@ def post_city(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    if not request.is_json: # check for malformed request
+    if not request.is_json:  # check for malformed request
         abort(400, 'Not a JSON')
     data = request.get_json()
     if not data:
@@ -79,7 +79,7 @@ def put_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    if not request.is_json: # check for malformed request
+    if not request.is_json:  # check for malformed request
         abort(400, 'Not a JSON')
     data = request.get_json()
     if not data:
