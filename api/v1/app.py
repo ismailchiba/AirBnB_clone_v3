d01c747dedd6e8b427c2c0ecef9c2b8c49914060
LJThao <yengfong.thao@atlasschool.com>
2024-07-15 23:16:31 +0000

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """
 Module for Flask application setup & config
 """
+
 from flask import Flask
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', '..')))
 
 from models import storage
 from api.v1.views import app_views
@@ -14,6 +16,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(exception):
