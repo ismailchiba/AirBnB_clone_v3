dffefc45f72c5e61515013be64b033f5d9949fec
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-15 13:35:17 -0500

    pycode styling lines 91, 92

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -88,7 +88,8 @@ def put_review(review_id):
         abort(400, 'Not a JSON')
     data = request.get_json()
     for key, value in data.items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated_at']:
+        if key not in [
+            'id', 'user_id', 'place_id', 'created_at', 'updated_at']:
             setattr(review, key, value)
     review.save()
     return jsonify(review.to_dict()), 200
