a4dbae2e1b56ab4ab84b87e67be9d3fdf2843551
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-14 19:51:29 -0500

    line 67 city.save instead of storage.save

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,6 +8,7 @@ from models import storage
 from models.state import State
 from models.city import City
 
+
 @app_views.route(
         '/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_cities_by_state(state_id):
@@ -23,6 +24,7 @@ def get_cities_by_state(state_id):
         cities_list.append(city.to_dict())
     return jsonify(cities_list)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city_by_id(city_id):
     """
@@ -32,6 +34,7 @@ def get_city_by_id(city_id):
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -44,7 +47,9 @@ def delete_city(city_id):
     storage.delete(city)
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route(
+        '/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def post_city(state_id):
     """
     Creates a new city
@@ -59,9 +64,10 @@ def post_city(state_id):
         abort(400, 'Missing name')
     city = City(**data)
     setattr(city, 'state_id', state.id)
-    storage.save()
+    city.save()
     city_json = city.to_dict()
     return jsonify(city_json), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
