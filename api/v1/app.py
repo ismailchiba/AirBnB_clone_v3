4e5fda5fad25907ac90e5cc1b48d19f76e291cfd
popstarmav <cj.johnson2017@gmail.com>
2024-07-15 13:10:49 -0500

    Add Flask application setup and API status endpoint

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
 """
 Module for Flask application setup & config
-Sets up for Flask web app for CORS and routes
 """
+from flask import Flask
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
-from flask_cors import CORS
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 
 @app.teardown_appcontext
-def remove_session(exception):
-    """
-    Close the storage session after each request.
-
-    This function is called by Flask after each request.
-    It ensures that the storage session is closed.
-    """
+def teardown(exception):
+    """ Close storage session """
     storage.close()
-
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
