787801ec6df478c34f3dd57b31a9dd79218135ea
LJThao <yengfong.thao@atlasschool.com>
2024-07-16 00:34:46 +0000

    added code to retrieve objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,9 @@
 """Index for Route Module"""
 
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
+from models import storage
+from models import Amenity, City, Place, Review, State, User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -13,12 +15,11 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def obj_count():
     """Returns the number of each object type in the storage"""
-    counts = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
-    }
-    return jsonify(counts)
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    count_obj = {}
+    for i in range(len(classes)):
+        count_obj[names[i]] = storage.count(classes[i])
+
+    return jsonify(count_obj)
