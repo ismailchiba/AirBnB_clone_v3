9d06f6a1a09410a73094458087907a924a610dca
LJThao <yengfong.thao@atlasschool.com>
2024-07-16 00:14:58 +0000

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,20 +5,14 @@ Module for Flask application setup & config
 
 
 import os
-import sys
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 
 
-sys.path.append(os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', '..')))
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
