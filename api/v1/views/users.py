0be049c0552852327e3f50feb2fd061930a3f9d6
LJThao <yengfong.thao@atlasschool.com>
2024-07-17 02:30:35 +0000

    added created and updated user functions

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -34,3 +34,33 @@ def delete_user(user_id):
     storage.delete(user)
     storage.save()
     return jsonify({}), 200
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a new User"""
+    data = request.get_json()
+    if not data:
+        abort(400, description='Not a JSON')
+    if 'email' not in data:
+        abort(400, description='Missing email')
+    if 'password' not in data:
+        abort(400, description='Missing password')
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates a User"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, description='Not a JSON')
+    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
