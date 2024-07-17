a7b105d7adc6c63b2ca85460d883240ff3e23ac9
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-17 01:01:33 +0000

    added routing to CORS or something

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
