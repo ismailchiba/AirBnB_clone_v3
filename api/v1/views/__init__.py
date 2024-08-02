9c65973ab44c2908d69a7c14b70122c8f5276879
mwanzaalbert <user.email>
2024-08-02 04:38:59 -0700

    pycache removed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Initialization file"""
-from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views import states
-from api.v1.views import cities
-from api.v1.views import amenities
-from api.v1.views import users
-from api.v1.views import places
-from api.v1.views import places_reviews
-from api.v1.views import places_amenities
+"""Init file for views module"""
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from flask import Blueprint
+
+# Importing the views after creating the Blueprint to avoid circular imports
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
