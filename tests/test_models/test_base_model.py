82e1fcf1c016ceb50707721a28e5fc60729456c1
basma ebraheim <basmaebraheim@gmail.com>
2024-08-02 17:11:39 +0300

    update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -86,7 +86,7 @@ class TestBaseModel(unittest.TestCase):
         inst1 = BaseModel()
         toc = datetime.now()
         print(tic)
-        print(inst1)
+        print(inst1.created_at)
         print(toc)
         self.assertTrue(tic <= inst1.created_at <= toc)
         time.sleep(1e-4)
