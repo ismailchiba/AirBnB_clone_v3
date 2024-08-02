a2b6d0b17af5a49d5054f6c8017053871c3723ce
mwanzaalbert <user.email>
2024-08-02 05:11:02 -0700

    redo status of api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""Init file for views module"""
+"""Initialization file"""
 
 from flask import Blueprint
 
-# Importing the views after creating the Blueprint to avoid circular imports
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from models import storage
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Get status in json format"""
-    return jsonify({"status": "OK"})
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
