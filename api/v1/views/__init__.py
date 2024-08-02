51bf0cff44759d15c7d5556457bec59cd5e53792
mwanzaalbert <user.email>
2024-08-02 12:36:19 +0300

    pycodestyle formatting

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """Initialization file"""
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views import states
 from api.v1.views import cities
@@ -11,4 +8,6 @@ from api.v1.views import amenities
 from api.v1.views import users
 from api.v1.views import places
 from api.v1.views import places_reviews
-from api.v1.views import places_amenities 
+from api.v1.views import places_amenities
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
