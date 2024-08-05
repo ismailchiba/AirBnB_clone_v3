a6bbc8c04d4c92e77457c5dfdbc4603b292d5e8d
= <=>
2024-08-05 11:54:46 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
 """ Flask Application """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, render_template, url_for
 from models import storage
 from api.v1.views import app_views
-from os import environ
+import os
+from flask_cors import CORS, cross_origin
+from flasgger import Swagger
+from werkzeug.exceptions import HTTPException
 
+# Global Flask Application Variable: app
 app = Flask(__name__)
+swagger = Swagger(app)
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+# Config Flask Application: app
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['DEBUG'] = True
+
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
+
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r'/*': {'origins': host}})
 
 
 @app.teardown_appcontext
@@ -17,12 +33,42 @@ def close_db(error):
     storage.close()
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+        Global Route to handle All Error Status Codes
+    """
+    if isinstance(err, HTTPException):
+        if type(err).__name__ == 'NotFound':
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
+
+
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
+
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    # initializes global error handling
+    setup_global_errors()
+    # start Flask app
     app.run(host=host, port=port, threaded=True)
