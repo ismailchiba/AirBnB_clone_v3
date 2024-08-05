4befae4c166c95aef704ced7bac65dd473472108
= <=>
2024-08-05 16:03:29 +0100

    testing

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,13 +4,11 @@ from models.city import City
 from models.state import State
 from models import storage
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
+from flask import abort, jsonify, request
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/city/cities_by_state.yml', methods=['GET'])
 def get_cities(state_id):
     """
     Retrieves the list of all cities objects
@@ -27,7 +25,6 @@ def get_cities(state_id):
 
 
 @app_views.route('/cities/<city_id>/', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/city/get_city.yml', methods=['GET'])
 def get_city(city_id):
     """
     Retrieves a specific city based on id
@@ -39,7 +36,6 @@ def get_city(city_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
-@swag_from('documentation/city/delete_city.yml', methods=['DELETE'])
 def delete_city(city_id):
     """
     Deletes a city based on id provided
@@ -51,12 +47,11 @@ def delete_city(city_id):
     storage.delete(city)
     storage.save()
 
-    return make_response(jsonify({}), 200)
+    return jsonify({}), 200
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/city/post_city.yml', methods=['POST'])
 def post_city(state_id):
     """
     Creates a City
@@ -73,11 +68,10 @@ def post_city(state_id):
     instance = City(**data)
     instance.state_id = state.id
     instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    return jsonify(instance.to_dict()), 201
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/city/put_city.yml', methods=['PUT'])
 def put_city(city_id):
     """
     Updates a City
@@ -96,4 +90,4 @@ def put_city(city_id):
         if key not in ignore:
             setattr(city, key, value)
     storage.save()
-    return make_response(jsonify(city.to_dict()), 200)
+    return jsonify(city.to_dict()), 200
