a58d3e98c7b341d8fe8f543c53c5242cc48eaaf4
= <=>
2024-08-05 07:29:24 +0100

    update

---
+++ api/v1/views/index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Index implementation """
+from api.v1.views import app_views
+from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    status = {"status": "OK"}
+    response = jsonify(status)
+    response.indent = 2
+    return response
+
+
+@app_views.route('stats', methods=['GET'], strict_slashes=False)
+def count_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
+    
