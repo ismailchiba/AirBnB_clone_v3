2fbbd6126f6f2f90cf60f75d34f3aa441fc2ade6
= <=>
2024-08-05 07:59:43 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """ Index implementation """
 from api.v1.views import app_views
-from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from flask import jsonify, request
 from models import storage
 
 
@@ -20,14 +14,21 @@ def status():
     return response
 
 
-@app_views.route('stats', methods=['GET'], strict_slashes=False)
-def count_objects():
-    """ Retrieves the number of each objects by type """
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objs)
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
