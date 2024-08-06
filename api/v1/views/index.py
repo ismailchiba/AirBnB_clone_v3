ecf89aeb1bbebead237ffd4ca4d0caa802ba39da
= <=>
2024-08-06 01:15:45 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Index implementation """
-from api.v1.views import app_views
+"""Contains the index view for the API."""
 from flask import jsonify
+
+from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 from models.city import City
@@ -11,18 +12,17 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """ Status of API """
-    status = {"status": "OK"}
-    response = jsonify(status)
-    response.indent = 2
-    return response
+@app_views.route('/status')
+def get_status():
+    """Gets the status of the API."""
+
+    return jsonify(status='OK')
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def number_objects():
-    """ Retrieves the number of each objects by type """
+@app_views.route('/stats')
+def get_stats():
+    """Gets the number of objects for each type."""
+
     objects = {
         'amenities': Amenity,
         'cities': City,
